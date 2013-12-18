5f13a731ea2b322e9ddcbd5b98b0b29ef5fa1359
Sami Luukkonen <sami@devlab.fi>
2013-12-18 13:43:15 +0200

    Terms of payment view tuning

--- app/controllers/terms_of_payments_controller.rb
+++ app/controllers/terms_of_payments_controller.rb
@@ -9,6 +9,7 @@ class TermsOfPaymentsController < ApplicationController
 
   # GET /terms_of_payments/1
   def show
+    render 'edit'
   end
 
   # # GET /terms_of_payments/new
@@ -16,9 +17,9 @@ class TermsOfPaymentsController < ApplicationController
   #   @terms_of_payment = TermsOfPayment.new
   # end
 
-  # # GET /terms_of_payments/1/edit
-  # def edit
-  # end
+  # GET /terms_of_payments/1/edit
+  def edit
+  end
 
   # # POST /terms_of_payments
   # def create
@@ -31,14 +32,14 @@ class TermsOfPaymentsController < ApplicationController
   #   end
   # end
 
-  # # PATCH/PUT /terms_of_payments/1
-  # def update
-  #   if @terms_of_payment.update(terms_of_payment_params)
-  #     redirect_to @terms_of_payment, notice: 'Terms of payment was successfully updated.'
-  #   else
-  #     render action: 'edit'
-  #   end
-  # end
+  # PATCH/PUT /terms_of_payments/1
+  def update
+    if @terms_of_payment.update(terms_of_payment_params)
+      redirect_to terms_of_payments_path, notice: 'Terms of payment was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
 
   # # DELETE /terms_of_payments/1
   # def destroy
@@ -49,7 +50,32 @@ class TermsOfPaymentsController < ApplicationController
   private
     # Only allow a trusted parameter "white list" through.
     def terms_of_payment_params
-      params[:terms_of_payment].permit(:teksti)
+      params[:terms_of_payment].permit(
+        :teksti,
+        :rel_pvm,
+        :abs_pvm,
+        :kassa_relpvm,
+        :kassa_abspvm,
+        :kassa_alepros,
+        :osamaksuehto1,
+        :osamaksuehto2,
+        :summanjakoprososa2,
+        :jv,
+        :kateinen,
+        :suoraveloitus,
+        :factoring,
+        :pankkiyhteystiedot,
+        :itsetulostus,
+        :jaksotettu,
+        :erapvmkasin,
+        :sallitut_maat,
+        :kaytossa,
+        :jarjestys,
+        :laatija,
+        :luontiaika,
+        :muutospvm,
+        :muuttaja
+      )
     end
 
     def find_terms_of_payment
