6b231b79b53b799a887fafbe4b12c077e763ea7a
Henri Hesse <hesse@devlab.fi>
2013-12-20 16:50:20 +0200

    added model validation & callback

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,9 +1,14 @@
 class Currency < ActiveRecord::Base
 
-  validates :nimi, presence: true
+  validates :nimi, length: { is: 3 }
+  before_validation :ensure_nimi_is_uppercase
 
   self.table_name = "valuu"
   self.primary_key = "tunnus"
   self.record_timestamps = false
 
+  protected
+    def ensure_nimi_is_uppercase
+        self.nimi = self.nimi.upcase if self.nimi.is_a? String
+    end
 end
---
+++ test/models/currency_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class CurrencyTest < ActiveSupport::TestCase
+
+  def setup
+    @currency = currencies(:currency1)
+  end
+
+  test 'should be valid nimi' do
+    @currency.nimi = 'TES'
+    assert @currency.valid?
+  end
+
+  test 'should be invalid nimi' do
+    @currency.nimi = 'TOO_LONG'
+    refute @currency.valid?
+
+    # Too short nimi
+    @currency.nimi = 'S'
+    refute @currency.valid?
+
+    @currency.nimi = nil
+    refute @currency.valid?
+  end
+
+  test 'nimi should be uppercased' do
+    @currency.nimi = 'tes'
+    assert @currency.save
+    assert_equal @currency.nimi, 'TES'
+  end
+
+end
