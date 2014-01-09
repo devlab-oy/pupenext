2e6a7ea524f9086d2143b1cbc7301757dfa03803
Sami Luukkonen <sami@devlab.fi>
2014-01-09 11:01:43 +0200

    Added client model

---
+++ app/models/client.rb
@@ -0,0 +1,9 @@
+class Client < ActiveRecord::Base
+
+  belongs_to :company, foreign_key: :yhtio, primary_key: :yhtio
+
+  self.table_name = "asiakas"
+  self.primary_key = "tunnus"
+  self.record_timestamps = false
+
+end
