337e8a9e5d5b305ba6daa2ca7037504e37d959c1
Sami Luukkonen <sami@devlab.fi>
2014-10-10 16:49:08 +0300

    Sortable helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,16 @@ module ApplicationHelper
     @current_company ||= current_user.company
   end
 
+  def sortable(column)
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    options = {}
+    options['sort'] = column
+    options['direction'] = direction
+    options['not_used'] = params[:not_used]
+
+    # If controller implements params_search, add search params to sort url
+    params_search.merge! options if respond_to? :params_search
+
+    link_to column, options
+  end
 end
