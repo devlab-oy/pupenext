8f25d7376ceff89541e2c27d164afe46d5bd4a4c
Joni Kanerva <joni@devlab.fi>
2014-10-16 11:17:54 +0300

    set version, manually for now

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141016000000) do
 
   create_table "abc_aputaulu", primary_key: "tunnus", force: true do |t|
     t.string   "yhtio",              limit: 5,                            default: "",  null: false
