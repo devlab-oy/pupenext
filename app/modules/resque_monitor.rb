73eb7f2bf71bdc59412540639ebac3c4c9a348b7
Johan Tötterman <juppe@devlab.fi>
2014-10-28 17:22:34 +0200

    Monitor rescue failed actions.

--- app/modules/resque_monitor.rb
+++ app/modules/resque_monitor.rb
@@ -16,13 +16,23 @@ module ResqueMonitor
     check_queue options
   end
 
+  def self.nagios_resque_failed
+    options  = {
+      redis_host: 'localhost',
+      redis_port: '6379',
+      critical:   10,
+      warning:    5
+    }
+
+    check_failed options
+  end
+
   private
 
     def self.check_queue(options)
       queuename = "Resque queue #{options[:queue]}"
 
       redis = Redis.new(:host => options[:redis_host], :port => options[:redis_port])
-
       Resque.redis = redis
 
       begin
@@ -38,6 +48,28 @@ module ResqueMonitor
         status = "CRITICAL - #{queuename} size #{queue_size} #{NAGIOS_STATE_CRITICAL}"
       elsif queue_size >= options[:warning]
         status = "WARNING - #{queuename} size #{queue_size} #{NAGIOS_STATE_WARNING}"
+      end
+
+      status
+    end
+
+    def self.check_failed(options)
+      redis = Redis.new(:host => options[:redis_host], :port => options[:redis_port])
+      Resque.redis = redis
+
+      begin
+        failed_count = Resque::Failure.count
+      rescue
+        status = "CRITICAL - Cannot get Resque failed count #{NAGIOS_STATE_CRITICAL}"
+        return status
+      end
+
+      status = "OK - Resque failed count is ok #{NAGIOS_STATE_OK}"
+
+      if failed_count >= options[:critical]
+        status = "CRITICAL - Resque failed count is #{failed_count} #{NAGIOS_STATE_CRITICAL}"
+      elsif failed_count >= options[:warning]
+        status = "WARNING - Resque failed count is #{failed_count} #{NAGIOS_STATE_WARNING}"
       end
 
       status
