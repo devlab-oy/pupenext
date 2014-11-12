4583aa4a5697ec60542140a5e9ff3245e4c9b61a
Joonas Harjukallio <joonas@devlab.fi>
2014-11-12 17:02:43 +0200

    Controller tests

--- test/controllers/sum_levels_controller_test.rb
+++ test/controllers/sum_levels_controller_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class SumLevelsControllerTest < ActionController::TestCase
-
   def setup
     cookies[:pupesoft_session] = users(:joe).session
+    @user = users(:joe)
+    @sum_level = sum_levels(:external)
   end
 
   test "should get index" do
@@ -11,8 +12,26 @@ class SumLevelsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should not get index" do
+    @user.revoke_all('pupenext/sum_levels')
+    get :index
+    assert_response :forbidden
+  end
+
   test "should get new" do
     get :new
+    assert_response :success
+  end
+
+  test "should not get new" do
+    @user.revoke_all('pupenext/sum_levels')
+    get :new
+    assert_response :forbidden
+  end
+
+  test "should show sum level" do
+    request = {id: @sum_level.id}
+    get :show, request
     assert_response :success
   end
 
@@ -31,5 +50,4 @@ class SumLevelsControllerTest < ActionController::TestCase
   test "should destroy" do
     assert_response :success
   end
-
 end
