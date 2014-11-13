723a9d70a990c3fa9659df5d13761722c580de07
Joonas Harjukallio <joonas@devlab.fi>
2014-11-13 10:08:33 +0200

    Override model_name in SumLevel children

--- app/models/sum_level/internal.rb
+++ app/models/sum_level/internal.rb
@@ -11,4 +11,10 @@ class SumLevel::Internal < SumLevel
   def self.human_readable_type
     "Sisäinen"
   end
+
+  #Rails figures out paths from the model name. User model has users_path etc.
+  #With STI we want to use same name for each child. Thats why we override model_name
+  def self.model_name
+    SumLevel.model_name
+  end
 end
--- app/models/sum_level/vat.rb
+++ app/models/sum_level/vat.rb
@@ -9,4 +9,10 @@ class SumLevel::Vat < SumLevel
   def self.human_readable_type
     "Alv"
   end
+
+  #Rails figures out paths from the model name. User model has users_path etc.
+  #With STI we want to use same name for each child. Thats why we override model_name
+  def self.model_name
+    SumLevel.model_name
+  end
 end
