1f8335eaf28735ebf7a86a2bb93b2fdab38f039a
Mika Myllynen <giefsufu@gmail.com>
2014-12-18 10:17:45 +0200

    tests for getting fiscal year

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -18,4 +18,18 @@ class CompanyTest < ActiveSupport::TestCase
     assert_not_nil @acme.parameter
   end
 
+  test 'get fiscal year returns fiscal year' do
+    @acme.tilikausi_alku = Date.today
+    @acme.tilikausi_loppu = Date.today+4
+    assert_equal [Date.today, Date.today+4], @acme.get_fiscal_year
+  end
+
+  test 'method returns if given date is in this fiscal year' do
+    @acme.tilikausi_alku = Date.today
+    @acme.tilikausi_loppu = Date.today+4
+    assert @acme.is_date_in_this_fiscal_year? Date.today+1
+    refute @acme.is_date_in_this_fiscal_year? Date.today+5
+    refute @acme.is_date_in_this_fiscal_year? Date.yesterday
+  end
+
 end
