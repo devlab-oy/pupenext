ad5de845d088e640735270f5b21adabac099f5ae
Joni Kanerva <joni@devlab.fi>
2014-12-18 10:09:02 +0200

    test all cases

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -48,14 +48,26 @@ class UserTest < ActiveSupport::TestCase
     @joe.company.parameter.kayttoliittyma = 'U'
     @joe.kayttoliittyma = ''
     refute @joe.classic_ui?, "company set new, joe set nothing, should be new"
+    @joe.kayttoliittyma = 'U'
+    refute @joe.classic_ui?, "company set new, joe set new, should be new"
+    @joe.kayttoliittyma = 'C'
+    assert @joe.classic_ui?, "company set new, joe set classic, should be classic"
 
     @joe.company.parameter.kayttoliittyma = 'C'
     @joe.kayttoliittyma = ''
     assert @joe.classic_ui?, "company set classic, joe set nothing, show be classic"
+    @joe.kayttoliittyma = 'U'
+    refute @joe.classic_ui?, "company set classic, joe set new, show be new"
+    @joe.kayttoliittyma = 'C'
+    assert @joe.classic_ui?, "company set classic, joe set classic, show be classic"
 
     @joe.company.parameter.kayttoliittyma = ''
     @joe.kayttoliittyma = ''
     assert @joe.classic_ui?, "company set nothing, joe set nothing, should be classic"
+    @joe.kayttoliittyma = 'U'
+    refute @joe.classic_ui?, "company set nothing, joe set new, should be new"
+    @joe.kayttoliittyma = 'C'
+    assert @joe.classic_ui?, "company set nothing, joe set classic, should be classic"
   end
 
 end
