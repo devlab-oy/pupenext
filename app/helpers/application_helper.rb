95295be3eed462c0fa00133981d939d78c72a8f6
tiere <tommi@devlab.fi>
2015-01-09 12:08:05 +0200

    Remove methods from application helper that don't belong there

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,36 +1,8 @@
 module ApplicationHelper
   PER_PAGE=10
 
-  def current_user
-    @current_user ||= User.find_by_session(cookies[:pupesoft_session])
-  end
-
-  def current_company
-    @current_company ||= current_user.company
-  end
-
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
-  end
-
   def sortable(column_name)
     link_to column_name, sort_options(column_name)
-  end
-
-  def t(string)
-    language = current_user ? current_user.locale : nil
-    Dictionary.translate(string, language)
-  end
-
-  def read_access?
-    # Root path does not require access
-    return true if request_path == root_path
-
-    current_user.can_read? request_path
-  end
-
-  def update_access?
-    current_user.can_update? request_path
   end
 
   def firefox?
@@ -50,15 +22,6 @@ module ApplicationHelper
   end
 
   private
-
-    def request_path
-      # return first resource from request, our access control is based on it
-      path = request.path_info.split '/'
-
-      access = '/'
-      access << path.second unless path.empty?
-      access
-    end
 
     def user_agent_include?(value)
       request.user_agent.downcase.include? value.to_s.downcase
