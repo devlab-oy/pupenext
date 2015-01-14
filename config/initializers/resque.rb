f1f759ec6e03ec66e09da016b2fe254cecc7f3e4
Joni Kanerva <joni@devlab.fi>
2015-01-14 14:26:41 +0200

    change redis config

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,2 @@
-Resque.redis = Redis.new(:url => 'http://localhost:6379')
+Resque.redis = Redis.new(host: 'localhost', port: 6379)
 Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
