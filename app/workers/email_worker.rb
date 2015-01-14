f3ee76fbee5a8a14ed911145aeb41b1e17a902b1
Joni Kanerva <joni@devlab.fi>
2015-01-14 14:38:08 +0200

    user default mailer in job

--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -2,47 +2,6 @@ class EmailWorker < ActiveJob::Base
   queue_as :PupeEmail
 
   def perform(params)
-    # Params are inserted by php-resque into redis database and are of the following format
-    #
-    #[{"user"          => "somebody@example.com",
-    #  "error_message" => "Sending failed to: somebody@example.com",
-    #  "from"          => "no-reply@example.com",
-    #  "from_name"     => "Somecompany Oy",
-    #  "to"            => "somebody@example.com",
-    #  "cc"            => "",
-    #  "subject"       => "Somecompany Oy - Invoice 1812107",
-    #  "body"          => "",
-    #  "attachements"  => [
-    #    {"filename"    => "/tmp/Lasku-adbce9b2d6403fa7e5ba71e2158ccc32.pdf",
-    #     "newfilename" => "Lasku 1812107.pdf"
-    #    }]
-    #  }
-    #]
-
-    files = params['attachements'] ? params['attachements'] : []
-
-    files.each do |file|
-      filename = file['newfilename']
-      filepath = file['filename']
-
-      attachments[filename] = File.read(filepath)
-    end
-
-    from = %Q("#{params['from_name']}" <#{params['from']}>)
-
-    begin
-      mail(from: from,
-           to: params['to'],
-           subject: params['subject'],
-           body: params['body']).deliver_now
-    rescue => e
-      # Notify user if mail sending failed
-      body = "#{params['error_message']}\n\nERROR: #{e}\n\n#{params['body']}"
-
-      mail(from: from,
-           to: params['user'],
-           subject: params['subject'],
-           body: body).deliver_now
-    end
+    DefaultMailer.pupesoft_email(params).deliver_now
   end
 end
