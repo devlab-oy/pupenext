165c5cf72a2c54cbe6547b5ba3a0776aef4e60ec
Joni Kanerva <joni@devlab.fi>
2015-01-20 13:22:02 +0200

    use searchable

--- app/models/fixed_assets/commodity.rb
+++ app/models/fixed_assets/commodity.rb
@@ -1,4 +1,6 @@
 class FixedAssets::Commodity < ActiveRecord::Base
+  include Searchable
+
   belongs_to :company
   has_one :voucher, foreign_key: :hyodyke_tunnus, class_name: 'Head::Voucher'
   has_many :commodity_rows
