4041cd6a8f5e335bc662cc34282e61aa98a094f3
Joni Kanerva <joni@devlab.fi>
2015-02-11 14:04:14 +0200

    remove unused routes for now

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,7 @@ Pupesoft::Application.routes.draw do
   get 'monitoring/nagios/resque/email', to: 'monitoring#nagios_resque_email'
   get 'monitoring/nagios/resque/failed', to: 'monitoring#nagios_resque_failed'
 
-
   namespace :fixed_assets do
-    get '/commodities/:id/select_purchase_order(.:format)',
-      to: 'commodities#select_purchase_order', as: 'commodities_purchase_orders'
-    get '/commodities/:id/select_voucher',
-      to: 'commodities#select_voucher',as: 'commodities_vouchers'
-    get '/commodities/fiscal_year_run',
-      to: 'commodities#fiscal_year_run',as: 'fiscal_year_run'
     resources :commodities, except: :destroy
   end
 
