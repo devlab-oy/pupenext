bcd0f5eb05ca4f3feac915609bd5b41f350ccbc7
Joni Kanerva <joni@devlab.fi>
2015-02-11 16:09:29 +0200

    tidy

--- test/controllers/fixed_assets/commodities_controller_test.rb
+++ test/controllers/fixed_assets/commodities_controller_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class FixedAssets::CommoditiesControllerTest < ActionController::TestCase
-
   setup do
     login users(:bob)
     @commodity = fixed_assets_commodities(:commodity_one)
@@ -11,8 +10,8 @@ class FixedAssets::CommoditiesControllerTest < ActionController::TestCase
     get :index
     assert_response :success
 
-    assert_template "index", "Template should be index"
     assert_not_nil assigns(:commodities)
+    assert_template "index", "Template should be index"
   end
 
   test 'should show commodity' do
@@ -34,16 +33,19 @@ class FixedAssets::CommoditiesControllerTest < ActionController::TestCase
   end
 
   test 'should get create new commodity' do
-    assert_difference('FixedAssets::Commodity.count',1) do
-      patch :create, fixed_assets_commodity: {
-            name: 'Chair30000',
-            description: 'Chair for CEO',
-            planned_depreciation_type: 'B'
-      }
-      assert_response :found
-      assert_nil assigns(:commodity).planned_depreciation_type
-      assert_redirected_to edit_commodity_path assigns(:commodity)
+    params = {
+      name: 'Chair30000',
+      description: 'Chair for CEO',
+      planned_depreciation_type: 'B'
+    }
+
+    assert_difference('FixedAssets::Commodity.count') do
+      patch :create, fixed_assets_commodity: params
     end
+
+    assert_response :found
+    assert_nil assigns(:commodity).planned_depreciation_type
+    assert_redirected_to edit_commodity_path assigns(:commodity)
   end
 
   test 'should update commodity' do
@@ -71,10 +73,13 @@ class FixedAssets::CommoditiesControllerTest < ActionController::TestCase
   test 'should not create new commodity due to permissions' do
     login users(:joe)
 
+    params = {
+      name: 'House',
+      description: 'Gregory, M.D.'
+    }
+
     assert_no_difference('FixedAssets::Commodity.count') do
-      post :create, fixed_assets_commodity: {
-        name: 'House', description: 'Gregory, M.D.'
-      }
+      post :create, fixed_assets_commodity: params
     end
 
     assert_response :forbidden
@@ -84,13 +89,18 @@ class FixedAssets::CommoditiesControllerTest < ActionController::TestCase
     assert_no_difference('FixedAssets::Commodity.count') do
       post :create, fixed_assets_commodity: { name: nil }
     end
+
     assert_template 'new', 'Template should be new'
   end
 
   test 'should not update commodity due to params missing' do
-    patch :update, id: @commodity, fixed_assets_commodity: {
-      name: 'Kissa', description: ''
+    params = {
+      name: 'Kissa',
+      description: ''
     }
+
+    patch :update, id: @commodity, fixed_assets_commodity: params
+
     assert_not_equal 'Kissa', @commodity.reload.name
     assert_template 'edit', 'Template should be edit'
   end
@@ -98,7 +108,7 @@ class FixedAssets::CommoditiesControllerTest < ActionController::TestCase
   test 'should not update commodity due to rights' do
     login users(:joe)
 
-    patch :update, id: @commodity.id, fixed_assets_commodity: {nimi: ''}
+    patch :update, id: @commodity.id, fixed_assets_commodity: { nimi: '' }
 
     assert_response :forbidden
   end
