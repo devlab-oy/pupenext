63277b8629af7cb2813c07256695f53dc7499f8f
Joni Kanerva <joni@devlab.fi>
2015-03-12 12:54:34 +0200

    regular module

--- app/models/concerns/translatable.rb
+++ app/models/concerns/translatable.rb
@@ -1,11 +1,5 @@
 module Translatable
-  extend ActiveSupport::Concern
-
-  module ClassMethods
-    private
-
-    def t(string)
-      Dictionary.translate(string, I18n.locale.to_s)
-    end
+  def t(string)
+    Dictionary.translate(string, I18n.locale.to_s)
   end
 end
