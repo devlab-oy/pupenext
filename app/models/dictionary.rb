28e5faa9a9260066e8f34da21f3f10fb4076d3d8
Joni Kanerva <joni@devlab.fi>
2015-03-12 15:52:04 +0200

    fix translation bug translate method now returns the untranslated string if we don't have a translation in the database

--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -3,22 +3,27 @@ class Dictionary < ActiveRecord::Base
   self.table_name = :sanakirja
   self.primary_key = :tunnus
 
-  def self.translate(string, language = nil)
-    language ||= "fi"
+  def self.translate(string, language = 'fi')
     language.downcase!
 
-    # Return string if we ask for Finnish word
-    return string if language == "fi"
+    # Return the string if we ask for a Finnish word
+    return string if language == 'fi'
 
-    # Generate cache key for string
+    # Generate a cache key for the string
     cache_key = string.hash
 
-    # Fetch translation (cache does not expire)
+    # Fetch the translation (cache does not expire)
     translation = Rails.cache.fetch("translation_#{cache_key}") do
-      where("fi = BINARY ?", string).first || string
+      where("fi = BINARY ?", string).first
     end
 
-    # Return string if we don't have translation
-    translation[language].present? ? translation[language] : string
+    # Return the string if don't have it in the database
+    return string if translation.nil?
+
+    # Get the translation for the given language
+    t_string = translation.send(language)
+
+    # Return the string if we don't have translation in correct languate
+    t_string.present? ? t_string : string
   end
 end
