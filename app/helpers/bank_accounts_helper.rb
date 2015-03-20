5a4dc3409697282670398db0d0dd74f37d0a9516
tiere <tommi@devlab.fi>
2015-03-20 16:41:40 +0200

    Refactor bank account form

---
+++ app/helpers/bank_accounts_helper.rb
@@ -0,0 +1,21 @@
+module BankAccountsHelper
+  def currency_options
+    current_company.currencies.order(:jarjestys)
+  end
+
+  def hyvaksyja_options
+    current_company.users.order(:nimi)
+  end
+
+  def kustannuspaikka_options
+    current_company.cost_centers.order("koodi+0, koodi, nimi")
+  end
+
+  def kohde_options
+    current_company.targets.order("koodi+0, koodi, nimi")
+  end
+
+  def projekti_options
+    current_company.projects.order("koodi+0, koodi, nimi")
+  end
+end
