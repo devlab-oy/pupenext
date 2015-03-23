fca0de2a516a73aad56f297ec94ffb9c5fc976e9
tiere <tommi@devlab.fi>
2015-03-23 16:23:17 +0200

    Add relations
    * Add default liquidity, expense and clearing account relations to bank account
    * Add default cost center, target and project relations to bank account

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -6,6 +6,19 @@ class BankAccount < ActiveRecord::Base
 
   belongs_to :company, foreign_key: :yhtio, primary_key: :yhtio
 
+  with_options class_name: "Account", primary_key: :tilino do |o|
+    o.belongs_to :default_liquidity_account, ->(b) { where(yhtio: b.yhtio) },
+                 foreign_key: :oletus_rahatili
+    o.belongs_to :default_expense_account, ->(b) { where(yhtio: b.yhtio) },
+                 foreign_key: :oletus_kulutili
+    o.belongs_to :default_clearing_account, ->(b) { where(yhtio: b.yhtio) },
+                 foreign_key: :oletus_selvittelytili
+  end
+
+  belongs_to :default_cost_center, foreign_key: :oletus_kustp, class_name: "Qualifier::CostCenter"
+  belongs_to :default_target, foreign_key: :oletus_kohde, class_name: "Qualifier::Target"
+  belongs_to :default_project, foreign_key: :oletus_projekti, class_name: "Qualifier::Project"
+
   validates :nimi, presence: true
   validates :iban, presence: true, uniqueness: { scope: :company }
   validates :oletus_rahatili, presence: true
