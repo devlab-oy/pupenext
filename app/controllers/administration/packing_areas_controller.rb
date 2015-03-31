7282fe12549cae8e814d4e03c57cda1752c32a83
Sami Luukkonen <sami@devlab.fi>
2015-03-31 16:06:14 +0300

    fix controller and controller test

--- app/controllers/administration/packing_areas_controller.rb
+++ app/controllers/administration/packing_areas_controller.rb
@@ -1,34 +1,29 @@
 class Administration::PackingAreasController < AdministrationController
-
-  before_action :find_packing_area, only: [:show, :edit, :update]
   before_action :find_printers, only: [:new, :show, :create, :edit, :update]
   before_action :find_storages, only: [:new, :show, :create, :edit, :update]
 
   def index
-    @packing_areas = current_user.company.packing_areas
+    @packing_areas = current_company.packing_areas
   end
 
   def show
-     render 'edit'
+     render :edit
   end
 
   def edit
   end
 
   def new
-    @packing_area = current_user.company.packing_areas.build
+    @packing_area = current_company.packing_areas.build
     @packing_area.varasto = nil
     @packing_area.prio = nil
     @packing_area.pakkaamon_prio = nil
   end
 
   def create
-    @packing_area = current_user.company.packing_areas.build
-    @packing_area.attributes = packing_area_params
-    @packing_area.muuttaja = current_user.kuka
-    @packing_area.laatija = current_user.kuka
+    @packing_area = current_company.packing_areas.build(packing_area_params)
 
-    if @packing_area.save
+    if @packing_area.save_by current_user
       redirect_to packing_areas_path, notice: 'Packing area was successfully created.'
     else
       render action: 'new'
@@ -36,10 +31,7 @@ class Administration::PackingAreasController < AdministrationController
   end
 
   def update
-    @packing_area.attributes = packing_area_params
-    @packing_area.muuttaja = current_user.kuka
-
-    if @packing_area.save
+    if @packing_area.update_by(packing_area_params, current_user)
       redirect_to packing_areas_path, notice: 'Packing area was successfully updated.'
     else
       render action: 'edit'
@@ -53,8 +45,8 @@ class Administration::PackingAreasController < AdministrationController
 
   private
 
-    def find_packing_area
-      @packing_area = current_user.company.packing_areas.find(params[:id])
+    def find_resource
+      @packing_area = current_company.packing_areas.find(params[:id])
     end
 
     def find_printers
@@ -81,7 +73,4 @@ class Administration::PackingAreasController < AdministrationController
           :printteri7
         )
     end
-
-
-
 end
