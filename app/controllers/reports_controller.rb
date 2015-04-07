4609324f332fb50cffb6459794a09c6ab72123b0
Mika Myllynen <giefsufu@gmail.com>
2015-04-07 14:03:15 +0300

    add reports controller and basic tests

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,11 @@
+class ReportsController < ApplicationController
+  def depreciations_balance_sheet
+
+  end
+
+  def depreciation
+  end
+
+  def balance_statements
+  end
+end
---
+++ test/controllers/reports_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class ReportsControllerTest < ActionController::TestCase
+  setup do
+    login users(:bob)
+  end
+
+  test 'should get balance sheet with depreciations report' do
+    get :depreciations_balance_sheet
+    assert_response :success
+  end
+
+  test 'should get depreciation report' do
+    get :depreciation
+    assert_response :success
+  end
+
+  test 'should get balance statements report' do
+    get :balance_statements
+    assert_response :success
+  end
+end
