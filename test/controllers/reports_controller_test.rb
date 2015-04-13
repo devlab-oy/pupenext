c570762f75f8724d0f7bb00d863188fd8437fe71
Mika Myllynen <giefsufu@gmail.com>
2015-04-13 10:47:11 +0300

    test classes

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -7,17 +7,26 @@ class ReportsControllerTest < ActionController::TestCase
   end
 
   test 'should get balance sheet with depreciations report' do
+    example = DepreciationsBalanceSheetReport.new companies(:acme).id
     get :depreciations_balance_sheet
     assert_response :success
+    assigns(:report)
+    assert_equal example.class, assigns(:report).class
   end
 
   test 'should get depreciation difference report' do
+    example = DepreciationDifferenceReport.new companies(:acme).id
     get :depreciation_difference
     assert_response :success
+    assigns(:report)
+    assert_equal example.class, assigns(:report).class
   end
 
   test 'should get balance statements report' do
+    example = BalanceStatementsReport.new companies(:acme).id
     get :balance_statements
     assert_response :success
+    assigns(:report)
+    assert_equal example.class, assigns(:report).class
   end
 end
