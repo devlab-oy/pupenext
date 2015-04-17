6639a23ce500575662055d89514d8968736e0371
Joonas Harjukallio <joonas@devlab.fi>
2015-04-17 13:26:43 +0300

    Use date_field_tags. Fix support for date search

--- app/modules/datetime_utils.rb
+++ app/modules/datetime_utils.rb
@@ -1,4 +1,23 @@
 module DatetimeUtils
+  def self.is_db_date?(value)
+    is_db_format?(value) && is_valid?(value)
+  end
+
+  def self.is_db_format?(value)
+    parts = value.split '-' if value.is_a?(String)
+
+    return false if parts.nil?
+    return false unless parts.count == 3
+
+    begin
+      Date.new parts[0], parts[1], parts[2]
+    rescue
+      return false
+    end
+
+    true
+  end
+
   def self.is_valid?(value)
     begin
       DateTime.parse(value.to_s)
