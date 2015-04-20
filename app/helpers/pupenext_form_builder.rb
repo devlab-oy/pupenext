89d713fc8627f7df153aeca6716a51bbed8a3152
Joonas Harjukallio <joonas@devlab.fi>
2015-04-20 12:16:13 +0300

    Make more rails like

--- app/helpers/pupenext_form_builder.rb
+++ app/helpers/pupenext_form_builder.rb
@@ -1,17 +1,38 @@
 class PupenextFormBuilder < ActionView::Helpers::FormBuilder
   def pupenext_date_field(method)
-    if @object.send(method).is_a?(Date)
-      day   = @object.send(method).day
-      month = @object.send(method).month
-      year  = @object.send(method).year
-    else
-      day   = ''
-      month = ''
-      year  = ''
-    end
+    options = default_values method
 
-    @template.text_field_tag(@object_name, method, { value: day, size: 3 }) + ' ' +
-      @template.text_field_tag(@object_name, method, { value: month, size: 3 }) + ' ' +
-      @template.text_field_tag(@object_name, method, { value: year, size: 5 })
+    options.map! { |option| option[:value] = '' } unless @object.send(method).is_a?(Date)
+
+    @template.text_field_tag(options[:day][:name], method, { value: options[:day][:value], size: options[:day][:size] }) + ' ' +
+      @template.text_field_tag(options[:month][:name], method, { value: options[:month][:value], size: options[:month][:size] }) + ' ' +
+      @template.text_field_tag(options[:year][:name], method, { value: options[:year][:value], size: options[:year][:size] })
+  end
+
+  def input_name_from_type(field_name, type)
+    prefix     = @object_name.to_s
+    field_name += "(#{ActionView::Helpers::DateTimeSelector::POSITION[type]}i)"
+
+    "#{prefix}[#{field_name}]"
+  end
+
+  def default_values(method)
+    {
+      day:   {
+        value: @object.send(method).day,
+        name:  input_name_from_type(method.try(:to_s), :day),
+        size:  3
+      },
+      month: {
+        value: @object.send(method).month,
+        name:  input_name_from_type(method.try(:to_s), :month),
+        size:  3
+      },
+      year:  {
+        value: @object.send(method).year,
+        name:  input_name_from_type(method.try(:to_s), :year),
+        size:  5
+      },
+    }
   end
 end
