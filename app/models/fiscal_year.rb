8815d71da6e644fef1a332f72f9bd3cb2b5469aa
Joonas Harjukallio <joonas@devlab.fi>
2015-04-21 15:44:13 +0300

    Comment

--- app/models/fiscal_year.rb
+++ app/models/fiscal_year.rb
@@ -3,11 +3,15 @@ class FiscalYear < ActiveRecord::Base
 
   belongs_to :company, foreign_key: :yhtio, primary_key: :yhtio
 
-  self.table_name = :tilikaudet
+  self.table_name  = :tilikaudet
   self.primary_key = :tunnus
 
   validates :tilikausi_alku, allow_blank: false, date: true
   validates :tilikausi_loppu, allow_blank: false, date: true
+
+  # For future reference: avaava_tase is a relationship column to lasku
+  # This means FiscalYear has one lasku
+  # Avaava tase functionality is not implemented yet.
 
   def period
     tilikausi_alku..tilikausi_loppu
