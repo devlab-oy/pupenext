703440757972fda8f27594b7b3f627370d51fd27
Joonas Harjukallio <joonas@devlab.fi>
2015-04-29 13:55:40 +0300

    Refactored printers

--- app/controllers/administration/printers_controller.rb
+++ app/controllers/administration/printers_controller.rb
@@ -1,25 +1,20 @@
 class Administration::PrintersController < AdministrationController
-  # GET /printers
   def index
-    @printers = current_company
-      .printers
+    @printers = Printer
       .search_like(search_params)
       .order(order_params)
   end
 
-  # GET /printers/1
   def show
     render :edit
   end
 
-  # GET /printers/new
   def new
-    @printer = current_company.printers.build
+    @printer = Printer.new
   end
 
-  # POST /printers
   def create
-    @printer = current_company.printers.build(printer_params)
+    @printer = Printer.new(printer_params)
 
     if @printer.save_by current_user
       redirect_to printers_path, notice: "Kirjoitin luotiin onnistuneesti"
@@ -28,11 +23,9 @@ class Administration::PrintersController < AdministrationController
     end
   end
 
-  # GET /printers/1/edit
   def edit
   end
 
-  # PATCH/PUT /printers/1
   def update
     if @printer.update_by(printer_params, current_user)
       redirect_to printers_path, notice: "Kirjoitin päivitettiin onnistuneesti"
@@ -67,7 +60,7 @@ class Administration::PrintersController < AdministrationController
     end
 
     def find_resource
-      @printer = current_company.printers.find(params[:id])
+      @printer = Printer.find(params[:id])
     end
 
     def sortable_columns
