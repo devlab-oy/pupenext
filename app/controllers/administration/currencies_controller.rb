676f5a8acd5dfb44b30af64c915b180c272c4c05
Joonas Harjukallio <joonas@devlab.fi>
2015-04-29 13:10:57 +0300

    Refactor currencies

--- app/controllers/administration/currencies_controller.rb
+++ app/controllers/administration/currencies_controller.rb
@@ -1,26 +1,20 @@
 class Administration::CurrenciesController < AdministrationController
-  # GET /currencies
   def index
-    @currencies = current_company
-      .currencies
+    @currencies = Currency
       .search_like(search_params)
       .order(order_params)
   end
 
-  # GET /currencies/1
   def show
     render :edit
   end
 
-  # GET /currencies/new
   def new
-    @currency = current_company.currencies.build
+    @currency = Currency.new
   end
 
-  # POST /currencies
   def create
-    @currency = current_company.currencies.build
-    @currency.attributes = currency_params
+    @currency = Currency.new(currency_params)
 
     if @currency.save_by current_user
       redirect_to currencies_path, notice: 'Valuutta luotiin onnistuneesti.'
@@ -29,11 +23,9 @@ class Administration::CurrenciesController < AdministrationController
     end
   end
 
-  # GET /currencies/1/edit
   def edit
   end
 
-  # PATCH/PUT /currencies/1
   def update
     if @currency.update_by(currency_params, current_user)
       redirect_to currencies_path, notice: 'Valuutta päivitettiin onnistuneesti.'
@@ -45,7 +37,7 @@ class Administration::CurrenciesController < AdministrationController
   private
 
     def find_resource
-      @currency = current_company.currencies.find(params[:id])
+      @currency = Currency.find(params[:id])
     end
 
     def currency_params
