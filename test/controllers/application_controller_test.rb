7a30d530f3ac48b4c1c7c5dfedd66e26eb3d193c
Joonas Harjukallio <joonas@devlab.fi>
2015-04-29 10:08:51 +0300

    Add test for Company.current

---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+# The purpose of AdministrationControllerTest is to test the common logic behind AdministrationController
+class ApplicationControllerTest < ActionController::TestCase
+  tests Administration::AccountsController
+
+  setup do
+    @user = users(:bob)
+    login users(:bob)
+  end
+
+  test 'test current company' do
+    assert_nil Company.current
+
+    get :index
+
+    assings(:accounts).each do |account|
+      assert_equal @user.company.id, account.company.id
+    end
+  end
+end
