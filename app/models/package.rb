0b49492032337575669ee44ad739700140933af5
Sami Luukkonen <sami@devlab.fi>
2015-04-30 09:51:21 +0300

    code style

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,14 +1,14 @@
 class Package < ActiveRecord::Base
-
   belongs_to :company, foreign_key: :yhtio, primary_key: :yhtio
+
   has_many :keywords, foreign_key: :perhe, primary_key: :tunnus, class_name: 'PackageKeyword'
   has_many :package_codes, foreign_key: :pakkaus, primary_key: :tunnus
 
   validates :pakkaus, presence: true
   validates :pakkauskuvaus, presence: true
 
-  self.table_name  = "pakkaus"
-  self.primary_key = "tunnus"
+  self.table_name  = :pakkaus
+  self.primary_key = :tunnus
 
   def luontiaika
     luontiaika = read_attribute(:luontiaika)
@@ -21,6 +21,4 @@ class Package < ActiveRecord::Base
       ["Kyllä", "K"]
     ]
   end
-
-
 end
