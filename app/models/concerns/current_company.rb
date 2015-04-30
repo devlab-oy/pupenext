f14b02977007d710349d46a2d5114cb70f071bc3
Joonas Harjukallio <joonas@devlab.fi>
2015-04-30 12:41:22 +0300

    Remove documentation

--- app/models/concerns/current_company.rb
+++ app/models/concerns/current_company.rb
@@ -6,18 +6,6 @@ module CurrentCompany
       record.company ||= Company.current if Company.current
     end
 
-    # This works
     default_scope { where(yhtio: Company.current.yhtio) } if Company.current
-  end
-
-  # This doesnt. Nor does any else class method implementation
-  module ClassMethods
-    def default_scope
-      l = lambda do |company|
-        where(company: company)
-      end
-
-      l.call(Company.current) if Company.current
-    end
   end
 end
