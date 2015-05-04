b0bb719046f9ee0cfb11950d3637a3f512cf2f89
Mika Myllynen <giefsufu@gmail.com>
2015-05-04 14:15:02 +0300

    begin balance statements r-logic

--- app/modules/balance_statements_report.rb
+++ app/modules/balance_statements_report.rb
@@ -4,4 +4,10 @@ class BalanceStatementsReport
   def initialize(company_id)
     self.current_company = Company.find_by(tunnus: company_id)
   end
+
+  def vouchers_by_sumlevel(sum_level)
+    accounts = current_company.accounts.where(evl_taso: sum_level.taso).map(&:tilino).uniq
+
+    rows = current_company.voucher_rows.where(tilino: accounts).count
+  end
 end
