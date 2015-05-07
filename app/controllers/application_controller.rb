6a0ebc134a60671d9c5df9d7fe5c4a2592cba1c8
Joonas Harjukallio <joonas@devlab.fi>
2015-05-07 15:09:37 +0300

    Set current company in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authorize
+  before_action :set_current_company
   before_action :set_locale
   before_action :access_control
 
@@ -13,11 +14,11 @@ class ApplicationController < ActionController::Base
   helper_method :t
 
   def current_user
-    @current_user ||= User.find_by_session(cookies[:pupesoft_session])
+    @current_user ||= User.unscoped.find_by_session(cookies[:pupesoft_session])
   end
 
-  def current_company
-    @current_company ||= current_user.company
+  def set_current_company
+    Company.current = current_user.company
   end
 
   def read_access?
@@ -45,7 +46,7 @@ class ApplicationController < ActionController::Base
       I18n.locale = current_user.locale || I18n.default_locale
     end
 
-  private
+    private
 
     def request_path
       # return first resource from request, our access control is based on it
