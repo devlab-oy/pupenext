d510675787cac198b7ed50d23c8e77565cf52a2d
tiere <tommi@devlab.fi>
2015-05-11 09:56:52 +0300

    Better formatting

--- app/assets/javascripts/administration/dictionaries.js
+++ app/assets/javascripts/administration/dictionaries.js
@@ -1,10 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(function () {
+$(function() {
   "use strict";
 
-  $("#dictionary_submit").on("click", function (e) {
+  $("#dictionary_submit").on("click", function(e) {
     e.preventDefault();
     $("#dictionary_form").submit();
   });
