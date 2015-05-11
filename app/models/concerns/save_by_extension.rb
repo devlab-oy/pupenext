fa93a9dea916874079105cfb89c4f1627c3183da
Joonas Harjukallio <joonas@devlab.fi>
2015-05-11 12:31:26 +0300

    Indent

--- app/models/concerns/save_by_extension.rb
+++ app/models/concerns/save_by_extension.rb
@@ -33,35 +33,35 @@ module SaveByExtension
 
   private
 
-  def touch_modern_columns(user)
-    self.created_by  = user.kuka unless self.persisted? && self.created_by.present?
-    self.modified_by = user.kuka
-  end
-
-  def touch_antique_columns(user)
-    self.laatija  = user.kuka unless self.persisted? && self.laatija.present?
-    self.muuttaja = user.kuka if self.respond_to?(:muuttaja=)
-  end
-
-  def set_legacy_timestamps
-    if self.respond_to?(:luontiaika=) && self.respond_to?(:muutospvm=)
-      self.luontiaika = Time.now unless self.persisted? && self.luontiaika.present?
-      self.muutospvm  = Time.now
+    def touch_modern_columns(user)
+      self.created_by  = user.kuka unless self.persisted? && self.created_by.present?
+      self.modified_by = user.kuka
     end
-  end
 
-  def table_does_not_have_company?
-    %w{
-        fixed_assets_commodities
-        fixed_assets_commodity_rows
-        git_paivitykset
-        git_pulkkarit
-        karhu_lasku
-        maat
-        schema_migrations
-        taric_veroperusteet
-        tiliointi
-        valuu_historia
-      }.include? self.class.table_name
-  end
+    def touch_antique_columns(user)
+      self.laatija  = user.kuka unless self.persisted? && self.laatija.present?
+      self.muuttaja = user.kuka if self.respond_to?(:muuttaja=)
+    end
+
+    def set_legacy_timestamps
+      if self.respond_to?(:luontiaika=) && self.respond_to?(:muutospvm=)
+        self.luontiaika = Time.now unless self.persisted? && self.luontiaika.present?
+        self.muutospvm  = Time.now
+      end
+    end
+
+    def table_does_not_have_company?
+      %w{
+          fixed_assets_commodities
+          fixed_assets_commodity_rows
+          git_paivitykset
+          git_pulkkarit
+          karhu_lasku
+          maat
+          schema_migrations
+          taric_veroperusteet
+          tiliointi
+          valuu_historia
+        }.include? self.class.table_name
+    end
 end
