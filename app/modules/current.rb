bbf5058d8ef3e25363f799b738c32b2e79b61e25
Joonas Harjukallio <joonas@devlab.fi>
2015-05-12 11:04:52 +0300

    Remove unused methods

--- app/modules/current.rb
+++ app/modules/current.rb
@@ -6,12 +6,4 @@ module Current
   def self.company
     RequestStore.store[:current_company]
   end
-
-  def self.user=(user)
-    RequestStore.store[:current_user] = user
-  end
-
-  def self.user
-    RequestStore.store[:current_user]
-  end
 end
