7d71259d163c1dd440ea1be9232b88a658dd6d54
tiere <tommi@devlab.fi>
2015-05-13 09:24:45 +0300

    Remove unnecessary after_initialize callback since default scope handles that

--- app/models/concerns/current_company.rb
+++ app/models/concerns/current_company.rb
@@ -2,10 +2,6 @@ module CurrentCompany
   extend ActiveSupport::Concern
 
   included do
-    after_initialize do |record|
-      record.company ||= Current.company
-    end
-
     default_scope -> do
       raise CurrentCompanyNil if Current.company.nil?
       where(company: Current.company)
