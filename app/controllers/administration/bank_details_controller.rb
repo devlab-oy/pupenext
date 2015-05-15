032f4217ca2063b6f319e156078d40b3a8a2ef1c
tiere <tommi@devlab.fi>
2015-05-15 11:29:37 +0300

    Refactor to use the new current company

--- app/controllers/administration/bank_details_controller.rb
+++ app/controllers/administration/bank_details_controller.rb
@@ -1,10 +1,10 @@
 class Administration::BankDetailsController < AdministrationController
   def index
-    @bank_details = current_company.bank_details.search_like(search_params).order(order_params)
+    @bank_details = BankDetail.search_like(search_params).order(order_params)
   end
 
   def create
-    @bank_detail = current_company.bank_details.build(bank_detail_attributes)
+    @bank_detail = BankDetail.new(bank_detail_attributes)
 
     if @bank_detail.save_by current_user
       redirect_to bank_details_url
@@ -22,21 +22,30 @@ class Administration::BankDetailsController < AdministrationController
   end
 
   def new
-    @bank_detail = current_company.bank_details.new
+    @bank_detail = BankDetail.new
   end
 
   private
 
     def bank_detail_attributes
-      params
-        .require(:bank_detail)
-        .permit(:nimitys, :pankkinimi1, :pankkitili1, :pankkiiban1, :pankkiswift1, :pankkinimi2,
-                :pankkitili2, :pankkiiban2, :pankkiswift2, :pankkinimi3, :pankkitili3, :pankkiiban3,
-                :pankkiswift3, :viite)
+      params.require(:bank_detail).permit(:nimitys,
+                                          :pankkinimi1,
+                                          :pankkitili1,
+                                          :pankkiiban1,
+                                          :pankkiswift1,
+                                          :pankkinimi2,
+                                          :pankkitili2,
+                                          :pankkiiban2,
+                                          :pankkiswift2,
+                                          :pankkinimi3,
+                                          :pankkitili3,
+                                          :pankkiiban3,
+                                          :pankkiswift3,
+                                          :viite)
     end
 
     def find_resource
-      @bank_detail = current_company.bank_details.find(params[:id])
+      @bank_detail = BankDetail.find(params[:id])
     end
 
     def sortable_columns
