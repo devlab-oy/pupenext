21d2e2f062196c5a56bdafef37823c4a474b5f76
tiere <tommi@devlab.fi>
2015-05-18 13:15:49 +0300

    Allow searching and ordering in freight contracts controller

--- app/controllers/administration/freight_contracts_controller.rb
+++ app/controllers/administration/freight_contracts_controller.rb
@@ -1,5 +1,19 @@
 class Administration::FreightContractsController < AdministrationController
   def index
-    @freight_contracts = FreightContract.ordered.limited
+    @freight_contracts = FreightContract
+                           .includes(:customer)
+                           .limited
+                           .search_like(search_params)
+                           .order(order_params)
   end
+
+  private
+
+    def sortable_columns
+      [:asiakas, :toimitustapa, :rahtisopimus, :selite]
+    end
+
+    def searchable_columns
+      sortable_columns
+    end
 end
