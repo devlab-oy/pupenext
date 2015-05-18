7da11f44966ba22a04c452e0cba4a7139bf2232b
Mika Myllynen <giefsufu@gmail.com>
2015-05-18 17:02:07 +0300

    use current.company

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,16 +1,16 @@
 class ReportsController < ApplicationController
   def depreciations_balance_sheet
     # Tase-erittely sumu-poistoin
-    @report = DepreciationsBalanceSheetReport.new(current_company)
+    @report = DepreciationsBalanceSheetReport.new(Current.company)
   end
 
   def depreciation_difference
     # Poistoeroraportti
-    @report = DepreciationDifferenceReport.new(current_company)
+    @report = DepreciationDifferenceReport.new(Current.company)
   end
 
   def balance_statements
     # Tilinpäätöksen liitetiedot
-    @report = BalanceStatementsReport.new(current_company)
+    @report = BalanceStatementsReport.new(Current.company)
   end
 end
