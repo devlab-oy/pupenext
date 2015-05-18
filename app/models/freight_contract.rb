33ef907bbe4ec40fea91d2a4b2b5e508ef7c55e4
tiere <tommi@devlab.fi>
2015-05-18 10:19:18 +0300

    Add validations and associations to freight contract

--- app/models/freight_contract.rb
+++ app/models/freight_contract.rb
@@ -1,4 +1,9 @@
 class FreightContract < BaseModel
+  belongs_to :customer, foreign_key: :asiakas, primary_key: :tunnus
+
+  validates :rahtisopimus, presence: true
+  validates :customer, presence: true
+
   self.table_name = :rahtisopimukset
   self.primary_key = :tunnus
 end
