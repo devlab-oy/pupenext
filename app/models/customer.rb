3dfe0d8728db0ea35023ad52da40b28d935f033d
tiere <tommi@devlab.fi>
2015-05-18 13:24:52 +0300

    Change default scope to named scope in customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < BaseModel
   belongs_to :terms_of_payment, foreign_key: :maksuehto, primary_key: :tunnus
   has_many :freight_contracts, foreign_key: :asiakas, primary_key: :tunnus
 
-  default_scope { where.not(laji: %w(P R)) }
+  scope :active, -> { where.not(laji: %w(P R)) }
 
   self.table_name = :asiakas
   self.primary_key = :tunnus
