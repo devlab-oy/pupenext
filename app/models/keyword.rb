577e35248e546c0ad3b29b158bb2bbb16b9a86cb
Sami Luukkonen <sami@devlab.fi>
2015-05-28 12:23:19 +0300

    keyword methods

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -4,6 +4,26 @@ class Keyword < BaseModel
       .where("selitetark_2 IN (?) OR laji = 'alv'", Location.countries)
   end
 
+  def self.waybills
+    where(laji: "RAHTIKIRJA")
+  end
+
+  def self.mode_of_transports
+    where(laji: "KM")
+  end
+
+  def self.nature_of_transactions
+    where(laji: "KT")
+  end
+
+  def self.customs
+    where(laji: "TULLI")
+  end
+
+  def self.sorting_points
+    where(laji: "TOIMTAPALP")
+  end
+
   self.table_name = :avainsana
   self.primary_key = :tunnus
 
