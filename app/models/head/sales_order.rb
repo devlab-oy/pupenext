34a3d6778c9e9aa190238ca366fef95db008fc82
Joni Kanerva <joni@devlab.fi>
2015-07-28 13:35:10 +0300

    only sales order can be not delivered

--- app/models/head/sales_order.rb
+++ app/models/head/sales_order.rb
@@ -1,6 +1,8 @@
 class Head::SalesOrder < Head
   validates :tila, inclusion: { in: ['L'] }
 
+  scope :not_delivered, -> { where(alatila: %w(A C)) }
+
   # Rails requires sti_name method to return type column (tyyppi) value
   def self.sti_name
     "L"
