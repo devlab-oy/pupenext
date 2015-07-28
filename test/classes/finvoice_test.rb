7bb1c300a0375750c38f3926a4124849991ac17f
Joni Kanerva <joni@devlab.fi>
2015-07-28 15:08:39 +0300

    finvoice test skeleton

---
+++ test/classes/finvoice_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class FinvoiceTest < ActiveSupport::TestCase
+  setup do
+    @invoice = heads(:si_one)
+  end
+
+  test 'should initialize class with invoice' do
+    assert Finvoice.new(invoice_id: @invoice.id)
+    assert_raises(ActiveRecord::RecordNotFound) { Finvoice.new(invoice_id: nil) }
+  end
+
+  test 'should generate finvoice xml' do
+    example = File.read Rails.root.join('test', 'assets', 'example_finvoice.xml')
+    finvoice = Finvoice.new(invoice_id: @invoice.id)
+
+    assert_equal example, finvoice.to_xml
+  end
+end
