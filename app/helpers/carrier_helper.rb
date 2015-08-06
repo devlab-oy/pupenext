5fcc0e5cb81b3a87e1efd6f8040d540cd3230f05
Joni Kanerva <joni@devlab.fi>
2015-08-06 15:13:11 +0300

    use helper to draw options

---
+++ app/helpers/carrier_helper.rb
@@ -0,0 +1,8 @@
+module CarrierHelper
+  def neutraali_options
+    [
+      [t("administration.carriers.neutraali_options.true"), true],
+      [t("administration.carriers.neutraali_options.false"), false],
+    ]
+  end
+end
