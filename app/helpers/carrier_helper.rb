371d396a62765495dd9787f76b8aa9e20e72d9ac
Joni Kanerva <joni@devlab.fi>
2015-08-07 13:32:40 +0300

    remove dropdown

--- app/helpers/carrier_helper.rb
+++
@@ -1,8 +0,0 @@
-module CarrierHelper
-  def neutraali_options
-    [
-      [t("administration.carriers.neutraali_options.true"), 'o'],
-      [t("administration.carriers.neutraali_options.false"), ''],
-    ]
-  end
-end
