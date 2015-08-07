da562653119357d4b1a684d5ec4be2f7cfcc1471
Joni Kanerva <joni@devlab.fi>
2015-08-07 14:35:17 +0300

    don't load locale in production

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,7 +1,9 @@
-require 'localeapp/rails'
+unless Rails.env.production?
+  require 'localeapp/rails'
 
-Localeapp.configure do |config|
-  config.api_key = Rails.application.secrets.localeapp_api_key
-  config.sending_environments = [] # Never send missing keys
-  config.polling_environments = [] # Never poll for missing keys
+  Localeapp.configure do |config|
+    config.api_key = Rails.application.secrets.localeapp_api_key
+    config.sending_environments = [] # Never send missing keys
+    config.polling_environments = [] # Never poll for missing keys
+  end
 end
