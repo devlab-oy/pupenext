54a26cb2718e5d98be5fd5217392703d0483da91
Joni Kanerva <joni@devlab.fi>
2015-08-13 13:32:33 +0300

    remove incorrect model

--- app/models/printers.rb
+++
@@ -1,7 +0,0 @@
-class Printers < BaseModel
-  belongs_to :company, foreign_key: :yhtio, primary_key: :yhtio
-
-  # Map old database schema table to Qualifier class
-  self.table_name  = :kirjoittimet
-  self.primary_key = :tunnus
-end
