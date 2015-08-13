698827bfcbe8c706579d5ad040e01c41ef337490
Joni Kanerva <joni@devlab.fi>
2015-08-13 13:26:17 +0300

    controller cleanup * remove new, use edit instead * translate messages * remove methods that belong to helpers * codestyle

--- app/controllers/administration/packing_areas_controller.rb
+++ app/controllers/administration/packing_areas_controller.rb
@@ -1,14 +1,10 @@
 class Administration::PackingAreasController < AdministrationController
-  before_action :find_printers, only: [:new, :show, :create, :edit, :update]
-  before_action :find_storages, only: [:new, :show, :create, :edit, :update]
-  helper_method :get_storage
-
   def index
     @packing_areas = PackingArea.search_like(search_params).order(order_params)
   end
 
   def show
-     render :edit
+    render :edit
   end
 
   def edit
@@ -16,50 +12,35 @@ class Administration::PackingAreasController < AdministrationController
 
   def new
     @packing_area = PackingArea.new
-    @packing_area.varasto = nil
-    @packing_area.prio = nil
-    @packing_area.pakkaamon_prio = nil
   end
 
   def create
     @packing_area = PackingArea.new(packing_area_params)
 
     if @packing_area.save_by current_user
-      redirect_to packing_areas_path, notice: t("Pakkaamo luotiin onnistuneesti")
+      redirect_to packing_areas_path, notice: t('.create_success')
     else
-      render action: 'new'
+      render :edit
     end
   end
 
   def update
     if @packing_area.update_by(packing_area_params, current_user)
-      redirect_to packing_areas_path, notice: t("Pakkaamo päivitettiin onnistuneesti")
+      redirect_to packing_areas_path, notice: t('.update_success')
     else
-      render action: 'edit'
+      render :edit
     end
   end
 
   def destroy
     PackingArea.destroy(params[:id])
-    redirect_to packing_areas_path, notice: t("Pakkaamo poistettiin onnistuneesti")
+    redirect_to packing_areas_path, notice: t('.destroy_success')
   end
 
   private
 
-    def get_storage(storage_id)
-      Storage.find_by_tunnus(storage_id).nimitys
-    end
-
     def find_resource
       @packing_area = PackingArea.find(params[:id])
-    end
-
-    def find_printers
-      @printers = Printer.where.not(komento: "EDI").order(:kirjoitin)
-    end
-
-    def find_storages
-      @storages = Storage.where.not(tyyppi: "P").order(:nimitys)
     end
 
     def searchable_columns
@@ -77,19 +58,19 @@ class Administration::PackingAreasController < AdministrationController
     end
 
     def packing_area_params
-        params.require(:packing_area).permit(
-          :nimi,
-          :lokero,
-          :prio,
-          :pakkaamon_prio,
-          :varasto,
-          :printteri0,
-          :printteri1,
-          :printteri2,
-          :printteri3,
-          :printteri4,
-          :printteri6,
-          :printteri7
-        )
+      params.require(:packing_area).permit(
+        :nimi,
+        :lokero,
+        :prio,
+        :pakkaamon_prio,
+        :varasto,
+        :printteri0,
+        :printteri1,
+        :printteri2,
+        :printteri3,
+        :printteri4,
+        :printteri6,
+        :printteri7
+      )
     end
 end
