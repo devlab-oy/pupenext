d846c75e042fdbc427f8ed7e9988e7efe0e82e66
Joni Kanerva <joni@devlab.fi>
2015-08-14 10:55:31 +0300

    fix incorrect relations

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,8 +1,4 @@
 class Country < ActiveRecord::Base
-
-  belongs_to :company, foreign_key: :yhtio, primary_key: :yhtio
-  has_one :language
-
-  self.table_name  = "maat"
-  self.primary_key = "tunnus"
+  self.table_name  = :maat
+  self.primary_key = :tunnus
 end
