c1980e9fce5b6f0a53fa3d86e512c037d0300e85
Joni Kanerva <joni@devlab.fi>
2015-08-14 10:54:38 +0300

    remove wrong model

--- app/models/language.rb
+++
@@ -1,8 +0,0 @@
-class Language < ActiveRecord::Base
-
-  belongs_to :company, foreign_key: :yhtio, primary_key: :yhtio
-  has_one :country
-
-  self.table_name  = "sanakirja"
-  self.primary_key = "tunnus"
-end
