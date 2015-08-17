743c6ddfbabd611e1180619750b3a3b9ac291d0c
Joni Kanerva <joni@devlab.fi>
2015-08-17 16:15:46 +0300

    use STI for keywords

---
+++ app/models/keyword/foreign_vat.rb
@@ -0,0 +1,8 @@
+class Keyword::ForeignVat < Keyword
+  validates :selitetark, presence: true
+
+  # Rails requires sti_name method to return type column (laji) value
+  def self.sti_name
+    'ALVULK'
+  end
+end
