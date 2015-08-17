9b2ddd15fc6a19dcf29ad271561dfcd65cb27883
Joni Kanerva <joni@devlab.fi>
2015-08-17 15:58:26 +0300

    add foreign vats to options

--- app/helpers/keyword_helper.rb
+++ app/helpers/keyword_helper.rb
@@ -1,10 +1,19 @@
 module KeywordHelper
   def oletus_alv_options
-    Keyword::Vat.all.map do |k|
+    vat = Keyword::Vat.all.map do |k|
       [
         number_to_percentage(k.selite, strip_insignificant_zeros: true),
         k.selite
       ]
     end
+
+    Keyword::ForeignVat.where(selitetark_2: Location.countries).map do |k|
+      vat << [
+        number_to_percentage(k.selite, strip_insignificant_zeros: true),
+        k.selite
+      ]
+    end
+
+    vat.uniq.sort
   end
 end
