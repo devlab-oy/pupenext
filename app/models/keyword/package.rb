a54b8ae133c7a9ff5a37c07e386de2f0daae7f87
Joni Kanerva <joni@devlab.fi>
2015-08-17 15:13:40 +0300

    don't override model_name

--- app/models/keyword/package.rb
+++ app/models/keyword/package.rb
@@ -3,12 +3,6 @@ class Keyword::Package < Keyword
 
   # Rails requires sti_name method to return type column (tyyppi) value
   def self.sti_name
-    "PAKKAUSKV"
-  end
-
-  # Rails figures out paths from the model name. User model has users_path etc.
-  # With STI we want to use same name for each child. Thats why we override model_name
-  def self.model_name
-    Keyword.model_name
+    'PAKKAUSKV'
   end
 end
