c3ace3c69d880d4f7815c4d0140ee4bf452eb489
Sami Luukkonen <sami@devlab.fi>
2015-08-18 11:16:02 +0300

    add paid and unpaid scopes

--- app/models/head.rb
+++ app/models/head.rb
@@ -8,6 +8,9 @@ class Head < BaseModel
   belongs_to :terms_of_payment, foreign_key: :maksuehto, primary_key: :tunnus
   has_many :accounting_rows, class_name: 'Head::VoucherRow', foreign_key: :ltunnus
 
+  scope :paid, -> { where.not(mapvm: '0000-00-00') }
+  scope :unpaid, -> { where(mapvm: '0000-00-00') }
+
   def self.default_child_instance
     child_class 'N'
   end
