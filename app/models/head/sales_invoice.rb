3eabc101f07c610767eeb5ba831f11ab83bdd98b
Sami Luukkonen <sami@devlab.fi>
2015-08-18 11:15:39 +0300

    add sent scope

--- app/models/head/sales_invoice.rb
+++ app/models/head/sales_invoice.rb
@@ -1,6 +1,8 @@
 class Head::SalesInvoice < Head
   validates :tila, inclusion: { in: ['U'] }
 
+  scope :sent, -> { where(alatila: :X) }
+
   # Rails requires sti_name method to return type column (tyyppi) value
   def self.sti_name
     "U"
