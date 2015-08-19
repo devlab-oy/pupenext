56ece9ad5338e4eada8d06fbbcc34b1875d4e723
Joni Kanerva <joni@devlab.fi>
2015-08-19 12:49:06 +0300

    assert keywords are valid

--- test/models/keyword_test.rb
+++ test/models/keyword_test.rb
@@ -4,15 +4,17 @@ class KeywordTest < ActiveSupport::TestCase
   setup do
     @vat = keywords(:vat)
     @foreign_vat = keywords(:foreign_vat)
+    @top_translation = keywords(:top_locale_se)
   end
 
   test "fixtures are valid" do
     assert @vat.valid?, @vat.errors.full_messages
     assert @foreign_vat.valid?, @foreign_vat.errors.full_messages
+    assert @top_translation.valid?, @top_translation.errors.full_messages
+    assert keywords(:top_locale_en).valid?
   end
 
   test 'top translation relation' do
-    top_translation = keywords(:top_locale_se)
-    assert TermsOfPayment, top_translation.terms_of_payment.class
+    assert TermsOfPayment, @top_translation.terms_of_payment.class
   end
 end
