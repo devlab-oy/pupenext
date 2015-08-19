45bdc13e5eff9b8461494273881ceba8fb6be257
Joni Kanerva <joni@devlab.fi>
2015-08-19 12:51:50 +0300

    kieli and selitetark are required

--- app/models/keyword/terms_of_payment_translation.rb
+++ app/models/keyword/terms_of_payment_translation.rb
@@ -1,6 +1,8 @@
 class Keyword::TermsOfPaymentTranslation < Keyword
   belongs_to :terms_of_payment, foreign_key: :selite, primary_key: :tunnus
 
+  validates :kieli, :selitetark, presence: true
+
   # Rails requires sti_name method to return type column (laji) value
   def self.sti_name
     'MAKSUEHTOKV'
