61cce444ac4965df6f3af83136f44cd2091be0c1
Sami Luukkonen <sami@devlab.fi>
2015-08-20 13:02:47 +0300

    add validation

--- app/models/keyword/waybill.rb
+++ app/models/keyword/waybill.rb
@@ -1,4 +1,6 @@
 class Keyword::Waybill < Keyword
+  validates :selitetark, presence: true
+
   # Rails requires sti_name method to return type column (laji) value
   def self.sti_name
     'RAHTIKIRJA'
