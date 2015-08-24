6ab80d512fcdd1c735baaad770f4ee1554cacfe3
Joni Kanerva <joni@devlab.fi>
2015-08-24 12:31:37 +0300

    move supported locales into dictionary model

--- app/models/keyword/package_translation.rb
+++ app/models/keyword/package_translation.rb
@@ -2,7 +2,7 @@ class Keyword::PackageTranslation < Keyword
   belongs_to :package, foreign_key: :selite
 
   validates :selitetark, presence: true
-  validates :kieli, inclusion: { in: %w{ee en no se de dk ru} }
+  validates :kieli, inclusion: { in: Dictionary.locales }
   validates :kieli, uniqueness: { scope: [:yhtio, :selite] }
 
   # Rails requires sti_name method to return type column (laji) value
--- app/models/keyword/terms_of_payment_translation.rb
+++ app/models/keyword/terms_of_payment_translation.rb
@@ -2,7 +2,7 @@ class Keyword::TermsOfPaymentTranslation < Keyword
   belongs_to :terms_of_payment, foreign_key: :selite
 
   validates :selitetark, presence: true
-  validates :kieli, inclusion: { in: %w{ee en no se de dk ru} }
+  validates :kieli, inclusion: { in: Dictionary.locales }
   validates :kieli, uniqueness: { scope: [:yhtio, :selite] }
 
   # Rails requires sti_name method to return type column (laji) value
