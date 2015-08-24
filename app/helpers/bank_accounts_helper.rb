871cdf832cc2c58415ddeac4ca68f23ca9efd6e5
Joni Kanerva <joni@devlab.fi>
2015-08-24 18:23:13 +0300

    move methods to correct helpers

--- app/helpers/bank_accounts_helper.rb
+++ app/helpers/bank_accounts_helper.rb
@@ -1,24 +1,4 @@
 module BankAccountsHelper
-  def currency_options
-    Currency.order(:jarjestys).pluck(:nimi)
-  end
-
-  def hyvaksyja_options
-    User.select(:kuka, :nimi).order(:nimi)
-  end
-
-  def kustannuspaikka_options
-    Qualifier::CostCenter.order('koodi+0, koodi, nimi')
-  end
-
-  def kohde_options
-    Qualifier::Target.order('koodi+0, koodi, nimi')
-  end
-
-  def projekti_options
-    Qualifier::Project.order('koodi+0, koodi, nimi')
-  end
-
   def tili_kaytossa_options
     [
       [ t('administration.bank_accounts.kaytossa_options.active'),   :active   ],
