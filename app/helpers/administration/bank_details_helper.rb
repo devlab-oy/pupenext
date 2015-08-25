b0e4eabd8c172e60cc587b60998a8538ba5f7250
Joni Kanerva <joni@devlab.fi>
2015-08-25 13:48:45 +0300

    move options to helper

--- app/helpers/administration/bank_details_helper.rb
+++ app/helpers/administration/bank_details_helper.rb
@@ -1,5 +1,12 @@
 module Administration::BankDetailsHelper
   def nimitys_teksti(nimitys)
-    nimitys.present? ? nimitys : "*#{t("tyhjä")}*"
+    nimitys.present? ? nimitys : "*#{t('.empty')}*"
+  end
+
+  def viite_options
+    [
+      [ t('.finnish'), ''   ],
+      [ t('.swedish'), 'SE' ],
+    ]
   end
 end
--- app/models/bank_detail.rb
+++ app/models/bank_detail.rb
@@ -1,17 +1,9 @@
 class BankDetail < BaseModel
   include Searchable
-  extend Translatable
 
   validates :nimitys, presence: true
   validates :viite, inclusion: { in: %w(SE) }, allow_blank: true
 
   self.table_name = :pankkiyhteystiedot
   self.primary_key = :tunnus
-  self.record_timestamps = false
-
-  class << self
-    def viite_options
-      [[t("Suomi"), ""], [t("Ruotsi"), "SE"]]
-    end
-  end
 end
