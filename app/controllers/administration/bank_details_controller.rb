68296e9f08317a19c08ee103e7cddae57aec73c7
Joni Kanerva <joni@devlab.fi>
2015-08-25 13:48:29 +0300

    codestyle

--- app/controllers/administration/bank_details_controller.rb
+++ app/controllers/administration/bank_details_controller.rb
@@ -4,17 +4,17 @@ class Administration::BankDetailsController < AdministrationController
   end
 
   def create
-    @bank_detail = BankDetail.new(bank_detail_attributes)
+    @bank_detail = BankDetail.new bank_detail_attributes
 
-    if @bank_detail.save_by current_user
+    if @bank_detail.save
       redirect_to bank_details_url
     else
-      render :new
+      render :edit
     end
   end
 
   def update
-    if @bank_detail.update_by(bank_detail_attributes, current_user)
+    if @bank_detail.update bank_detail_attributes
       redirect_to bank_details_url
     else
       render :edit
@@ -23,29 +23,32 @@ class Administration::BankDetailsController < AdministrationController
 
   def new
     @bank_detail = BankDetail.new
+    render :edit
   end
 
   private
 
     def bank_detail_attributes
-      params.require(:bank_detail).permit(:nimitys,
-                                          :pankkinimi1,
-                                          :pankkitili1,
-                                          :pankkiiban1,
-                                          :pankkiswift1,
-                                          :pankkinimi2,
-                                          :pankkitili2,
-                                          :pankkiiban2,
-                                          :pankkiswift2,
-                                          :pankkinimi3,
-                                          :pankkitili3,
-                                          :pankkiiban3,
-                                          :pankkiswift3,
-                                          :viite)
+      params.require(:bank_detail).permit(
+        :nimitys,
+        :pankkiiban1,
+        :pankkiiban2,
+        :pankkiiban3,
+        :pankkinimi1,
+        :pankkinimi2,
+        :pankkinimi3,
+        :pankkiswift1,
+        :pankkiswift2,
+        :pankkiswift3,
+        :pankkitili1,
+        :pankkitili2,
+        :pankkitili3,
+        :viite,
+      )
     end
 
     def find_resource
-      @bank_detail = BankDetail.find(params[:id])
+      @bank_detail = BankDetail.find params[:id]
     end
 
     def sortable_columns
