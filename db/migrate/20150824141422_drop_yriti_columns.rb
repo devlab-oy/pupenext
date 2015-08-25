211907070e052c551d525f5056fd1d2461a61628
Joni Kanerva <joni@devlab.fi>
2015-08-25 12:45:32 +0300

    remove bank_account_id for now

--- db/migrate/20150824141422_drop_yriti_columns.rb
+++ db/migrate/20150824141422_drop_yriti_columns.rb
@@ -4,11 +4,6 @@ class DropYritiColumns < ActiveRecord::Migration
                            :salattukerta, :generointiavain, :asiakas, :pankki, :asiakastarkenne,
                            :pankkitarkenne, :nro, :kayttoavain
 
-    add_column :factoring,     :bank_account_id, :integer, after: :yhtio
-    add_column :suoritus,      :bank_account_id, :integer, after: :yhtio
-    add_column :tiliotedata,   :bank_account_id, :integer, after: :yhtio
-    add_column :tiliotesaanto, :bank_account_id, :integer, after: :yhtio
-
     add_index :yriti, [:yhtio, :iban]
 
     Company.find_each do |company|
@@ -34,11 +29,6 @@ class DropYritiColumns < ActiveRecord::Migration
     add_column :yriti, :pankkitarkenne,  :string,  after: :yhtio
     add_column :yriti, :nro,             :integer, after: :yhtio
     add_column :yriti, :kayttoavain,     :text,    after: :yhtio
-
-    remove_column :factoring,     :bank_account_id
-    remove_column :suoritus,      :bank_account_id
-    remove_column :tiliotedata,   :bank_account_id
-    remove_column :tiliotesaanto, :bank_account_id
 
     Company.find_each do |company|
       Current.company = company.yhtio
