8901960d966a4b6121df18c3edb018c40218d289
Joni Kanerva <joni@devlab.fi>
2015-08-26 09:58:13 +0300

    no need to specify primary key if it's not different

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < BaseModel
-  belongs_to :terms_of_payment, foreign_key: :maksuehto, primary_key: :tunnus
-  has_many :freight_contracts, foreign_key: :asiakas, primary_key: :tunnus
+  belongs_to :terms_of_payment, foreign_key: :maksuehto
+  has_many :freight_contracts, foreign_key: :asiakas
 
   scope :active, -> { where.not(laji: %w(P R)) }
 
