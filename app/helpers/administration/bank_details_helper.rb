22ae5d756c796504620051cea7a48d4e0e47bf0d
Joni Kanerva <joni@devlab.fi>
2015-08-26 09:04:51 +0300

    fix translations

--- app/helpers/administration/bank_details_helper.rb
+++ app/helpers/administration/bank_details_helper.rb
@@ -1,12 +1,12 @@
 module Administration::BankDetailsHelper
   def nimitys_teksti(nimitys)
-    nimitys.present? ? nimitys : "*#{t('.empty')}*"
+    nimitys.present? ? nimitys : "*#{t('administration.bank_details_helper.empty')}*"
   end
 
   def viite_options
     [
-      [ t('.finnish'), ''   ],
-      [ t('.swedish'), 'SE' ],
+      [ t('administration.bank_details_helper.finnish'), ''   ],
+      [ t('administration.bank_details_helper.swedish'), 'SE' ],
     ]
   end
 end
