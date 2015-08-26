0bea96bb56592a27f544c89a75c4c454f985e1b0
Joni Kanerva <joni@devlab.fi>
2015-08-26 16:46:24 +0300

    columns for table

--- app/helpers/custom_attribute_helper.rb
+++ app/helpers/custom_attribute_helper.rb
@@ -3,4 +3,9 @@ module CustomAttributeHelper
     Keyword::CustomAttribute.select(:set_name)
       .distinct.order(:set_name).map(&:set_name).reject(&:empty?)
   end
+
+  def attributes_for_table(table)
+    columns = ActiveRecord::Base.connection.columns table.to_s
+    columns.map &:name
+  end
 end
