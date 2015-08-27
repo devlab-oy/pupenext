4f784da7cd55ba9bd128aa49c95e414323526e7a
Joni Kanerva <joni@devlab.fi>
2015-08-27 14:19:36 +0300

    add helper for tables

--- app/helpers/custom_attribute_helper.rb
+++ app/helpers/custom_attribute_helper.rb
@@ -1,7 +1,17 @@
 module CustomAttributeHelper
   def custom_attribute_set_options
-    Keyword::CustomAttribute.select(:set_name)
-      .distinct.order(:set_name).map(&:set_name).reject(&:empty?)
+    Keyword::CustomAttribute.where.not(set_name: '').map do |a|
+      [ "#{a.table.capitalize} - #{a.set_name}", "#{a.table}/#{a.set_name}" ]
+    end.uniq.sort
+  end
+
+  def custom_attributes_tables_options
+    # List of table names available for custom attributes
+    [
+      [ Customer.model_name.human, 'asiakas' ],
+      [ 'Tuote', 'tuote' ],
+      [ 'Toimittaja', 'toimi' ],
+    ]
   end
 
   def attributes_for_table(table)
