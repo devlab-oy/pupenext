6873375ab720fee9e193cd2e15e2127a16434bcc
Joni Kanerva <joni@devlab.fi>
2015-08-27 14:21:56 +0300

    if we pass parameters to index we're redirected

--- app/controllers/administration/custom_attributes_controller.rb
+++ app/controllers/administration/custom_attributes_controller.rb
@@ -1,13 +1,31 @@
 class Administration::CustomAttributesController < AdministrationController
-  before_action :search_set, only: :index
+  before_action :redirect_to_show_set, only: :index
 
   def index
   end
 
+  def show_set
+    @attribute_set = Keyword::CustomAttribute.fetch_set set_parameters
+  end
+
   private
 
-    def search_set
-      @set_name = params[:custom_attributes] ? params[:custom_attributes][:set_name] : nil
-      @attribute_set = Keyword::CustomAttribute.where(set_name: @set_name) if @set_name.present?
+    def redirect_to_show_set
+      return unless set_parameters[:table_name].present? && set_parameters[:set_name].present?
+
+      redirect_to action: :show_set,
+        table_name: set_parameters[:table_name],
+        set_name: set_parameters[:set_name]
+    end
+
+    def set_parameters
+      allowed = params.permit(:table_name, :set_name).symbolize_keys
+
+      if params[:combo_name].present?
+        allowed[:table_name] = params[:combo_name].split('/').first
+        allowed[:set_name]   = params[:combo_name].split('/').last
+      end
+
+      allowed
     end
 end
--- test/controllers/administration/custom_attributes_controller_test.rb
+++ test/controllers/administration/custom_attributes_controller_test.rb
@@ -13,16 +13,30 @@ class Administration::CustomAttributesControllerTest < ActionController::TestCas
   end
 
   test 'should list custom attributes for set' do
-    get :index
-    assert_response :success
-    refute assigns(:attribute_set)
-
     params = {
+      table_name: 'asiakas',
       set_name: 'PROSPEKTI'
     }
 
-    get :index, custom_attributes: params
+    get :show_set, params
     assert_response :success
     assert assigns(:attribute_set)
+
+    get :index, params
+    assert_redirected_to custom_set_path(params)
+    assert assigns(:attribute_set)
+  end
+
+  test 'should get index if params missing' do
+    get :index, table_name: 'asiakas'
+    assert_response :success
+    assert_template :index
+
+    get :index, set_name: 'PROSPEKTI'
+    assert_response :success
+    assert_template :index
+
+    get :index, table_name: 'asiakas', set_name: 'PROSPEKTI'
+    assert_response :redirect
   end
 end
