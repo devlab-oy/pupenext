9114bff245e6b63ab89248cd91d12d977ea2c554
Joni Kanerva <joni@devlab.fi>
2015-08-28 12:52:30 +0300

    set default value for default set

--- db/migrate/20150826122121_update_mysqlalias_permissions.rb
+++ db/migrate/20150826122121_update_mysqlalias_permissions.rb
@@ -6,6 +6,10 @@ class UpdateMysqlaliasPermissions < ActiveRecord::Migration
       Permission.where(nimi: 'mysqlaliasyllapito.php').find_each do |permission|
         permission.update(nimi: 'pupenext/custom_attributes', alanimi: '')
       end
+
+      Keyword::CustomAttribute.where(selitetark_2: '').find_each do |keyword|
+        keyword.update(selitetark_2: 'Default')
+      end
     end
   end
 
@@ -16,6 +20,10 @@ class UpdateMysqlaliasPermissions < ActiveRecord::Migration
       Permission.where(nimi: 'pupenext/custom_attributes', alanimi: '').find_each do |permission|
         permission.update(nimi: 'mysqlaliasyllapito.php')
       end
+
+      # Update all skips validations (as selitetark_2 is required field).
+      # Hope you don't have to rollback.
+      Keyword::CustomAttribute.where(selitetark_2: 'Default').update_all(selitetark_2: '')
     end
   end
 end
