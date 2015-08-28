7f95f7bd279d2de64d5e304994dbe84012f97013
Joni Kanerva <joni@devlab.fi>
2015-08-28 10:34:11 +0300

    add required helpers

--- app/helpers/custom_attribute_helper.rb
+++ app/helpers/custom_attribute_helper.rb
@@ -5,17 +5,32 @@ module CustomAttributeHelper
     end.uniq.sort
   end
 
-  def custom_attributes_tables_options
-    # List of table names available for custom attributes
+  def database_field_options
+    options = []
+
+    ActiveRecord::Base.connection.tables.each do |table|
+      ActiveRecord::Base.connection.columns(table).each do |column|
+        name = column.name
+        skip_columns = %w(yhtio tunnus)
+
+        options << "#{table}.#{name}" unless skip_columns.include? name
+      end
+    end
+
+    options
+  end
+
+  def attribute_visibility_options
     [
-      [ Customer.model_name.human, 'asiakas' ],
-      [ 'Tuote', 'tuote' ],
-      [ 'Toimittaja', 'toimi' ],
+      [ t('administration.custom_attributes.visibility_options.visible'), 'visible' ],
+      [ t('administration.custom_attributes.visibility_options.hidden'),  'hidden'  ],
     ]
   end
 
-  def attributes_for_table(table)
-    columns = ActiveRecord::Base.connection.columns table.to_s
-    columns.map &:name
+  def attribute_required_options
+    [
+      [ t('administration.custom_attributes.required_options.optional'),  'optional'  ],
+      [ t('administration.custom_attributes.required_options.mandatory'), 'mandatory' ],
+    ]
   end
 end
