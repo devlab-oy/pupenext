499201971d308ccb32eae3b725ca279ffeabdf77
Joni Kanerva <joni@devlab.fi>
2015-09-01 09:53:53 +0300

    proof of concept

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -7,4 +7,15 @@ class AdministrationController < ApplicationController
   def update_access
     render text: t('.forbidden'), status: :forbidden unless update_access?
   end
+
+  def resource_parameters(*params)
+    alias_set  = params[:alias_set].to_s
+    set_name   = alias_set.empty? ? "Default" : alias_set
+    table_name = find_resource.class.table_name
+    attributes = Keyword::CustomAttribute.visible.fetch_set table_name: table_name, set_name: set_name
+
+    return params unless attributes.present?
+
+    attributes.map { |a| a.field.to_sym }
+  end
 end
