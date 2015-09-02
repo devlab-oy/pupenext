6ce2edc1d9f24927f5170e49e2e66665ed20507e
Sami Luukkonen <sami@devlab.fi>
2015-09-02 15:10:49 +0300

    remove unnecessary code

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,42 +3,11 @@ class ReportsController < ApplicationController
     @data = RevenueExpenditureReport.new(params[:period].to_i).data if params[:period]
   end
 
-  def revenue_expenditure_update
-    if RevenueExpenditureReport.update_keyword revenue_expenditure_params
-      flash.now[:notice] = t('.update_success')
-    end
-
-    @data = RevenueExpenditureReport.new(params[:period].to_i).data if params[:period]
-    render :revenue_expenditure
-  end
-
-  def revenue_expenditure_save
-    if RevenueExpenditureReport.save_keyword revenue_expenditure_params
-      flash.now[:notice] = t('.create_success')
-    end
-
-    @data = RevenueExpenditureReport.new(params[:period].to_i).data if params[:period]
-    render :revenue_expenditure
-  end
-
-  def revenue_expenditure_delete
-    if RevenueExpenditureReport.delete_keyword revenue_expenditure_params
-      flash.now[:notice] = t('.delete_success')
-    end
-
-    @data = RevenueExpenditureReport.new(params[:period].to_i).data if params[:period]
-    render :revenue_expenditure
-  end
-
   private
 
     def revenue_expenditure_params
       params.permit(
         :period,
-        :week,
-        :name,
-        :sum,
-        :id,
       )
     end
 end
