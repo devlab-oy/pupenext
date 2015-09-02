6760abe859312e76316067a2833eaf8fbcb66718
Sami Luukkonen <sami@devlab.fi>
2015-09-02 13:30:19 +0300

    update method

--- app/controllers/administration/revenue_expenditure_report_datum_controller.rb
+++ app/controllers/administration/revenue_expenditure_report_datum_controller.rb
@@ -10,9 +10,25 @@ class Administration::RevenueExpenditureReportDatumController < AdministrationCo
     render :edit
   end
 
+  def update
+    if @data.update revenue_expenditure_report_data_params
+      redirect_to revenue_expenditure_report_datum_index_path
+    else
+      render :edit
+    end
+  end
+
   private
 
     def find_resource
       @data = Keyword::RevenueExpenditureReportData.find params[:id]
+    end
+
+    def revenue_expenditure_report_data_params
+      params.require(:data).permit(
+        :selite,
+        :selitetark,
+        :selitetark_2,
+      )
     end
 end
