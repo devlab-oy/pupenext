4f70c09af2d12fd37cf6e9b951dcb1d8a69c7f76
Sami Luukkonen <sami@devlab.fi>
2015-09-02 14:48:18 +0300

    new and create tests

--- test/controllers/administration/revenue_expenditure_report_datum_controller_test.rb
+++ test/controllers/administration/revenue_expenditure_report_datum_controller_test.rb
@@ -20,4 +20,23 @@ class Administration::RevenueExpenditureReportDatumControllerTest < ActionContro
     get :show, id: @data.tunnus
     assert_response :success
   end
+
+  test 'should get new' do
+    get :new
+    assert_response :success
+  end
+
+  test 'should create keyword' do
+    params = {
+      selite: '36 / 2015',
+      selitetark: 'Palkat',
+      selitetark_2: 1000,
+    }
+
+    assert_difference("Keyword::RevenueExpenditureReportData.count") do
+      post :create, data: params
+    end
+
+    assert_redirected_to revenue_expenditure_report_datum_index_path
+  end
 end
