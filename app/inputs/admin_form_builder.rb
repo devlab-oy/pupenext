d4fcf28f71abc36b10a9aad8da97e4a8f76c8e93
Joni Kanerva <joni@devlab.fi>
2015-09-02 13:13:39 +0300

    we need to pass alias set as an option

--- app/inputs/admin_form_builder.rb
+++ app/inputs/admin_form_builder.rb
@@ -1,5 +1,11 @@
 # FormBuilder that allows only fields spcified in custom_attributes
 class AdminFormBuilder < SimpleForm::FormBuilder
+  def initialize(*) #:nodoc:
+    super
+    @alias_set = options[:alias_set] || 'Default'
+    @table_name = @object.class.table_name
+  end
+
   def input(attribute_name, options = {}, &block)
     return unless input_allowed? attribute_name
 
@@ -9,13 +15,11 @@ class AdminFormBuilder < SimpleForm::FormBuilder
   private
 
     def input_allowed?(attribute_name)
-      set_name = 'Default'
-      table_name = @object.class.table_name
-      database_field = "#{table_name}.#{attribute_name}"
-      attributes = Keyword::CustomAttribute.fetch_set table_name: table_name, set_name: set_name
+      database_field = "#{@table_name}.#{attribute_name}"
+      attributes = Keyword::CustomAttribute.fetch_set table_name: @table_name, set_name: @alias_set
 
       # If have no attributes defined, and we're using default set -> allow everything
-      return true if attributes.blank? && set_name = 'Default'
+      return true if attributes.blank? && @alias_set = 'Default'
 
       # Check that current attribute is marked as visible
       attributes.visible.where(database_field: database_field).present?
