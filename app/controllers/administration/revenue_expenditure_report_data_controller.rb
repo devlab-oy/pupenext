e84ca23a5f042acd4154b2fb82c6377fef855535
Sami Luukkonen <sami@devlab.fi>
2015-09-02 12:56:03 +0300

    revenue expenditure report data administration

---
+++ app/controllers/administration/revenue_expenditure_report_data_controller.rb
@@ -0,0 +1,46 @@
+class Administration::RevenueExpenditureReportDataController < AdministrationController
+  def index
+    @data_set = Keyword::RevenueExpenditureReportData.all
+  end
+
+  # def self.delete_keyword(params)
+
+  #   if Keyword::RevenueExpenditureReportData.delete params[:id]
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
+
+  # def self.update_keyword(params)
+
+  #   update_params = {
+  #     selite: params[:week],
+  #     selitetark: params[:name],
+  #     selitetark_2: params[:sum],
+  #   }
+
+  #   if Keyword::RevenueExpenditureReportData.update params[:id], update_params
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
+
+  # def self.save_keyword(params)
+
+  #   save_params = {
+  #     selite: params[:week],
+  #     selitetark: params[:name],
+  #     selitetark_2: params[:sum],
+  #   }
+
+  #   keyword = Keyword::RevenueExpenditureReportData.new save_params
+
+  #   if keyword.save
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
+end
