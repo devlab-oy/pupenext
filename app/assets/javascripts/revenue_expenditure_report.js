f58f10e25f4161c914212cde7bf4ade3f96657ee
Sami Luukkonen <sami@devlab.fi>
2015-09-03 17:02:24 +0300

    revenue expenditure report datum helper

--- app/assets/javascripts/revenue_expenditure_report.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $('.custom_expenditure').hide();
-
-  $('#rows_table').on('click', '.custom_expenditure_rows', function() {
-    id = $(this).attr('id');
-    $('.' + id).toggle();
-  });
-});
