54b255ae2656bddb669ee5fed39575f352208cd0
Sami Luukkonen <sami@devlab.fi>
2015-09-04 14:35:46 +0300

    fix fixtures

--- test/classes/revenue_expenditure_report_test.rb
+++ test/classes/revenue_expenditure_report_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class RevenueExpenditureReportTest < ActiveSupport::TestCase
-  fixtures %w(heads head/voucher_rows keywords accounts companies)
+  fixtures %w(heads head/voucher_rows keywords accounts companies permissions)
 
   setup do
     travel_to Date.parse '2015-08-14'
