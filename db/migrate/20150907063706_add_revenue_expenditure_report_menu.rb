43f746103dce67a5f6a29cad472c6275e994659e
Joni Kanerva <joni@devlab.fi>
2015-09-07 09:53:56 +0300

    käyttöoikeusmigration

---
+++ db/migrate/20150907063706_add_revenue_expenditure_report_menu.rb
@@ -0,0 +1,50 @@
+class AddRevenueExpenditureReportMenu < ActiveRecord::Migration
+  def up
+    Company.find_each do |company|
+      Current.company = company.yhtio
+
+      # Luodaan valikko
+      Permission.create!(
+        sovellus: 'Ostoreskontra',
+        nimi: 'pupenext/revenue_expenditure',
+        nimitys: 'Kassavirta',
+        jarjestys: 145
+      )
+
+      # Lisätään Admin profiiliin
+      Permission.create!(
+        kuka: 'Admin profiili',
+        profiili: 'Admin profiili',
+        paivitys: 1,
+        sovellus: 'Ostoreskontra',
+        nimi: 'pupenext/revenue_expenditure',
+        nimitys: 'Kassavirta',
+        jarjestys: 145
+      )
+
+      # Lisätään Adminille käyttöoikeus
+      admin = User.find_by(kuka: 'admin')
+
+      if admin
+        Permission.create!(
+          kuka: 'admin',
+          user_id: admin.tunnus,
+          profiili: 'Admin profiili',
+          paivitys: 1,
+          sovellus: 'Ostoreskontra',
+          nimi: 'pupenext/revenue_expenditure',
+          nimitys: 'Kassavirta',
+          jarjestys: 145
+        )
+      end
+    end
+  end
+
+  def down
+    Company.find_each do |company|
+      Current.company = company.yhtio
+
+      Permission.where(nimi: 'pupenext/revenue_expenditure').delete_all
+    end
+  end
+end
