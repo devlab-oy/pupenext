eb8be80069e1ef6f8e5c5eb2428b34994a0a79e0
Sami Luukkonen <sami@devlab.fi>
2015-09-07 13:01:39 +0300

    scope date fix

--- app/models/head.rb
+++ app/models/head.rb
@@ -8,8 +8,8 @@ class Head < BaseModel
   belongs_to :terms_of_payment, foreign_key: :maksuehto
   has_many :accounting_rows, class_name: 'Head::VoucherRow', foreign_key: :ltunnus
 
-  scope :paid, -> { where.not(mapvm: '0000-00-00') }
-  scope :unpaid, -> { where(mapvm: '0000-00-00') }
+  scope :paid, -> { where.not(mapvm: 0) }
+  scope :unpaid, -> { where(mapvm: 0) }
 
   before_create :set_date_fields
   after_create :fix_datetime_fields
