421f1b3157ae7b6e45b825f9cee9ad659384e8de
Joni Kanerva <joni@devlab.fi>
2015-09-08 12:26:42 +0300

    add tests for custom sets

--- test/integration/pupenext_admin_form_builder_test.rb
+++ test/integration/pupenext_admin_form_builder_test.rb
@@ -43,7 +43,37 @@ class PupenextAdminFormBuilderTest < ActionDispatch::IntegrationTest
     assert_response :success
 
     # we should see only two: created_by and modified_by
-    assert_select "tr", { count: 2 }, 'only three rows should be visible'
+    assert_select "tr", { count: 2 }, 'only two rows should be visible'
     assert_select "input[id=account_nimi]", { count: 0 }, 'nimi cannot be present'
+  end
+
+  test "we should not see any fields if no fields are visible in given alias_set" do
+    attribute = keywords(:mysql_alias_1)
+    permission = permissions(:bob_accounts_update)
+
+    attribute.update! database_field: 'tili.nimi', set_name: 'bobset', visibility: :hidden
+    permission.update! alias_set: 'bobset'
+
+    get edit_account_path(@account), alias_set: 'bobset'
+    assert_response :success
+
+    # we should see only two: created_by and modified_by
+    assert_select "tr", { count: 2 }, 'only two rows should be visible'
+    assert_select "input[id=account_nimi]", { count: 0 }, 'nimi cannot be present'
+  end
+
+  test "we should see visible fields in given alias_set" do
+    attribute = keywords(:mysql_alias_1)
+    permission = permissions(:bob_accounts_update)
+
+    attribute.update! database_field: 'tili.nimi', set_name: 'bobset', visibility: :visible
+    permission.update! alias_set: 'bobset'
+
+    get edit_account_path(@account), alias_set: 'bobset'
+    assert_response :success
+
+    # we should see only three: nimi, created_by, and modified_by
+    assert_select "tr", { count: 3 }, 'only three rows should be visible'
+    assert_select "input[id=account_nimi]", { count: 1 }, 'nimi must be present'
   end
 end
