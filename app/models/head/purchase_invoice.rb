cf8ed40a0d7ce4e5dfed0cd2d59a1cea12a98030
Joni Kanerva <joni@devlab.fi>
2015-09-09 10:12:01 +0300

    better name for static and remove sti_column as it does not support multiple values, we must do a scope to fetch all subclasses at once

--- app/models/head/purchase_invoice.rb
+++ app/models/head/purchase_invoice.rb
@@ -3,11 +3,5 @@ class Head::PurchaseInvoice < Head
   self.abstract_class = true
 
   # We have actually 5 types, that are saved in "tila".
-  # Eventough it's supposed to be the STI column.
-  PURCHASE_INVOICE_TYPES = %w{H Y M P Q}
-
-  # Rails requires sti_name method to return type column (tyyppi) value
-  def self.sti_name
-    PURCHASE_INVOICE_TYPES
-  end
+  TYPES = %w{H Y M P Q}
 end
