e8dba8ef5020b74cbd99578d1f93c25ee72cb421
Sami Luukkonen <sami@devlab.fi>
2015-09-09 15:47:47 +0300

    alias attributes

--- app/models/product/group.rb
+++ app/models/product/group.rb
@@ -1,6 +1,9 @@
 class Product::Group < Keyword
   has_many :products, foreign_key: :osasto, primary_key: :selite
 
+  alias_attribute :value, :selite
+  alias_attribute :name, :selitetark
+
   # Rails requires sti_name method to return type column (laji) value
   def self.sti_name
     'OSASTO'
