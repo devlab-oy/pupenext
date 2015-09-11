1e372e1a7c2061d2432fbd77ef998783c363ceb8
Sami Luukkonen <sami@devlab.fi>
2015-09-11 14:41:38 +0300

    no need to state primary key for relation

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,5 +1,5 @@
 class Supplier < BaseModel
-  has_many :product_suppliers, foreign_key: :liitostunnus, primary_key: :tunnus, class_name: 'Product::Supplier'
+  has_many :product_suppliers, foreign_key: :liitostunnus, class_name: 'Product::Supplier'
   has_many :products, through: :product_suppliers
 
   self.table_name = :toimi
