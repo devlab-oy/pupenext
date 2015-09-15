8b8f640ecb1801d5142a2bc03a75a55029a98cce
Sami Luukkonen <sami@devlab.fi>
2015-09-15 15:43:22 +0300

    add validations

--- app/models/pending_update.rb
+++ app/models/pending_update.rb
@@ -3,4 +3,9 @@ class PendingUpdate < ActiveRecord::Base
   include UserDefinedValidations
 
   belongs_to :pending_updatable, polymorphic: true
+
+  validates :key, presence: true
+  validates :value_type, presence: true, inclusion: {
+    in: %w(Date DateTime Decimal Float Integer String Text Time Timestamp)
+  }
 end
