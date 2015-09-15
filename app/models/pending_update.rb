9545cd43230afa63fbcb62676aa822b67afcc697
Sami Luukkonen <sami@devlab.fi>
2015-09-15 11:10:35 +0300

    use activerecord::base instead of basemodel

--- app/models/pending_update.rb
+++ app/models/pending_update.rb
@@ -1,3 +1,6 @@
-class PendingUpdate < BaseModel
+class PendingUpdate < ActiveRecord::Base
+  include CurrentUser
+  include UserDefinedValidations
+
   belongs_to :pending_updatable, polymorphic: true
 end
