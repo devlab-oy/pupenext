216dd8cdabf7e727a3acf362e6f5e7539cddc1a3
Johan Tötterman <juppe@devlab.fi>
2015-09-15 15:20:17 +0300

    Added default scope

--- app/models/head/sales_invoice_row.rb
+++ app/models/head/sales_invoice_row.rb
@@ -3,6 +3,7 @@ class Head::SalesInvoiceRow < BaseModel
   belongs_to :invoice, foreign_key: :uusiotunnus, class_name: 'Head::SalesInvoice'
 
   default_scope { where(tyyppi: 'L') }
+  default_scope { where.not(var: %w(P J O S)) }
 
   self.table_name = :tilausrivi
   self.primary_key = :tunnus
