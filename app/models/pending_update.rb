b6044d96faef563423b1795717b4b5486e896642
Sami Luukkonen <sami@devlab.fi>
2015-09-16 11:02:08 +0300

    attributes validation

--- app/models/pending_update.rb
+++ app/models/pending_update.rb
@@ -8,4 +8,22 @@ class PendingUpdate < ActiveRecord::Base
   validates :value_type, presence: true, inclusion: {
     in: %w(Date DateTime Decimal Float Integer String Text Time Timestamp)
   }
+
+  validate :attributes
+
+  def attributes
+    hash = {}
+    obj = pending_updatable_type.classify.constantize
+
+    PendingUpdate.where(pending_updatable_id: pending_updatable_id, pending_updatable_type: pending_updatable_type).each do |attr|
+      hash[attr.key] = attr.value
+    end
+
+    hash[key] = value
+
+    x = obj.find pending_updatable_id
+    x.attributes = hash
+
+    errors.add(:value, x.errors.full_messages.join) unless x.valid?
+  end
 end
