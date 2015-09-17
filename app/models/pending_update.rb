571c71ec4e185def7de1500ce9a4257e33060a15
Joni Kanerva <joni@devlab.fi>
2015-09-17 13:25:06 +0300

    rename method

--- app/models/pending_update.rb
+++ app/models/pending_update.rb
@@ -6,24 +6,26 @@ class PendingUpdate < ActiveRecord::Base
 
   validates :key, presence: true
 
-  validate :attributes
+  validate :updatable_attributes
 
-  def attributes
-    klass = pending_updatable_type.classify.constantize
+  private
 
-    pending_updates = PendingUpdate.where pending_updatable_id: pending_updatable_id,
-                                          pending_updatable_type: pending_updatable_type
+    def updatable_attributes
+      klass = pending_updatable_type.classify.constantize
 
-    hash = {}
-    pending_updates.each do |attr|
-      hash[attr.key] = attr.value
+      pending_updates = PendingUpdate.where pending_updatable_id: pending_updatable_id,
+                                            pending_updatable_type: pending_updatable_type
+
+      hash = {}
+      pending_updates.each do |attr|
+        hash[attr.key] = attr.value
+      end
+
+      hash[key] = value
+
+      obj = klass.find pending_updatable_id
+      obj.attributes = hash
+
+      errors.add(:value, obj.errors.full_messages.join(', ')) unless obj.valid?
     end
-
-    hash[key] = value
-
-    obj = klass.find pending_updatable_id
-    obj.attributes = hash
-
-    errors.add(:value, obj.errors.full_messages.join(', ')) unless obj.valid?
-  end
 end
