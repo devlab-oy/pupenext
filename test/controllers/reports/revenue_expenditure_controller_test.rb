00b1bed40b4c581c0ec44f73df77ac01919b7d73
Joni Kanerva <joni@devlab.fi>
2015-09-28 09:34:31 +0300

    test period select works

--- test/controllers/reports/revenue_expenditure_controller_test.rb
+++ test/controllers/reports/revenue_expenditure_controller_test.rb
@@ -4,10 +4,21 @@ class Reports::RevenueExpenditureControllerTest < ActionController::TestCase
   setup do
     @user = users(:bob)
     login @user
+
+    @first_header = I18n.t('reports.revenue_expenditure.index.history_revenue')
   end
 
   test 'test get report' do
-    get :revenue_expenditure
+    get :index
     assert_response :success
+
+    assert_select "th", { text: @first_header, count: 0 }
+  end
+
+  test 'test executing report' do
+    get :index, period: 1
+    assert_response :success
+
+    assert_select "th", { text: @first_header, count: 1 }
   end
 end
