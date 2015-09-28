e592f2bc1ef3c281dc1487e7e93402271f91a565
Joni Kanerva <joni@devlab.fi>
2015-09-28 09:11:20 +0300

    add reports scope

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,9 @@ Pupesoft::Application.routes.draw do
     get 'qr_codes/generate'
   end
 
-  get :revenue_expenditure, to: 'reports#revenue_expenditure'
+  scope module: :reports do
+    get :revenue_expenditure, to: 'revenue_expenditure#revenue_expenditure'
+  end
 
   root to: 'home#index'
   get '/test', to: 'home#test'
