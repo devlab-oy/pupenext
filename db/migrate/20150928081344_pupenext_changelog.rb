223ad314e4294c4c3891be0cbd5321cae28a8845
Johan Tötterman <juppe@devlab.fi>
2015-09-28 14:58:21 +0300

    Indentation

--- db/migrate/20150928081344_pupenext_changelog.rb
+++ db/migrate/20150928081344_pupenext_changelog.rb
@@ -1,10 +1,10 @@
 class PupenextChangelog < ActiveRecord::Migration
   def change
-      add_column :git_pulkkarit, :repository, :string, limit: 20, default: 'pupesoft', null: false, after: :id
-      add_column :git_paivitykset, :repository, :string, limit: 20, default: 'pupesoft', null: false, after: :hash
-      add_column :git_paivitykset, :hash_pupenext, :string, limit: 50, default: '', null: false, after: :hash
-      rename_column :git_paivitykset, :hash, :hash_pupesoft
+    add_column :git_pulkkarit, :repository, :string, limit: 20, default: 'pupesoft', null: false, after: :id
+    add_column :git_paivitykset, :repository, :string, limit: 20, default: 'pupesoft', null: false, after: :hash
+    add_column :git_paivitykset, :hash_pupenext, :string, limit: 50, default: '', null: false, after: :hash
+    rename_column :git_paivitykset, :hash, :hash_pupesoft
 
-      GitPull.update_pupenext_hash
-    end
+    GitPull.update_pupenext_hash
+  end
 end
