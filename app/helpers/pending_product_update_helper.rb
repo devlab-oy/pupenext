7ee4e73255e75142558e9191ebfe3281e03b360f
Sami Luukkonen <sami@devlab.fi>
2015-09-28 16:30:31 +0300

    remove duplicates from helper & use onchange

--- app/helpers/pending_product_update_helper.rb
+++ app/helpers/pending_product_update_helper.rb
@@ -2,24 +2,30 @@ module PendingProductUpdateHelper
   def categories_options(args = {})
     return Current.company.categories if args[:osasto].nil? && args[:try].nil? && args[:tuotemerkki].nil?
 
-    Product::Subcategory.categories(args[:try]) +
-    Product::Category.categories(args[:osasto]) +
-    Product::Brand.categories(args[:tuotemerkki])
+    result = Product::Subcategory.categories(args[:try]) +
+             Product::Category.categories(args[:osasto]) +
+             Product::Brand.categories(args[:tuotemerkki])
+    result.uniq!
+    result
   end
 
   def subcategories_options(args = {})
     return Current.company.subcategories if args[:osasto].nil? && args[:try].nil? && args[:tuotemerkki].nil?
 
-    Product::Subcategory.subcategories(args[:try]) +
-    Product::Category.subcategories(args[:osasto]) +
-    Product::Brand.subcategories(args[:tuotemerkki])
+    result = Product::Subcategory.subcategories(args[:try]) +
+             Product::Category.subcategories(args[:osasto]) +
+             Product::Brand.subcategories(args[:tuotemerkki])
+    result.uniq!
+    result
   end
 
   def brands_options(args = {})
     return Current.company.brands if args[:osasto].nil? && args[:try].nil? && args[:tuotemerkki].nil?
 
-    Product::Subcategory.brands(args[:try]) +
-    Product::Category.brands(args[:osasto]) +
-    Product::Brand.brands(args[:tuotemerkki])
+    result = Product::Subcategory.brands(args[:try]) +
+             Product::Category.brands(args[:osasto]) +
+             Product::Brand.brands(args[:tuotemerkki])
+    result.uniq!
+    result
   end
 end
