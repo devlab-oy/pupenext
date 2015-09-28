a81275e0a593fda5a78490af83ec8201a1bbaaf9
Johan Tötterman <juppe@devlab.fi>
2015-09-28 14:40:05 +0300

    Update current pupenext git hash to git_paivitykset when migrating.

---
+++ app/models/git_pull.rb
@@ -0,0 +1,20 @@
+class GitPull < ActiveRecord::Base
+  self.table_name  = :git_paivitykset
+  self.primary_key = :id
+
+  def self.update_pupenext_hash
+    lastid = get_pupenext_hash
+
+    if lastid
+      update(lastid.id, :hash_pupenext => get_current_git_hash)
+    end
+  end
+
+  def self.get_pupenext_hash
+    where.not(hash_pupesoft: 'github_api_request').order(id: :desc).limit(1).last
+  end
+
+  def self.get_current_git_hash
+    `git rev-parse HEAD`.strip
+  end
+end
