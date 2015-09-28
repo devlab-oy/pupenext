cb30e0168861d8e3924010d3832c2ed384bed43a
Joni Kanerva <joni@devlab.fi>
2015-09-28 15:15:19 +0300

    add ftpsend logic

--- app/jobs/ftp_send_job.rb
+++ app/jobs/ftp_send_job.rb
@@ -1,7 +1,37 @@
-class FtpSendJob < ActiveJob::Base
-  queue_as :default
+require 'net/ftp'
 
-  def perform(*args)
-    # Do something later
+class FtpSendJob < ActiveJob::Base
+  queue_as :ftp_send
+
+  def perform(transport_id:, file:)
+    raise "File '#{file}' not found" unless File.exist?(file)
+
+    @transport = Transport.find transport_id
+    @file = file
+
+    send_file
   end
+
+  private
+
+    def send_file
+      ftp = Net::FTP.new
+
+      begin
+        ftp.connect @transport.hostname
+        ftp.login @transport.username, @transport.password
+        ftp.chdir @transport.path
+        ftp.put @file
+        ftp.quit
+      rescue => e
+        message = "FTP Error: #{e.message}"
+
+        STDERR.puts message
+        Rails.logger.error message
+
+        raise message
+      ensure
+        ftp.close
+      end
+    end
 end
