6e6ee35f3ee8f1da60083dc000a8c7db0f80f3e5
Joni Kanerva <joni@devlab.fi>
2015-09-29 15:04:03 +0300

    use category filter

--- app/models/product/brand.rb
+++ app/models/product/brand.rb
@@ -1,4 +1,6 @@
 class Product::Brand < Keyword
+  include CategoryFilter
+
   has_many :products, foreign_key: :tuotemerkki, primary_key: :selite
 
   alias_attribute :tag, :selite
@@ -6,17 +8,5 @@ class Product::Brand < Keyword
   # Rails requires sti_name method to return type column (laji) value
   def self.sti_name
     'TUOTEMERKKI'
-  end
-
-  def self.categories(merkki)
-    Current.company.categories.joins(:products).where(tuote: { tuotemerkki: merkki }).pluck(:selite)
-  end
-
-  def self.subcategories(merkki)
-    Current.company.subcategories.joins(:products).where(tuote: { tuotemerkki: merkki }).pluck(:selite)
-  end
-
-  def self.brands(merkki)
-    Current.company.brands.joins(:products).where(tuote: { tuotemerkki: merkki }).pluck(:selite)
   end
 end
