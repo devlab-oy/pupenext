c50c3967fc63c4aa78f6184d6eca1f672cac0aa8
Joni Kanerva <joni@devlab.fi>
2015-09-29 15:08:15 +0300

    call name name

--- app/models/product/brand.rb
+++ app/models/product/brand.rb
@@ -3,7 +3,7 @@ class Product::Brand < Keyword
 
   has_many :products, foreign_key: :tuotemerkki, primary_key: :selite
 
-  alias_attribute :tag, :selite
+  alias_attribute :name, :selite
 
   # Rails requires sti_name method to return type column (laji) value
   def self.sti_name
