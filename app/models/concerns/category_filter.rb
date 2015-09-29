54139b98e2f84ede0a4d5421c58db25d066eb0f5
Joni Kanerva <joni@devlab.fi>
2015-09-29 15:15:42 +0300

    filter, don't override

--- app/models/concerns/category_filter.rb
+++ app/models/concerns/category_filter.rb
@@ -4,9 +4,9 @@ module CategoryFilter
   module ClassMethods
     def filter(categories: nil, subcategories: nil, brands: nil)
       products = Product.all
-      products = Product.where(tuote: { osasto: categories }) if categories
-      products = Product.where(tuote: { try: subcategories }) if subcategories
-      products = Product.where(tuote: { tuotemerkki: brands }) if brands
+      products = products.where(tuote: { osasto: categories }) if categories
+      products = products.where(tuote: { try: subcategories }) if subcategories
+      products = products.where(tuote: { tuotemerkki: brands }) if brands
 
       joins(:products).where(tuote: { tunnus: products })
     end
