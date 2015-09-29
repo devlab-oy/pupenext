1f11c727c52516c50614d42b5ede488c9cb276e2
Sami Luukkonen <sami@devlab.fi>
2015-09-29 11:47:40 +0300

    use properly named fields & fix tests

--- app/helpers/pending_product_update_helper.rb
+++ app/helpers/pending_product_update_helper.rb
@@ -1,30 +1,30 @@
 module PendingProductUpdateHelper
   def categories_options(args = {})
-    return Current.company.categories if args[:osasto].nil? && args[:try].nil? && args[:tuotemerkki].nil?
+    return Current.company.categories if args[:category].nil? && args[:subcategory].nil? && args[:brand].nil?
 
-    result = Product::Subcategory.categories(args[:try]) +
-             Product::Category.categories(args[:osasto]) +
-             Product::Brand.categories(args[:tuotemerkki])
+    result = Product::Subcategory.categories(args[:subcategory]) +
+             Product::Category.categories(args[:category]) +
+             Product::Brand.categories(args[:brand])
     result.uniq!
     result
   end
 
   def subcategories_options(args = {})
-    return Current.company.subcategories if args[:osasto].nil? && args[:try].nil? && args[:tuotemerkki].nil?
+    return Current.company.subcategories if args[:category].nil? && args[:subcategory].nil? && args[:brand].nil?
 
-    result = Product::Subcategory.subcategories(args[:try]) +
-             Product::Category.subcategories(args[:osasto]) +
-             Product::Brand.subcategories(args[:tuotemerkki])
+    result = Product::Subcategory.subcategories(args[:subcategory]) +
+             Product::Category.subcategories(args[:category]) +
+             Product::Brand.subcategories(args[:brand])
     result.uniq!
     result
   end
 
   def brands_options(args = {})
-    return Current.company.brands if args[:osasto].nil? && args[:try].nil? && args[:tuotemerkki].nil?
+    return Current.company.brands if args[:category].nil? && args[:subcategory].nil? && args[:brand].nil?
 
-    result = Product::Subcategory.brands(args[:try]) +
-             Product::Category.brands(args[:osasto]) +
-             Product::Brand.brands(args[:tuotemerkki])
+    result = Product::Subcategory.brands(args[:subcategory]) +
+             Product::Category.brands(args[:category]) +
+             Product::Brand.brands(args[:brand])
     result.uniq!
     result
   end
