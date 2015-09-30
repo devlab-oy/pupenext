be8b3ba66aef35feb68df32d3213dda95681ebca
Sami Luukkonen <sami@devlab.fi>
2015-09-30 15:14:36 +0300

    add list test

--- test/controllers/pending_product_updates_controller_test.rb
+++ test/controllers/pending_product_updates_controller_test.rb
@@ -11,17 +11,27 @@ class PendingProductUpdatesControllerTest < ActionController::TestCase
     get :index
     assert_response :success
 
-    assert_template "index", "Template should be index"
+    assert_template :index, "Template should be index"
+  end
+
+  test 'should get list' do
+    get :list
+    assert_response :success
+
+    assert_template :index, "Without pressing submit-button template should be index"
 
     product_supplier = product_suppliers :domestic_product_supplier
 
     request = {
       'tuotteen_toimittajat.toim_tuoteno' => product_supplier[:toim_tuoteno],
       ei_saldoa: 'true',
-      poistettu: 'true'
+      poistettu: 'true',
+      commit: 'search'
     }
 
-    get :index, request
+    get :list, request
+    assert_template :list, "By pressing submit-button template should be list"
     assert_not_nil assigns(:products)
   end
+
 end
