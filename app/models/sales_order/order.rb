e821e8372ecebfe60eb69ab945a583ccec3eaaad
Joni Kanerva <joni@devlab.fi>
2015-09-30 14:05:10 +0300

    add sales order row model

--- app/models/sales_order/order.rb
+++ app/models/sales_order/order.rb
@@ -1,4 +1,6 @@
 class SalesOrder::Order < Head
+  has_many :rows, foreign_key: :otunnus, class_name: 'SalesOrder::Row'
+
   validates :tila, inclusion: { in: ['L'] }
 
   scope :not_delivered, -> { where(alatila: %w(A C)) }
--- test/models/sales_order/order_test.rb
+++ test/models/sales_order/order_test.rb
@@ -3,10 +3,11 @@ require 'test_helper'
 class SalesOrder::OrderTest < ActiveSupport::TestCase
   fixtures %w(
     sales_order/orders
+    sales_order/rows
   )
 
   setup do
-    @order = sales_order_orders(:so_one)
+    @order = sales_order_orders :so_one
   end
 
   test 'fixture should be valid' do
@@ -16,5 +17,7 @@ class SalesOrder::OrderTest < ActiveSupport::TestCase
 
   test 'model relations' do
     assert @order.accounting_rows.count > 0
+    assert @order.rows.count > 0
+    assert_equal "L", @order.rows.first.tyyppi
   end
 end
