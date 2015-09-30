0a8b153d24dde5b42d8c4ae13b8715c4f1557fe1
Joni Kanerva <joni@devlab.fi>
2015-09-30 13:56:06 +0300

    add manufacture and stocktrasfer rows relation

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,10 +3,12 @@ require 'test_helper'
 class ProductTest < ActiveSupport::TestCase
   fixtures %w(
     keywords
+    manufacture_order/rows
     pending_updates
     product/suppliers
     products
     shelf_locations
+    stock_transfer/rows
     suppliers
   )
 
@@ -19,27 +21,16 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test 'relations' do
-    category = keywords :category_tools
-    assert_equal category.description, @product.category.description
-
-    subcategory = keywords :subcategory_tools
-    assert_equal subcategory.description, @product.subcategory.description
-
-    brand = keywords :brand_tools
-    assert_equal brand.tag, @product.brand.tag
-
-    status = keywords :status_active
-    assert_equal status.description, @product.status.description
-
-    product_supplier = product_suppliers :domestic_product_supplier
-    assert_equal @product.tuoteno, product_supplier.product.tuoteno
-
-    supplier = suppliers :domestic_supplier
-    assert_equal supplier.nimi, @product.suppliers.first.nimi
-
-    assert_equal @product.id, @product.pending_updates.first.pending_updatable_id
-
+    assert_equal 'OSASTO', @product.category.laji
+    assert_equal 'TRY', @product.subcategory.laji
+    assert_equal 'TUOTEMERKKI', @product.brand.laji
+    assert_equal 'S', @product.status.laji
+    assert @product.product_suppliers.count > 0
+    assert @product.suppliers.count > 0
+    assert @product.pending_updates.count > 0
     assert @product.shelf_locations.count > 0
+    assert @product.manufacture_rows.count > 0
+    assert @product.stock_transfer_rows.count > 0
   end
 
   test 'product stock' do
