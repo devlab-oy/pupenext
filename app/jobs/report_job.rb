9e3f93682fe016c82fd78e335d886dc3e48822ec
Joni Kanerva <joni@devlab.fi>
2015-10-01 16:26:05 +0300

    cannot pass an instance, must pass name and params

--- app/jobs/report_job.rb
+++ app/jobs/report_job.rb
@@ -1,16 +1,16 @@
 class ReportJob < ActiveJob::Base
-  # To use this worker, "report_instance" should be an instance of the report class.
-  # Report must implement a "to_file" method that runs the report, saves the result to a file,
-  # and returns a string with a full path to the created file.
+  # To use this worker, report class instance must implement a "to_file" method
+  # that runs the report, saves the result to a file, and returns a string with a full
+  # path to the created file.
 
   queue_as :reports
 
-  def perform(user_id:, report_instance:, report_name:)
+  def perform(user_id:, report_class:, report_params:, report_name:)
     user = User.find user_id
     Current.company = user.company
 
     # Run the report
-    filename = report_instance.to_file
+    filename = report_class.constantize.new(report_params).to_file
 
     # Save the resulting file to user's downloads list
     download = user.downloads.create! report_name: report_name
