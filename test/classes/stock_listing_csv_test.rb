e76f6c931327e0ef07c3d73ddacb6e5c84d41c31
Joni Kanerva <joni@devlab.fi>
2015-10-01 07:54:43 +0300

    add stock after next purchace order

--- test/classes/stock_listing_csv_test.rb
+++ test/classes/stock_listing_csv_test.rb
@@ -12,6 +12,7 @@ class StockListingCsvTest < ActiveSupport::TestCase
 
   setup do
     @company = companies :acme
+    @purchase_order = purchase_order_orders :po_one
   end
 
   test 'report initialize' do
@@ -23,17 +24,31 @@ class StockListingCsvTest < ActiveSupport::TestCase
 
   test 'report output' do
     product = @company.products.active.first
+    product.shelf_locations.update_all(saldo: 0)
+    product.shelf_locations.first.update!(tuoteno: 'A1', saldo: 10)
     product.update! tuoteno: 'A1', eankoodi: 'FOO', nimitys: 'BAR'
 
-    # We should get product info and stock available
+    # We should get product info and stock available 10
     report = StockListingCsv.new(company_id: @company.id)
-    output = "A1,FOO,BAR,#{product.stock_available}\n"
+    output = "A1,FOO,BAR,10.0,,10.0\n"
     assert_equal output, report.csv_data.lines.first
 
-    # if stock is negative, we should get zero for stock
-    product.shelf_locations.update_all(saldo: -10)
+    # If stock is negative we should get zero stock
+    product.shelf_locations.first.update!(tuoteno: 'A1', saldo: -10)
     report = StockListingCsv.new(company_id: @company.id)
-    output = "A1,FOO,BAR,0.0\n"
+    output = "A1,FOO,BAR,0.0,,0.0\n"
+    assert_equal output, report.csv_data.lines.first
+    assert_equal -10, product.stock
+
+    # if we have upcoming orders, we should get the date and stock after
+    @purchase_order.rows.first.update!(
+      tuoteno: product.tuoteno,
+      toimaika: '2015-01-01',
+      varattu: 20,
+      laskutettuaika: 0
+    )
+    report = StockListingCsv.new(company_id: @company.id)
+    output = "A1,FOO,BAR,0.0,2015-01-01,10.0\n"
     assert_equal output, report.csv_data.lines.first
   end
 
