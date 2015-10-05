e4d088998c437f70f1d4431cd0befa5a6f201b7d
Joni Kanerva <joni@devlab.fi>
2015-10-05 14:17:34 +0300

    draft excel read implementation

--- app/controllers/data_import_controller.rb
+++ app/controllers/data_import_controller.rb
@@ -3,13 +3,33 @@ class DataImportController < ApplicationController
   end
 
   def products
-    uploaded_io = params[:file]
-    file_path = Rails.root.join 'tmp', uploaded_io.original_filename
-
-    File.open(file_path, 'wb') do |file|
-      file.write uploaded_io.read
+    if @spreadsheet = uploaded_spreadsheet
+      render :results
+    else
+      flash[:error] = 'Invalid file!'
+      redirect_to data_import_path
     end
-
-    redirect_to data_import_path
   end
+
+  private
+
+    def uploaded_spreadsheet
+      uploaded_io = params[:file]
+
+      return unless uploaded_io
+
+      extension = File.extname uploaded_io.original_filename
+      file_path = Tempfile.new ['data_import', ".#{extension}"]
+
+      begin
+        File.open(file_path, 'wb') { |file| file.write uploaded_io.read }
+
+        Roo::Spreadsheet.open file_path.path
+      rescue => e
+        false
+      ensure
+        file_path.close
+        file_path.unlink
+      end
+    end
 end
