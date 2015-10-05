414c7a7d9bbb24518c9987686c1c6daed7ff751b
Joni Kanerva <joni@devlab.fi>
2015-10-05 12:41:16 +0300

    add file upload

--- app/controllers/data_import_controller.rb
+++ app/controllers/data_import_controller.rb
@@ -3,6 +3,13 @@ class DataImportController < ApplicationController
   end
 
   def products
+    uploaded_io = params[:file]
+    file_path = Rails.root.join 'tmp', uploaded_io.original_filename
+
+    File.open(file_path, 'wb') do |file|
+      file.write uploaded_io.read
+    end
+
     redirect_to data_import_path
   end
 end
