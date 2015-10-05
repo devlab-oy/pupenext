bd28e7533595ccbe212bdd9f3545771cfea7d251
Joni Kanerva <joni@devlab.fi>
2015-10-05 16:29:54 +0300

    simplify controller

--- app/controllers/data_import_controller.rb
+++ app/controllers/data_import_controller.rb
@@ -1,35 +1,26 @@
 class DataImportController < ApplicationController
+  before_action :check_for_file, except: [ :index ]
+
   def index
   end
 
   def product_keywords
-    if @spreadsheet = uploaded_spreadsheet
-      render :results
-    else
-      flash[:error] = 'Invalid file!'
+    if @uploaded_file
+      flash[:notice] = 'Data imported!'
       redirect_to data_import_path
+    else
+      render :results
     end
   end
 
   private
 
-    def uploaded_spreadsheet
-      uploaded_io = params[:file]
+    def check_for_file
+      @uploaded_file = params[:file]
 
-      return unless uploaded_io
-
-      extension = File.extname uploaded_io.original_filename
-      file_path = Tempfile.new ['data_import', ".#{extension}"]
-
-      begin
-        File.open(file_path, 'wb') { |file| file.write uploaded_io.read }
-
-        Roo::Spreadsheet.open file_path.path
-      rescue => e
-        false
-      ensure
-        file_path.close
-        file_path.unlink
+      unless @uploaded_file
+        flash[:error] = 'no file found!'
+        redirect_to data_import_path
       end
     end
 end
