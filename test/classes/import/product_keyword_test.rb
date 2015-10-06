016f5151d29ed4102849d1d39259319d2dc0a61f
Joni Kanerva <joni@devlab.fi>
2015-10-06 14:36:14 +0300

    fix typos

--- test/classes/import/product_keyword_test.rb
+++ test/classes/import/product_keyword_test.rb
@@ -22,11 +22,11 @@ class Import::ProductKeywordTest < ActiveSupport::TestCase
     assert Import::ProductKeyword.new company_id: @company, user_id: @user, filename: @filename
     assert Import::ProductKeyword.new company_id: @company, user_id: @user, filename: rails_upload
     assert_raises { Import::ProductKeyword.new company_id: @company, user_id: @user, filename: nil }
-    assert_raises { Import::ProductKeyword.new company_id: @company, user_id: @user, filename: 'not_found_file.xls' }
+    assert_raises { Import::ProductKeyword.new company_id: @company, user_id: @user, filename: 'not_found.xls' }
     assert_raises { Import::ProductKeyword.new company_id: @company, user_id: @user, filename: wrong_format }
   end
 
-  test 'first row contains column labels' do
+  test 'imports keywords' do
     keywords = Import::ProductKeyword.new company_id: @company, user_id: @user, filename: @filename
     Product::Keyword.delete_all
 
