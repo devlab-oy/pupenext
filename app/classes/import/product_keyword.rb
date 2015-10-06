a504fae0cb98f782443820e0f33a07795873e275
Joni Kanerva <joni@devlab.fi>
2015-10-06 14:14:52 +0300

    add import method

--- app/classes/import/product_keyword.rb
+++ app/classes/import/product_keyword.rb
@@ -1,13 +1,53 @@
 class Import::ProductKeyword
   def initialize(filename)
     @file = setup_file filename
+    @errors = []
+
+    Current.user = User.find_by kuka: 'joe'
   end
 
   def import
-    true
+    spreadsheet.each(column_definitions) do |excel_row|
+      row = Row.new excel_row
+
+      if row.product.nil?
+        add_error I18n.t('errors.messages.invalid')
+        next
+      end
+
+      if !row.create
+        add_error row.keyword.errors.full_messages
+        next
+      end
+    end
+  end
+
+  def errors
+    @errors
   end
 
   private
+
+    def spreadsheet
+      @file.sheet(0)
+    end
+
+    def column_definitions
+      {
+        jarjestys:  'jarjestys',
+        kieli:      'kieli',
+        laji:       'laji',
+        nakyvyys:   'nakyvyys',
+        selite:     'selite',
+        selitetark: 'selitetark',
+        toiminto:   'toiminto',
+        tuoteno:    'tuoteno',
+      }
+    end
+
+    def add_error(message)
+      @errors << message
+    end
 
     def setup_file(filename)
       # if we have an rails UploadedFile class
@@ -23,4 +63,39 @@ class Import::ProductKeyword
     ensure
       file.close if file
     end
+end
+
+class Import::ProductKeyword::Row
+  def initialize(hash)
+    @tuoteno  = hash.delete :tuoteno
+    @toiminto = hash.delete :toiminto
+    @hash     = hash
+  end
+
+  def product
+    @product ||= Product.find_by tuoteno: @tuoteno
+  end
+
+  def keyword
+    return unless product
+
+    @keyword ||= product.keywords.find_or_create_by(laji: values[:laji], kieli: values[:kieli])
+  end
+
+  def create
+    return unless product
+
+    keyword.attributes = values
+    keyword.save
+  end
+
+  def values
+    hash = @hash
+    hash.each { |k, v| hash[k] = '' if hash[k].nil? }
+    hash
+  end
+
+  def add_new?
+    %w(lisää lisÄÄ lisaa).include? @toiminto.to_s.downcase
+  end
 end
--- test/classes/import/product_keyword_test.rb
+++ test/classes/import/product_keyword_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class Import::ProductKeywordTest < ActiveSupport::TestCase
+  fixtures %w(
+    keyword/product_information_types
+    keyword/product_keyword_types
+    keyword/product_parameter_types
+    product/keywords
+    products
+  )
+
   setup do
-    @filename = Rails.root.join 'test', 'fixtures', 'files', 'product_keyword_information_test.xlsx'
+    @filename = Rails.root.join 'test', 'fixtures', 'files', 'product_keyword_test.xlsx'
   end
 
   test 'initialize' do
@@ -15,5 +23,14 @@ class Import::ProductKeywordTest < ActiveSupport::TestCase
     assert_raises { Import::ProductKeyword.new nil }
     assert_raises { Import::ProductKeyword.new 'not_found_file.xls' }
     assert_raises { Import::ProductKeyword.new wrong_format }
+  end
+
+  test 'first row contains column labels' do
+    keywords = Import::ProductKeyword.new(@filename)
+    Product::Keyword.delete_all
+
+    assert_difference 'Product::Keyword.count', 3 do
+      keywords.import
+    end
   end
 end
