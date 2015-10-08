85d2fa755eea5505d2e77624fbd529ffc6ea0a82
tiere <tommi@devlab.fi>
2015-10-08 14:43:48 +0300

    Add legacy methods module and a method for fetching customer price from pupesoft

---
+++ app/modules/legacy_methods.rb
@@ -0,0 +1,24 @@
+require('open3')
+
+module LegacyMethods
+  LEGACY_API_DIR = "#{Rails.application.secrets.pupesoft_install_dir}/rajapinnat"
+
+  class << self
+    def customer_price(customer_id, product_id)
+      price = nil
+
+      Open3.popen2('php',
+                   '-f',
+                   'alehinta.php',
+                   Current.company.yhtio,
+                   'asiakas',
+                   customer_id.to_s,
+                   product_id.to_s,
+                   chdir: LEGACY_API_DIR) do |_i, o, _t|
+        price = JSON.parse(o.gets, symbolize_names: true)
+      end
+
+      price[:price].to_d
+    end
+  end
+end
