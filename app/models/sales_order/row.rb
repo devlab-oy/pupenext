c798be6de0972652b68ac7e115d288266ff2124e
Mika Myllynen <giefsufu@gmail.com>
2015-10-09 11:24:19 +0300

    open scope for sales order row

--- app/models/sales_order/row.rb
+++ app/models/sales_order/row.rb
@@ -3,6 +3,8 @@ class SalesOrder::Row < Row
 
   validates :tyyppi, inclusion: { in: ['L'] }
 
+  scope :open, -> { where("tilausrivi.varattu > 0 AND tilausrivi.laskutettuaika = '0000-00-00'") }
+
   # Rails requires sti_name method to return type column (tyyppi) value
   def self.sti_name
     "L"
