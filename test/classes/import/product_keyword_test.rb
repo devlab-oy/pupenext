6f228b14772cb5dc2c436a7b27336d4b5dae53d8
Joni Kanerva <joni@devlab.fi>
2015-10-14 14:27:24 +0300

    add poista functionality

--- test/classes/import/product_keyword_test.rb
+++ test/classes/import/product_keyword_test.rb
@@ -39,14 +39,16 @@ class Import::ProductKeywordTest < ActiveSupport::TestCase
     end
   end
 
-  test 'toiminto lisaa and muuta' do
+  test 'toiminto values' do
     spreadsheet = create_xlsx([
-      ['tuoteno',            'laji',    'selite', 'toiminto'],
-      ["#{@helmet.tuoteno}", 'nimitys', 'foo',    'LISAA'   ],
-      ["#{@hammer.tuoteno}", 'nimitys', 'bar 1',  'LISÄÄ'   ],
-      ["#{@hammer.tuoteno}", 'nimitys', 'bar 2',  'muoKKAa' ],
+      ['tuoteno',            'laji',    'selite', 'toiminto'      ],
+      ["#{@helmet.tuoteno}", 'nimitys', 'foo',    'LISAA'         ],
+      ["#{@hammer.tuoteno}", 'nimitys', 'bar 1',  'LISÄÄ'         ],
+      ["#{@hammer.tuoteno}", 'nimitys', 'bar 2',  'muoKKAa'       ],
       ["#{@hammer.tuoteno}", 'kuvaus',  'bar 2',  'muokkaa/lisää' ],
       ["#{@hammer.tuoteno}", 'kuvaus',  'bar 3',  'muokkaa/lisää' ],
+      ["#{@helmet.tuoteno}", 'kuvaus',  'foo',    'LISAA'         ],
+      ["#{@helmet.tuoteno}", 'kuvaus',  'foo',    'POISTA'        ],
     ])
 
     keywords = Import::ProductKeyword.new company_id: @company, user_id: @user, filename: spreadsheet
