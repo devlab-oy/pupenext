f90fb969f9744e348f276cd5f43d788bc8ea6a3a
Joni Kanerva <joni@devlab.fi>
2015-10-16 14:42:26 +0300

    draft view

--- app/controllers/data_export_controller.rb
+++ app/controllers/data_export_controller.rb
@@ -1,9 +1,28 @@
 class DataExportController < ApplicationController
+  include ColumnSort
+
   def product_keywords
-    render nothing: true
   end
 
   def product_keywords_generate
-    render nothing: true
+    render :product_keywords and return if params[:commit].blank?
+
+    @products = Product.regular.search_like(search_params)
+
+    render json: @products
   end
+
+  private
+
+    def sortable_columns
+      []
+    end
+
+    def searchable_columns
+      [
+        osasto: [],
+        try: [],
+        tuotemerkki: [],
+      ]
+    end
 end
