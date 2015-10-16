cdfe100ea35b111cf4072fbd072cfaf4d2d7a9e3
Joni Kanerva <joni@devlab.fi>
2015-10-16 15:08:54 +0300

    move filter param into own method

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -1,23 +1,14 @@
 module ProductHelper
   def categories_options
-    options_for_select(
-      Product::Category.filter(args).pluck(:description, :tag).uniq,
-      params[:osasto]
-    )
+    options_for_select category_filter, params[:osasto]
   end
 
   def subcategories_options
-    options_for_select(
-      Product::Subcategory.filter(args).pluck(:description, :tag).uniq,
-      params[:try]
-    )
+    options_for_select subcatecory_filter, params[:try]
   end
 
   def brands_options
-    options_for_select(
-      Product::Brand.filter(args).pluck(:name).uniq,
-      params[:tuotemerkki]
-    )
+    options_for_select brand_filter, params[:tuotemerkki]
   end
 
   private
@@ -28,5 +19,17 @@ module ProductHelper
         subcategories: params[:try],
         brands:        params[:tuotemerkki]
       }
+    end
+
+    def category_filter
+      Product::Category.filter(args).pluck(:description, :tag).uniq
+    end
+
+    def subcatecory_filter
+      Product::Subcategory.filter(args).pluck(:description, :tag).uniq
+    end
+
+    def brand_filter
+      Product::Brand.filter(args).pluck(:name).uniq
     end
 end
