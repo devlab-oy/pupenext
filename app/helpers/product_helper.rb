37615e0314e4472fe4f32750e9015c252a0a1b10
Joni Kanerva <joni@devlab.fi>
2015-10-16 14:55:45 +0300

    move code to helper

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -1,13 +1,32 @@
 module ProductHelper
-  def categories_options(args = {})
-    Product::Category.filter(args).pluck(:description, :tag).uniq
+  def categories_options
+    options_for_select(
+      Product::Category.filter(args).pluck(:description, :tag).uniq,
+      params[:osasto]
+    )
   end
 
-  def subcategories_options(args = {})
-    Product::Subcategory.filter(args).pluck(:description, :tag).uniq
+  def subcategories_options
+    options_for_select(
+      Product::Subcategory.filter(args).pluck(:description, :tag).uniq,
+      params[:try]
+    )
   end
 
-  def brands_options(args = {})
-    Product::Brand.filter(args).pluck(:name).uniq
+  def brands_options
+    options_for_select(
+      Product::Brand.filter(args).pluck(:name).uniq,
+      params[:tuotemerkki]
+    )
   end
+
+  private
+
+    def args
+      {
+        categories:    params[:osasto],
+        subcategories: params[:try],
+        brands:        params[:tuotemerkki]
+      }
+    end
 end
