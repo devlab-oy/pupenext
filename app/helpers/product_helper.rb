e14c6f26bd3e3a51bd5ee1f0bf2fd97f83923511
tiere <tommi@devlab.fi>
2015-10-16 13:16:38 +0300

    Use empty hash as default value

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -1,13 +1,13 @@
 module ProductHelper
-  def categories_options(args = nil)
+  def categories_options(args = {})
     Product::Category.filter(args).pluck(:description, :tag).uniq
   end
 
-  def subcategories_options(args = nil)
+  def subcategories_options(args = {})
     Product::Subcategory.filter(args).pluck(:description, :tag).uniq
   end
 
-  def brands_options(args = nil)
+  def brands_options(args = {})
     Product::Brand.filter(args).pluck(:name).uniq
   end
 end
