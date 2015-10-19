ee61d6c0b6db6841cfd10d0a84b9f5d041a7d08d
Joni Kanerva <joni@devlab.fi>
2015-10-19 13:47:37 +0300

    redirect to category selection if submit not pressed

--- test/controllers/data_export_controller_test.rb
+++ test/controllers/data_export_controller_test.rb
@@ -10,8 +10,13 @@ class DataExportControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test 'product keyword export' do
+  test 'redirect to category selection if submit not pressed' do
     post :product_keywords_generate
+    assert_redirected_to product_keyword_export_path
+  end
+
+  test 'product keyword export' do
+    post :product_keywords_generate, commit: true, format: :xlsx
     assert_response :success
   end
 end
