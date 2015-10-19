de542ff3c5711d698bfe55a9284bc9673f26d116
Joni Kanerva <joni@devlab.fi>
2015-10-19 13:47:37 +0300

    redirect to category selection if submit not pressed

--- app/controllers/data_export_controller.rb
+++ app/controllers/data_export_controller.rb
@@ -5,7 +5,7 @@ class DataExportController < ApplicationController
   end
 
   def product_keywords_generate
-    render :product_keywords and return if params[:commit].blank?
+    redirect_to product_keyword_export_path(search_params) and return if params[:commit].blank?
 
     @products = Product.regular.includes(:keywords).search_like(search_params)
 
