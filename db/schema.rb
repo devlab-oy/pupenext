cd9574d998129a50bb6e3fe550e97ff8bd614a23
tiere <tommi@devlab.fi>
2015-10-19 10:02:34 +0300

    Change files data to longblob

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012100714) do
+
+ActiveRecord::Schema.define(version: 20151019065817) do
 
   create_table "abc_aputaulu", primary_key: "tunnus", force: :cascade do |t|
     t.string   "yhtio",              limit: 5,                            default: "",  null: false
@@ -580,9 +581,9 @@ ActiveRecord::Schema.define(version: 20151012100714) do
   create_table "files", force: :cascade do |t|
     t.integer  "download_id", limit: 4
     t.string   "filename",    limit: 255
-    t.binary   "data",        limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.binary   "data",        limit: 4294967295
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
   add_index "files", ["download_id"], name: "index_files_on_download_id", using: :btree
