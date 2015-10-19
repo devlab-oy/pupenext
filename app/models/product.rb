345d360c78bf104f2dd3043b949502dd98afabea
tiere <tommi@devlab.fi>
2015-10-19 12:40:08 +0300

    Add tests for customer price

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,7 +9,7 @@ class Product < BaseModel
   has_many :pending_updates, as: :pending_updatable, dependent: :destroy
   has_many :suppliers, through: :product_suppliers
   has_many :attachments, foreign_key: :liitostunnus, class_name: 'Attachment::ProductAttachment'
-  has_many :customer_prices, foreign_key: :tuoteno
+  has_many :customer_prices, foreign_key: :tuoteno, primary_key: :tuoteno
   has_many :customers, through: :customer_prices
 
   delegate :images, to: :attachments
---
+++ test/models/customer_price_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class CustomerPriceTest < ActiveSupport::TestCase
+  fixtures %w(customer_prices customers products)
+
+  setup do
+    @one               = customer_prices(:one)
+    @hammer            = products(:hammer)
+    @stubborn_customer = customers(:stubborn_customer)
+  end
+
+  test "fixtures are valid" do
+    assert @one.valid?, @one.errors.full_messages
+  end
+
+  test "associations work" do
+    assert_equal @hammer, @one.product
+    assert_equal @stubborn_customer, @one.customer
+  end
+end
