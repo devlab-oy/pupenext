e33ea46492d1499709de9700bf80b9864d69707d
tiere <tommi@devlab.fi>
2015-10-19 14:10:52 +0300

    Test that customer subcategory price list works with product filters

--- test/controllers/reports/customer_price_lists_controller_test.rb
+++ test/controllers/reports/customer_price_lists_controller_test.rb
@@ -19,6 +19,7 @@ class Reports::CustomerPriceListsControllerTest < ActionController::TestCase
       post :create, { commit: true, target_type: 1, target: @customer.id, osasto: 1000, try: 2000 }
 
       assert_equal Product.where(osasto: 1000, try: 2000), assigns(:products)
+      assert_equal @customer, assigns(:customer)
 
       assert_redirected_to customer_price_lists_path
     end
@@ -29,6 +30,7 @@ class Reports::CustomerPriceListsControllerTest < ActionController::TestCase
       post :create, { commit: true, target_type: 1, target: @customer.id, osasto: 1000 }
 
       assert_equal Product.where(osasto: 1000), assigns(:products)
+      assert_equal @customer, assigns(:customer)
 
       assert_redirected_to customer_price_lists_path
     end
@@ -39,6 +41,18 @@ class Reports::CustomerPriceListsControllerTest < ActionController::TestCase
       post :create, { commit: true, target_type: 1, target: @customer.id, try: 2000 }
 
       assert_equal Product.where(try: 2000), assigns(:products)
+      assert_equal @customer, assigns(:customer)
+
+      assert_redirected_to customer_price_lists_path
+    end
+  end
+
+  test "customer group prices with product filters work with osasto and try" do
+    LegacyMethods.stub(:customer_subcategory_price, 30) do
+      post :create, { commit: true, target_type: 2, target: @customer.ryhma, osasto: 1000, try: 2000 }
+
+      assert_equal Product.where(osasto: 1000, try: 2000), assigns(:products)
+      assert_equal @customer.ryhma, assigns(:customer_subcategory)
 
       assert_redirected_to customer_price_lists_path
     end
