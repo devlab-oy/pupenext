0061e558d26c37020a3e551805f7ebad0a74fec6
tiere <tommi@devlab.fi>
2015-10-19 11:17:29 +0300

    Add customer association to customer price

--- app/models/customer_price.rb
+++ app/models/customer_price.rb
@@ -1,5 +1,6 @@
 class CustomerPrice < BaseModel
   belongs_to :product, foreign_key: :tuoteno, primary_key: :tuoteno
+  belongs_to :customer, foreign_key: :asiakas
 
   self.table_name        = :asiakashinta
   self.primary_key       = :tunnus
