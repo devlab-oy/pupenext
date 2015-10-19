ca92d6841801cf0f0903ba44eaf1a7b748292c5d
Joni Kanerva <joni@devlab.fi>
2015-10-19 12:55:49 +0300

    display category tag

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -14,14 +14,20 @@ module ProductHelper
   private
 
     def category_filter
-      Product::Category.where(kieli: current_user.locale).pluck(:description, :tag).uniq
+      Product::Category.where(kieli: current_user.locale)
+        .order(:jarjestys, :selite, :selitetark)
+        .map { |c| ["#{c.tag} - #{c.description}", c.tag] }.uniq
     end
 
     def subcatecory_filter
-      Product::Subcategory.where(kieli: current_user.locale).filter(categories: params[:osasto]).pluck(:description, :tag).uniq
+      Product::Subcategory.where(kieli: current_user.locale)
+        .filter(categories: params[:osasto])
+        .map { |c| ["#{c.tag} - #{c.description}", c.tag] }.uniq
     end
 
     def brand_filter
-      Product::Brand.where(kieli: current_user.locale).filter(categories: params[:osasto], subcategories: params[:try]).pluck(:name).uniq
+      Product::Brand.where(kieli: current_user.locale)
+        .filter(categories: params[:osasto], subcategories: params[:try])
+        .pluck(:name).uniq
     end
 end
