f51df162419364ce97737f5daa51cb1926a7ada5
tiere <tommi@devlab.fi>
2015-10-21 09:22:37 +0300

    Add test for customer's contract_price? method

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -10,8 +10,10 @@ class CustomerTest < ActiveSupport::TestCase
   )
 
   setup do
-    @one                    = customers :stubborn_customer
-    @customer_subcategory_1 = keyword_customer_subcategories :customer_subcategory_1
+    @one    = customers :stubborn_customer
+    @hammer = products :hammer
+    @helmet = products :helmet
+    @ski    = products :ski
   end
 
   test 'fixtures are valid' do
@@ -23,5 +25,12 @@ class CustomerTest < ActiveSupport::TestCase
     assert_equal @customer_subcategory_1, @one.subcategory
     assert @one.customer_prices.count > 0
     assert @one.products.count > 0
+  end
+
+  test 'contract_price?' do
+    assert @one.contract_price?(@hammer)
+    assert @one.contract_price?(@helmet)
+
+    refute @one.contract_price?(@ski)
   end
 end
