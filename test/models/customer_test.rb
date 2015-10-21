90c20700f08357d2e43e6df9b764f094091231ac
tiere <tommi@devlab.fi>
2015-10-21 12:11:47 +0300

    Add customer subcategory model

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,10 +1,14 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  fixtures %w(customers transports)
+  fixtures %w(customers customer_prices products transports keyword/customer_subcategories)
 
   setup do
-    @one = customers :stubborn_customer
+    @one                    = customers :stubborn_customer
+    @hammer                 = products :hammer
+    @helmet                 = products :helmet
+    @ski                    = products :ski
+    @customer_subcategory_1 = keyword_customer_subcategories :customer_subcategory_1
   end
 
   test 'fixtures are valid' do
@@ -13,5 +17,15 @@ class CustomerTest < ActiveSupport::TestCase
 
   test 'relations' do
     assert_not_equal 0, @one.transports.count
+    assert @one.customer_prices.count > 0
+    assert @one.products.count > 0
+    assert_equal @customer_subcategory_1, @one.customer_subcategory
+  end
+
+  test 'contract_price?' do
+    assert @one.contract_price?(@hammer)
+    assert @one.contract_price?(@helmet)
+
+    refute @one.contract_price?(@ski)
   end
 end
---
+++ test/models/keyword/customer_subcategory_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class Keyword::CustomerSubcategoryTest < ActiveSupport::TestCase
+  fixtures %w(keyword/customer_subcategories)
+
+  setup do
+    @one = keyword_customer_subcategories :customer_subcategory_1
+    @two = keyword_customer_subcategories :customer_subcategory_2
+  end
+
+  test 'fixtures are valid' do
+    assert @one.valid?, @one.errors.full_messages
+  end
+
+  test 'associations work' do
+    assert_equal 1, @one.customers.count
+    assert_equal 0, @two.customers.count
+  end
+end
