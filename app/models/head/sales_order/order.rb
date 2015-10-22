9ff92470910229c64736cda7f8c65bdd231b98c2
Mika Myllynen <giefsufu@gmail.com>
2015-10-22 15:08:06 +0300

    invoiced scope

--- app/models/head/sales_order/order.rb
+++ app/models/head/sales_order/order.rb
@@ -2,6 +2,7 @@ class Head::SalesOrder::Order < Head::SalesOrder
   validates :tila, inclusion: { in: ['L'] }
 
   scope :not_delivered, -> { where(alatila: %w(A C)) }
+  scope :invoiced, -> { where(alatila: 'X') }
 
   # Rails requires sti_name method to return type column (tyyppi) value
   def self.sti_name
