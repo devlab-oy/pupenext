1e684ff02dfb810ef577f18ce4bfeb519a39e583
Joni Kanerva <joni@devlab.fi>
2015-10-26 16:08:39 +0200

    fetch keywords

--- app/controllers/data_export_controller.rb
+++ app/controllers/data_export_controller.rb
@@ -8,6 +8,7 @@ class DataExportController < ApplicationController
     redirect_to product_keyword_export_path(keyword_params) and return if params[:commit].blank?
 
     @extra_fields = keyword_params[:extra_fields]
+    @keywords = product_keywords
     @products = Product.regular.includes(:keywords).search_like(search_params)
 
     respond_to do |format|
@@ -23,6 +24,8 @@ class DataExportController < ApplicationController
 
     def keyword_params
       params.permit(
+        :language,
+        :type,
         extra_fields: [],
         osasto: [],
         try: [],
@@ -36,5 +39,20 @@ class DataExportController < ApplicationController
         try: [],
         tuotemerkki: [],
       ]
+    end
+
+    def product_keywords
+      keywords = case keyword_params[:type]
+      when 'keyword'
+        Keyword::ProductKeywordType.all
+      when 'information'
+        Keyword::ProductInformationType.all
+      when 'parameter'
+        Keyword::ProductParameterType.all
+      else
+        Keyword.none
+      end
+
+      keywords.where(kieli: keyword_params[:language]).order(:jarjestys, :selitetark)
     end
 end
