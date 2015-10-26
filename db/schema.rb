d92154eae21a6c91db500107fda9863ed5fe4af6
tiere <tommi@devlab.fi>
2015-10-26 12:38:34 +0200

    Formatting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151019065817) do
 
   create_table "abc_aputaulu", primary_key: "tunnus", force: :cascade do |t|
