a58db9d42a51dc0752da58776915bf3020deeda2
Joni Kanerva <joni@devlab.fi>
2015-10-28 07:41:08 +0200

    return space instead of nil

--- test/controllers/data_export_controller_test.rb
+++ test/controllers/data_export_controller_test.rb
@@ -1,6 +1,14 @@
 require 'test_helper'
 
 class DataExportControllerTest < ActionController::TestCase
+  fixtures %w(
+    keyword/product_information_types
+    keyword/product_keyword_types
+    keyword/product_parameter_types
+    product/keywords
+    products
+  )
+
   setup do
     login users(:bob)
   end
@@ -19,4 +27,35 @@ class DataExportControllerTest < ActionController::TestCase
     post :product_keywords_generate, commit: true, format: :xlsx
     assert_response :success
   end
+
+  test 'prodcut keywords excel format' do
+    params = {
+     commit: true,
+     format: :xlsx,
+     language: 'fi',
+     type: 'parameter',
+    }
+
+    post :product_keywords_generate, params
+    excel = open_excel(response.body)
+
+    header = ["Tuoteno", "Malliston nimi", "Tuotteen väri"]
+    assert_equal header, excel.row(1)
+
+    row = ["hammer123", "Aluminium", " "]
+    assert_equal row, excel.row(2)
+  end
+
+  private
+
+    def open_excel(response)
+      filename = Tempfile.new('spreadsheet').path
+      File.open(filename, 'wb') { |file| file.write(response) }
+
+      xlsx = Roo::Spreadsheet.open(filename, extension: :xlsx)
+
+      File.delete(filename)
+
+      xlsx.sheet(0)
+    end
 end
