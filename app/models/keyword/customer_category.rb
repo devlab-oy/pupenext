98426fe49f1dd41c85815c1f5abbace982d01a26
tiere <tommi@devlab.fi>
2015-11-03 13:21:52 +0200

    Override to_s methods

--- app/models/keyword/customer_category.rb
+++ app/models/keyword/customer_category.rb
@@ -1,6 +1,10 @@
 class Keyword::CustomerCategory < Keyword
   has_many :customers, foreign_key: :osasto, primary_key: :selite
 
+  def to_s
+    selitetark
+  end
+
   def self.sti_name
     :ASIAKASOSASTO
   end
--- app/models/keyword/customer_subcategory.rb
+++ app/models/keyword/customer_subcategory.rb
@@ -1,6 +1,10 @@
 class Keyword::CustomerSubcategory < Keyword
   has_many :customers, foreign_key: :ryhma, primary_key: :selite
 
+  def to_s
+    selitetark
+  end
+
   def self.sti_name
     :ASIAKASRYHMA
   end
