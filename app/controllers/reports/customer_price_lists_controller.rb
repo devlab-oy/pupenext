e920eaeceec3257e05788416dc15f43aaf1d5b8f
tiere <tommi@devlab.fi>
2015-11-05 11:33:33 +0200

    Require product filters always and refactor

--- app/controllers/reports/customer_price_lists_controller.rb
+++ app/controllers/reports/customer_price_lists_controller.rb
@@ -2,26 +2,29 @@ class Reports::CustomerPriceListsController < ApplicationController
   def create
     return render :index unless params[:commit].present?
 
+    unless params[:osasto] || params[:try]
+      flash.now[:alert] = t('.no_filters_specified')
+      return render :index
+    end
+
     @products = Product.includes(:attachments).active
 
-    if params[:target_type] == "1"
+    case params[:target_type].to_i
+    when 1 # Customer
       @customer = Customer.find_by(tunnus: params[:target])
 
       unless @customer
         flash.now[:alert] = t('.customer_not_found')
         return render :index, status: :not_found
       end
-    elsif params[:target_type] == "2"
+    when 2 # Customer subcategory
       @customer_subcategory = params[:target]
+    else
+      return render :index, status: :bad_request
     end
 
-    if params[:osasto] || params[:try]
-      @products = @products.where(osasto: params[:osasto]) if params[:osasto]
-      @products = @products.where(try: params[:try]) if params[:try]
-    elsif params[:contract_filter] != "2"
-      flash.now[:alert] = t('.no_filters_specified')
-      return render :index
-    end
+    @products = @products.where(osasto: params[:osasto]) if params[:osasto]
+    @products = @products.where(try: params[:try]) if params[:try]
 
     if @products.empty?
       flash.now[:alert] = t('.products_not_found')
@@ -33,8 +36,6 @@ class Reports::CustomerPriceListsController < ApplicationController
 
     kit.stylesheets << Rails.root.join('app', 'assets', 'stylesheets', 'reports', 'pdf_styles.css')
 
-    send_data kit.to_pdf,
-              disposition: :inline,
-              filename:    "#{t('.filename')}.pdf"
+    send_data kit.to_pdf, filename: "#{t('.filename')}.pdf"
   end
 end
