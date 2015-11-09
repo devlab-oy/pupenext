02cdb8c3856a043e012457dd2ebccdf3f0d5e44f
tiere <tommi@devlab.fi>
2015-11-09 13:35:46 +0200

    Filter product by contract price in controller

--- app/controllers/reports/customer_price_lists_controller.rb
+++ app/controllers/reports/customer_price_lists_controller.rb
@@ -8,24 +8,36 @@ class Reports::CustomerPriceListsController < ApplicationController
     end
 
     @products = Product.active
+    @products = @products.where(osasto: params[:osasto]) if params[:osasto]
+    @products = @products.where(try: params[:try]) if params[:try]
+    @products = @products.where(tuotemerkki: params[:tuotemerkki]) if params[:tuotemerkki]
 
     case params[:target_type].to_i
     when 1 # Customer
       @customer = Customer.find_by(asiakasnro: params[:target])
 
-      unless @customer
+      if @customer
+        target = @customer
+      else
         flash.now[:alert] = t('.customer_not_found')
         return render :index, status: :not_found, formats: :html
       end
     when 2 # Customer subcategory
-      @customer_subcategory = params[:target]
+      @customer_subcategory = Keyword::CustomerSubcategory.find(params[:target])
+
+      if @customer_subcategory
+        target = @customer_subcategory
+      else
+        flash.now[:alert] = t('.customer_subcategory_not_found')
+        return render :index, status: :not_found, formats: :html
+      end
     else
       return render :index, status: :bad_request, formats: :html
     end
 
-    @products = @products.where(osasto: params[:osasto]) if params[:osasto]
-    @products = @products.where(try: params[:try]) if params[:try]
-    @products = @products.where(tuotemerkki: params[:tuotemerkki]) if params[:tuotemerkki]
+    if params[:contract_filter].to_i == 2
+      @products = @products.select { |p| p.contract_price?(target) }
+    end
 
     if @products.empty?
       flash.now[:alert] = t('.products_not_found')
