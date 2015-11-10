ce4170c89264101669ff8f8e825169c2f92a86b6
tiere <tommi@devlab.fi>
2015-11-10 16:03:10 +0200

    Add country to form

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,12 @@
 class Country < ActiveRecord::Base
   self.table_name  = :maat
   self.primary_key = :tunnus
+
+  def self.unique_codes_and_names
+    where.not(koodi: '')
+    .where.not(nimi: '')
+    .select(:koodi, :nimi)
+    .distinct
+    .order(:nimi)
+  end
 end
