e176c68ae64ae6c1237ec3882989f9dc7672843f
Sami Luukkonen <sami@devlab.fi>
2015-11-11 10:41:23 +0200

    add ABC-reporting column to customer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106070812) do
+ActiveRecord::Schema.define(version: 20151111083616) do
 
   create_table "abc_aputaulu", primary_key: "tunnus", force: :cascade do |t|
     t.string   "yhtio",              limit: 5,                            default: "",  null: false
@@ -180,6 +180,7 @@ ActiveRecord::Schema.define(version: 20151106070812) do
     t.decimal  "erikoisale",                                     precision: 5,  scale: 2, default: 0.0, null: false
     t.string   "myyntikielto",                     limit: 1,                              default: "",  null: false
     t.string   "myynninseuranta",                  limit: 1,                              default: "",  null: false
+    t.string   "abc_raportointi",                  limit: 1,                              default: "",  null: false
     t.decimal  "luottoraja",                                     precision: 12, scale: 2, default: 0.0, null: false
     t.string   "luottovakuutettu",                 limit: 1,                              default: "",  null: false
     t.decimal  "kuluprosentti",                                  precision: 8,  scale: 3, default: 0.0, null: false
@@ -592,8 +593,8 @@ ActiveRecord::Schema.define(version: 20151106070812) do
     t.integer  "profit_account_id",               limit: 4
     t.integer  "sales_account_id",                limit: 4
     t.integer  "voucher_id",                      limit: 4
-    t.datetime "created_at",                                                                        null: false
-    t.datetime "updated_at",                                                                        null: false
+    t.datetime "created_at",                                                           null: false
+    t.datetime "updated_at",                                                           null: false
     t.date     "activated_at"
     t.date     "deactivated_at"
     t.string   "name",                            limit: 255
@@ -2646,8 +2647,8 @@ ActiveRecord::Schema.define(version: 20151106070812) do
     t.string   "username",    limit: 255
     t.string   "password",    limit: 255
     t.string   "path",        limit: 255
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   add_index "transports", ["customer_id"], name: "index_transports_on_customer_id", using: :btree
