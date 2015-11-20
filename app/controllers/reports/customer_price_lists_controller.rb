5da14439eb6f9648dc5826f8768fea84f5094130
tiere <tommi@devlab.fi>
2015-11-20 13:56:42 +0200

    Find customer subcategory by selite instead of id

--- app/controllers/reports/customer_price_lists_controller.rb
+++ app/controllers/reports/customer_price_lists_controller.rb
@@ -48,7 +48,7 @@ class Reports::CustomerPriceListsController < ApplicationController
           errors << t('reports.customer_price_lists.create.customer_not_found')
         end
       when 2 # Customer subcategory
-        @customer_subcategory = Keyword::CustomerSubcategory.find_by(tunnus: params[:target])
+        @customer_subcategory = Keyword::CustomerSubcategory.find_by(selite: params[:target])
 
         if @customer_subcategory
           @target = @customer_subcategory
