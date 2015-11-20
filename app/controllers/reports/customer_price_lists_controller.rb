81320f107e92a511d7512339af53fa903f825748
Joni Kanerva <joni@devlab.fi>
2015-11-20 11:05:13 +0200

    esim näin

--- app/controllers/reports/customer_price_lists_controller.rb
+++ app/controllers/reports/customer_price_lists_controller.rb
@@ -1,40 +1,11 @@
 class Reports::CustomerPriceListsController < ApplicationController
   def create
-    return render :index, formats: :html unless params[:commit].present?
-
-    unless params[:osasto] || params[:try] || params[:tuotemerkki]
-      flash.now[:alert] = t('.no_filters_specified')
-      return render :index, formats: :html
-    end
+    return render :index, formats: :html unless params_valid?
 
     @products = params[:product_image] ? Product.includes(:cover_thumbnail).active : Product.active
-
     @products = @products.where(osasto: params[:osasto]) if params[:osasto]
     @products = @products.where(try: params[:try]) if params[:try]
     @products = @products.where(tuotemerkki: params[:tuotemerkki]) if params[:tuotemerkki]
-
-    case params[:target_type].to_i
-    when 1 # Customer
-      @customer = Customer.find_by(asiakasnro: params[:target])
-
-      if @customer
-        target = @customer
-      else
-        flash.now[:alert] = t('.customer_not_found')
-        return render :index, status: :not_found, formats: :html
-      end
-    when 2 # Customer subcategory
-      @customer_subcategory = Keyword::CustomerSubcategory.find_by(tunnus: params[:target])
-
-      if @customer_subcategory
-        target = @customer_subcategory
-      else
-        flash.now[:alert] = t('.customer_subcategory_not_found')
-        return render :index, status: :not_found, formats: :html
-      end
-    else
-      return render :index, status: :bad_request, formats: :html
-    end
 
     if params[:contract_filter].to_i == 2
       @products = @products.select { |p| p.contract_price?(target) }
@@ -51,4 +22,46 @@ class Reports::CustomerPriceListsController < ApplicationController
            header:      { right: "#{t('.page')} [page] / [toPage]" },
            template:    'reports/customer_price_lists/report.html.erb'
   end
+
+  private
+
+    def params_valid?
+      errors = []
+
+      if params[:target].blank?
+        errors << t('.customer_not_found')
+      end
+
+      unless params[:osasto] || params[:try] || params[:tuotemerkki]
+        errors << t('.no_filters_specified')
+      end
+
+      case params[:target_type].to_i
+      when 1 # Customer
+        @customer = Customer.find_by(asiakasnro: params[:target])
+
+        if @customer
+          target = @customer
+        else
+          errors << t('.customer_not_found')
+        end
+      when 2 # Customer subcategory
+        @customer_subcategory = Keyword::CustomerSubcategory.find_by(tunnus: params[:target])
+
+        if @customer_subcategory
+          target = @customer_subcategory
+        else
+          errors << t('.customer_subcategory_not_found')
+        end
+      else
+        errors << 'invalid target type'
+      end
+
+      if errors.present?
+        flash.now[:alert] = errors.to_sentence
+        false
+      else
+        true
+      end
+    end
 end
