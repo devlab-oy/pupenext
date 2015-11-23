f7e98c5b7a0ec4ce098e2b291b4d5d88c6297b63
tiere <tommi@devlab.fi>
2015-11-23 14:51:38 +0200

    Combine migrations

--- db/migrate/20151123071517_create_online_stores.rb
+++ db/migrate/20151123071517_create_online_stores.rb
@@ -4,6 +4,7 @@ class CreateOnlineStores < ActiveRecord::Migration
       t.string :name
       t.string :type
       t.references :online_store_theme, index: true, foreign_key: true
+      t.references :company, index: true, foreign_key: true
 
       t.timestamps null: false
     end
--- db/migrate/20151123123807_add_company_to_online_stores.rb
+++
@@ -1,5 +0,0 @@
-class AddCompanyToOnlineStores < ActiveRecord::Migration
-  def change
-    add_reference :online_stores, :company, index: true, foreign_key: true
-  end
-end
