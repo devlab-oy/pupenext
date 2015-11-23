f72742f0636b8ddbfd2726d680896b9fa32d03c1
tiere <tommi@devlab.fi>
2015-11-23 14:14:30 +0200

    Include PupenextSingleTableInheritance

--- app/models/online_store.rb
+++ app/models/online_store.rb
@@ -1,4 +1,6 @@
 class OnlineStore < ActiveRecord::Base
+  include PupenextSingleTableInheritance
+
   belongs_to :theme, foreign_key: :online_store_theme_id, class_name: 'OnlineStoreTheme'
 
   has_and_belongs_to_many :products, join_table: :online_stores_products
@@ -7,5 +9,12 @@ class OnlineStore < ActiveRecord::Base
 
   def to_s
     name
+  end
+
+  def self.child_class_names
+    [
+      OnlineStore::Magento,
+      OnlineStore::Pupeshop
+    ].index_by(&:to_s)
   end
 end
