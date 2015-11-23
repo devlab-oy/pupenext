644a9c64508b3a37a9c11f0427a810230eb8d3c2
tiere <tommi@devlab.fi>
2015-11-23 09:43:01 +0200

    Add validations

--- app/models/online_store.rb
+++ app/models/online_store.rb
@@ -1,3 +1,5 @@
 class OnlineStore < ActiveRecord::Base
   belongs_to :theme, foreign_key: :online_store_theme_id, class_name: 'OnlineStoreTheme'
+
+  validates :name, presence: true, length: { in: 1..255 }
 end
--- app/models/online_store_theme.rb
+++ app/models/online_store_theme.rb
@@ -1,2 +1,3 @@
 class OnlineStoreTheme < ActiveRecord::Base
+  validates :name, presence: true, length: { in: 1..255 }
 end
