a22da79168d4fdcc5d61531d23110ee2cab109b1
Joni Kanerva <joni@devlab.fi>
2015-11-25 11:31:48 +0200

    make migrations reversable

--- db/migrate/20151120143426_laite_alter.rb
+++ db/migrate/20151120143426_laite_alter.rb
@@ -1,5 +1,9 @@
 class LaiteAlter < ActiveRecord::Migration
-  def change
+  def up
     change_column :laite, :valmistajan_sopimus_paattymispaiva, :date
+  end
+
+  def down
+    change_column :laite, :valmistajan_sopimus_paattymispaiva, :datetime
   end
 end
--- db/migrate/20151120143727_add_column_to_laskun_lisatiedot.rb
+++ db/migrate/20151120143727_add_column_to_laskun_lisatiedot.rb
@@ -1,9 +1,5 @@
 class AddColumnToLaskunLisatiedot < ActiveRecord::Migration
-  def up
+  def change
     add_column :laskun_lisatiedot, :sopimus_numero, :string, limit: 50, default: '', after: :sopimus_lisatietoja2
-  end
-
-  def down
-    remove_column :laskun_lisatiedot, :sopimus_numero
   end
 end
