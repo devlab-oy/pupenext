ffcce4f787df49909dd0583ba24d762f01610227
tiere <tommi@devlab.fi>
2015-12-08 13:06:04 +0200

    Add name search test

--- test/controllers/supplier_product_informations_controller_test.rb
+++ test/controllers/supplier_product_informations_controller_test.rb
@@ -9,4 +9,11 @@ class SupplierProductInformationsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+
+  test 'searching with name works' do
+    get :index, product_name: 'amb'
+
+    assert_select 'td', { count: 1, text: 'Tramboline' }
+    assert_select 'td', { count: 0, text: 'Chair' }
+  end
 end
