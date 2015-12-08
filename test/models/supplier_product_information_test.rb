29f43c37382ff1a972a01399454f1ee88270c4a0
tiere <tommi@devlab.fi>
2015-12-08 10:32:07 +0200

    Add first tests

--- test/models/supplier_product_information_test.rb
+++ test/models/supplier_product_information_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class SupplierProductInformationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures %w(
+    supplier_product_informations
+  )
+
+  setup do
+    @one = supplier_product_informations(:one)
+    @two = supplier_product_informations(:two)
+  end
+
+  test 'fixtures are valid' do
+    assert @one.valid?, @one.errors.full_messages
+    assert @two.valid?, @two.errors.full_messages
+  end
 end
