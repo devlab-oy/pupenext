187a53b3f5c3fdfae8bacac90c9b0901498ab679
tiere <tommi@devlab.fi>
2015-12-08 13:05:44 +0200

    Make supplier product informations searchable by product name

--- app/controllers/supplier_product_informations_controller.rb
+++ app/controllers/supplier_product_informations_controller.rb
@@ -1,4 +1,19 @@
 class SupplierProductInformationsController < ApplicationController
+  include ColumnSort
+
   def index
+    @supplier_product_informations = SupplierProductInformation.search_like(search_params)
+  end
+
+  private
+
+  def searchable_columns
+    %i(
+      product_name
+    )
+  end
+
+  def sortable_columns
+    searchable_columns
   end
 end
