583a4d93bd6245b6f8a150c29c351de3e49b8143
tiere <tommi@devlab.fi>
2015-12-08 13:19:03 +0200

    Test searching with product id works

--- test/controllers/supplier_product_informations_controller_test.rb
+++ test/controllers/supplier_product_informations_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SupplierProductInformationsControllerTest < ActionController::TestCase
+  fixtures %i(
+    supplier_product_informations
+  )
+
   setup do
     login users(:bob)
   end
@@ -10,10 +14,17 @@ class SupplierProductInformationsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test 'searching with name works' do
+  test 'searching with product name works' do
     get :index, product_name: 'amb'
 
     assert_select 'td', { count: 1, text: 'Tramboline' }
     assert_select 'td', { count: 0, text: 'Chair' }
+  end
+
+  test 'searching with product id works' do
+    get :index, product_id: '2'
+
+    assert_select 'td', { count: 1, text: 'Chair' }
+    assert_select 'td', { count: 0, text: 'Tramboline' }
   end
 end
