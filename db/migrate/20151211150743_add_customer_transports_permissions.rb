c715c8a0719fb1b0cbbaa834f8e805c96058a8b1
Sami Luukkonen <sami@devlab.fi>
2015-12-14 10:37:47 +0200

    use permission helper in migration

--- db/migrate/20151211150743_add_customer_transports_permissions.rb
+++ db/migrate/20151211150743_add_customer_transports_permissions.rb
@@ -1,63 +1,15 @@
 class AddCustomerTransportsPermissions < ActiveRecord::Migration
   def up
-    Company.find_each do |company|
-      Current.company = company.yhtio
-
-      # Luodaan valikko
-      Permission.create!(
-        kuka: '',
-        sovellus: 'Ylläpito',
-        nimi: 'pupenext/customer_transports',
-        nimitys: 'Asiakkaiden tiedonsiirtoyhteydet',
-        laatija: 'admin',
-        luontiaika: DateTime.now,
-        muuttaja: 'admin',
-        muutospvm: DateTime.now,
-        jarjestys: 620
-      )
-
-      # Lisätään Admin profiiliin
-      Permission.create!(
-        kuka: 'Admin profiili',
-        profiili: 'Admin profiili',
-        paivitys: 1,
-        sovellus: 'Ylläpito',
-        nimi: 'pupenext/customer_transports',
-        nimitys: 'Asiakkaiden tiedonsiirtoyhteydet',
-        laatija: 'admin',
-        luontiaika: DateTime.now,
-        muuttaja: 'admin',
-        muutospvm: DateTime.now,
-        jarjestys: 620
-      )
-
-      # Lisätään Adminille käyttöoikeus
-      admin = User.find_by(kuka: 'admin')
-
-      if admin
-        Permission.create!(
-          kuka: 'admin',
-          user_id: admin.tunnus,
-          profiili: 'Admin profiili',
-          paivitys: 1,
-          sovellus: 'Ylläpito',
-          nimi: 'pupenext/customer_transports',
-          nimitys: 'Asiakkaiden tiedonsiirtoyhteydet',
-          laatija: 'admin',
-          luontiaika: DateTime.now,
-          muuttaja: 'admin',
-          muutospvm: DateTime.now,
-          jarjestys: 145
-        )
-      end
-    end
+    PermissionHelper.add_item(
+      program: 'Ylläpito',
+      name: 'Asiakkaiden tiedonsiirtoyhteydet',
+      uri: 'pupenext/customer_transports'
+    )
   end
 
   def down
-    Company.find_each do |company|
-      Current.company = company.yhtio
-
-      Permission.where(nimi: 'pupenext/customer_transports').delete_all
-    end
+    PermissionHelper.remove_all(
+      uri: 'pupenext/customer_transports'
+    )
   end
 end
