92dfc1c20ccc0903292039de3a84878e9ef4722a
tiere <tommi@devlab.fi>
2015-12-15 16:13:43 +0200

    Refactor transfer logic to own class

---
+++ app/classes/supplier_product_information_transfer.rb
@@ -0,0 +1,67 @@
+class SupplierProductInformationTransfer
+  def initialize(params, supplier:)
+    @transferable                  = params.select { |_k, v| v[:transfer] == '1' }
+    @supplier_product_informations = SupplierProductInformation.find(@transferable.keys)
+    @supplier                      = Supplier.find(supplier)
+  end
+
+  def transfer
+    duplicates = []
+
+    @supplier_product_informations.each do |s|
+      duplicate_products = Product.where('tuoteno = ? OR eankoodi = ?',
+                                         s.manufacturer_part_number,
+                                         s.manufacturer_ean)
+      if duplicate_products.present?
+        duplicates << s
+        next
+      end
+
+      extra_attributes = @transferable[s.id.to_s]
+
+      s.update(
+        p_price_update: extra_attributes[:toimittajan_ostohinta],
+        p_qty_update:   extra_attributes[:toimittajan_saldo]
+      )
+
+      product_params = {
+        alv:      24,
+        eankoodi: s.manufacturer_ean,
+        nimitys:  s.product_name,
+        tuoteno:  s.manufacturer_part_number
+      }
+
+      if extra_attributes[:tuotemerkki]
+        product_params[:brand] = Product::Brand.find(extra_attributes[:tuotemerkki])
+      end
+
+      if extra_attributes[:osasto]
+        product_params[:category] = Product::Category.find(extra_attributes[:osasto])
+      end
+
+      if extra_attributes[:nakyvyys]
+        product_params[:nakyvyys] = extra_attributes[:nakyvyys]
+      end
+
+      if extra_attributes[:status]
+        product_params[:status] = Product::Status.find(extra_attributes[:status])
+      end
+
+      if extra_attributes[:try]
+        product_params[:subcategory] = Product::Subcategory.find(extra_attributes[:try])
+      end
+
+      s.create_product(product_params)
+
+      @supplier.product_suppliers.create(
+        tehdas_saldo:            s.available_quantity,
+        tehdas_saldo_paivitetty: Time.now,
+        toim_nimitys:            s.product_name,
+        toim_tuoteno:            s.product_id,
+        tuoteno:                 s.manufacturer_part_number
+      )
+    end
+
+    duplicates
+  end
+end
--- app/controllers/supplier_product_informations_controller.rb
+++ app/controllers/supplier_product_informations_controller.rb
@@ -5,59 +5,21 @@ class SupplierProductInformationsController < ApplicationController
     session[:supplier] = params[:supplier] if params[:supplier].present?
 
     if session[:supplier].present? && search_params.present?
-      @supplier                      = Supplier.find(session[:supplier])
-      @supplier_product_informations = @supplier.supplier_product_informations.search_like(search_params)
+      @supplier = Supplier.find(session[:supplier])
+
+      @supplier_product_informations = @supplier
+                                       .supplier_product_informations
+                                       .search_like(search_params)
     else
       @supplier_product_informations = SupplierProductInformation.none
     end
   end
 
   def transfer
-    to_transfer = supplier_product_informations_params.select { |_k, v| v[:transfer] == '1' }
+    supplier_product_information_transfer = SupplierProductInformationTransfer.new(supplier_product_informations_params,
+                                                                                   supplier: session[:supplier])
 
-    @supplier_product_informations = SupplierProductInformation.find(to_transfer.keys)
-
-    supplier = Supplier.find(session[:supplier])
-
-    duplicates = []
-
-    @supplier_product_informations.each do |s|
-      duplicate_products = Product.where('tuoteno = ? OR eankoodi = ?',
-                                         s.manufacturer_part_number,
-                                         s.manufacturer_ean)
-      if duplicate_products.present?
-        duplicates << s
-        next
-      end
-
-      extra_attributes = to_transfer[s.id.to_s]
-
-      s.update(p_price_update: extra_attributes[:toimittajan_ostohinta],
-               p_qty_update: extra_attributes[:toimittajan_saldo])
-
-      product_params = {
-        alv:         24,
-        eankoodi:    s.manufacturer_ean,
-        nimitys:     s.product_name,
-        tuoteno:     s.manufacturer_part_number
-      }
-
-      product_params[:brand]       = Product::Brand.find(extra_attributes[:tuotemerkki]) if extra_attributes[:tuotemerkki]
-      product_params[:category]    = Product::Category.find(extra_attributes[:osasto])   if extra_attributes[:osasto]
-      product_params[:nakyvyys]    = extra_attributes[:nakyvyys]                         if extra_attributes[:nakyvyys]
-      product_params[:status]      = Product::Status.find(extra_attributes[:status])     if extra_attributes[:status]
-      product_params[:subcategory] = Product::Subcategory.find(extra_attributes[:try])   if extra_attributes[:try]
-
-      s.create_product(product_params)
-
-      supplier.product_suppliers.create(
-        tehdas_saldo:            s.available_quantity,
-        tehdas_saldo_paivitetty: Time.now,
-        toim_nimitys:            s.product_name,
-        toim_tuoteno:            s.product_id,
-        tuoteno:                 s.manufacturer_part_number
-      )
-    end
+    duplicates = supplier_product_information_transfer.transfer
 
     unless duplicates.present?
       return redirect_to supplier_product_informations_url, notice: t('.success')
