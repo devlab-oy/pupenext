64032c47e667a03c6ea2538f5f124cdfa9e77aa4
Sami Luukkonen <sami@devlab.fi>
2016-01-05 10:24:03 +0200

    product sales import

---
+++ app/classes/import/product_sales.rb
@@ -0,0 +1,107 @@
+class Import::ProductSales
+  include Import::Base
+
+  def initialize(company_id:, user_id:, filename:)
+    Current.company = Company.find company_id
+    Current.user = User.find user_id
+
+    @file = setup_file filename
+  end
+
+  def import
+    first_row = true
+
+    spreadsheet.each do |spreadsheet_row|
+
+      # create hash of the row (defined in Import::Base)
+      excel_row = row_to_hash spreadsheet_row
+
+      if first_row
+        response.add_headers names: excel_row.values
+        first_row = false
+        next
+      end
+
+      row = Row.new excel_row
+
+      number = row.product_or_customer_number
+      quantity = row.values['kpl']
+
+      number = Integer(number) rescue next
+
+      if quantity.present?
+        row.set_asiakasnro(number)
+      else
+        row.set_product(number)
+      end
+
+      errors = []
+
+      if row.product_raw.present? && row.product.nil?
+        errors << I18n.t('errors.import.product_not_found', product: row.product_raw)
+      elsif row.customer_raw.present? && row.customer.nil?
+        errors << I18n.t('errors.import.customer_not_found', customer: row.customer_raw)
+      # elsif !row.create
+      #   errors << row.keyword.errors.full_messages
+      end
+
+      response.add_row columns: excel_row.values, errors: errors.flatten
+    end
+
+    response
+  end
+
+  private
+
+    def response
+      @response ||= Import::Response.new
+    end
+end
+
+class Import::ProductSales::Row
+  def initialize(hash)
+    @hash     = hash.dup
+  end
+
+  def product
+    return unless @tuoteno.present?
+
+    @product ||= Product.find_by tuoteno: @tuoteno
+  end
+
+  def product_raw
+    @tuoteno
+  end
+
+  def set_product(tuoteno)
+    @tuoteno = tuoteno
+  end
+
+  def customer
+    return unless @asiakasnro.present?
+
+    @customer ||= Customer.find_by asiakasnro: @asiakasnro
+  end
+
+  def customer_raw
+    @asiakasnro
+  end
+
+  def set_asiakasnro(asiakasnro)
+    @asiakasnro = asiakasnro
+  end
+
+  def price
+    values['myynti eur']
+  end
+
+  def product_or_customer_number
+    values['tuote/asiakas'].split(' ').first
+  end
+
+  def values
+    hash = @hash
+    hash.each { |k, v| hash[k] = '' if hash[k].nil? }
+    hash
+  end
+end
---
+++ test/classes/import/product_sales_test.rb
@@ -0,0 +1,103 @@
+require 'test_helper'
+
+class Import::ProductSalesTest < ActiveSupport::TestCase
+  include SpreadsheetsHelper
+
+  fixtures %w(
+    products
+    customers
+  )
+
+  setup do
+    @user = users(:joe).id
+    @company = users(:joe).company.id
+
+    @helmet = products :helmet
+    @helmet.tuoteno = '12345'
+    @helmet.save
+
+    @hammer = products :hammer
+    @hammer.tuoteno = '67890'
+    @hammer.save
+
+    @customer = customers :stubborn_customer
+
+    @filename = create_xlsx([
+      ['Tuote/Asiakas',                                           'Kpl', 'Myynti EUR' ],
+      ['Yhteensä',                                                '',    123000       ],
+      ["#{@helmet.tuoteno} #{@helmet.osasto} #{@helmet.nimitys}", '',    23000        ],
+      ["#{@customer.asiakasnro} #{@customer.nimi}",               10,    23000        ],
+      ["#{@hammer.tuoteno} #{@hammer.osasto} #{@hammer.nimitys}", '',    100000       ],
+      ["#{@customer.asiakasnro} #{@customer.nimi}",               23,    100000       ],
+      ['Yhteensä',                                                '',    123000       ],
+    ])
+  end
+
+  test 'imports sales and returns response' do
+    sales = Import::ProductSales.new company_id: @company, user_id: @user, filename: @filename
+
+    # assert_difference 'Product::Keyword.count', 3 do
+      response = sales.import
+      assert_equal Import::Response, response.class
+    # end
+  end
+
+  test 'adding with invalid data should fail' do
+    filename = create_xlsx([
+      ['Tuote/Asiakas',                            'Kpl', 'Myynti EUR' ],
+      ['Yhteensä',                                 '',    123000       ],
+      ["999 #{@helmet.osasto} #{@helmet.nimitys}", '',    23000        ],
+      ["666 #{@customer.nimi}",                    10,    23000        ],
+      ['Yhteensä',                                 '',    123000       ],
+    ])
+
+    sales = Import::ProductSales.new company_id: @company, user_id: @user, filename: filename
+
+    response = sales.import
+    assert_equal 'Tuotetta "999" ei löytynyt!', response.rows.first.errors.first
+    assert_equal 'Asiakasta "666" ei löytynyt!', response.rows.second.errors.first
+  end
+
+  test 'errors are correct' do
+    # Let's store one correct product and customer
+    filename = create_xlsx([
+      ['Tuote/Asiakas',                                           'Kpl', 'Myynti EUR' ],
+      ['Yhteensä',                                                '',    123000       ],
+      ["#{@helmet.tuoteno} #{@helmet.osasto} #{@helmet.nimitys}", '',    23000        ],
+      ["#{@customer.asiakasnro} #{@customer.nimi}",               10,    23000        ],
+    ])
+
+    sales = Import::ProductSales.new company_id: @company, user_id: @user, filename: filename
+    result = sales.import
+    assert_equal 0, result.rows.first.errors.count
+
+    # Try adding with incorrect product
+    filename = create_xlsx([
+      ['Tuote/Asiakas',                            'Kpl', 'Myynti EUR' ],
+      ['Yhteensä',                                 '',    123000       ],
+      ["666 #{@helmet.osasto} #{@helmet.nimitys}", '',    23000        ],
+    ])
+
+    # We get only one error
+    sales = Import::ProductSales.new company_id: @company, user_id: @user, filename: filename
+    result = sales.import
+    assert_equal 1, result.rows.first.errors.count
+    error = I18n.t('errors.import.product_not_found', product: '666')
+    assert_equal error, result.rows.first.errors.first
+
+    # Try adding with incorrect customer
+    filename = create_xlsx([
+      ['Tuote/Asiakas',                                           'Kpl', 'Myynti EUR' ],
+      ['Yhteensä',                                                '',    123000       ],
+      ["#{@helmet.tuoteno} #{@helmet.osasto} #{@helmet.nimitys}", '',    23000        ],
+      ["999 #{@customer.nimi}",                                   10,    23000        ],
+    ])
+
+    # We get only one error
+    sales = Import::ProductSales.new company_id: @company, user_id: @user, filename: filename
+    result = sales.import
+    assert_equal 1, result.rows.second.errors.count
+    error = I18n.t('errors.import.customer_not_found', customer: '999')
+    assert_equal error, result.rows.second.errors.first
+  end
+end
