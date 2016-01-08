e935d67e5fba1de7312edcddf5d812aa53ff3e63
Tommi Järvinen <tommi@devlab.fi>
2016-01-08 11:27:11 +0200

    Add manufacturer filter UI

--- app/helpers/supplier_product_informations_helper.rb
+++ app/helpers/supplier_product_informations_helper.rb
@@ -2,4 +2,12 @@ module SupplierProductInformationsHelper
   def show_table?
     params[:supplier].present? || session[:supplier].present?
   end
+
+  def manufacturer_options
+    SupplierProductInformation
+      .order(:manufacturer_name)
+      .pluck(:manufacturer_name)
+      .map { |s| [s, "@#{s}"] }
+      .uniq
+  end
 end
