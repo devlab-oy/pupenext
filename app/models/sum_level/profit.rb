ec52fe66c451d19e4598ce082b9a82041cdd343c
Joni Kanerva <joni@devlab.fi>
2016-01-11 12:17:42 +0200

    fix relation

--- app/models/sum_level/profit.rb
+++ app/models/sum_level/profit.rb
@@ -1,4 +1,6 @@
 class SumLevel::Profit < SumLevel
+  has_many :accounts, primary_key: :taso, foreign_key: :tulosseuranta_taso
+
   validates :taso, numericality: { only_integer: true }
 
   # Rails requires sti_name method to return type column (tyyppi) value
