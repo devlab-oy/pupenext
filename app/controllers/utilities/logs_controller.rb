fe393d98143202370d56e9e235ef3a5126e84315
Joni Kanerva <joni@devlab.fi>
2016-02-16 12:44:20 +0200

    always download log, inline will freeze browsers

--- app/controllers/utilities/logs_controller.rb
+++ app/controllers/utilities/logs_controller.rb
@@ -6,7 +6,7 @@ class Utilities::LogsController < ApplicationController
   end
 
   def show
-    send_file "#{PATH}/#{params_basename}.log", type: 'text/plain', disposition: 'inline'
+    send_file "#{PATH}/#{params_basename}.log", type: 'text/plain'
   end
 
   private
