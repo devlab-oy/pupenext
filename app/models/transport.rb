da35eab6be309b57c88c008ac0ed8f7593978a96
Joni Kanerva <joni@devlab.fi>
2016-03-08 14:08:53 +0200

    add encoding enum

--- app/models/transport.rb
+++ app/models/transport.rb
@@ -7,4 +7,9 @@ class Transport < ActiveRecord::Base
   validates :username, presence: true
   validates :path, presence: true
   validates :port, numericality: { only_integer: true }, allow_blank: true
+
+  enum encoding: {
+    utf8: 0,
+    iso8859_15: 1,
+  }
 end
--- test/models/transport_test.rb
+++ test/models/transport_test.rb
@@ -20,4 +20,21 @@ class TransportTest < ActiveSupport::TestCase
     assert_equal Customer, @two.transportable.class
     assert_equal Company, @three.transportable.class
   end
+
+  test 'encoding values' do
+    @one.encoding = :utf8
+    assert @one.valid?
+    assert @one.utf8?
+    refute @one.iso8859_15?
+
+    @one.encoding = :iso8859_15
+    assert @one.valid?
+    assert @one.iso8859_15?
+    refute @one.utf8?
+
+    @one.encoding = nil
+    assert @one.valid?
+
+    assert_raises(ArgumentError) { @one.encoding = :foo }
+  end
 end
