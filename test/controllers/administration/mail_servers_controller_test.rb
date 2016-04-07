296c19736f00ef5f59af4294c71e4ac7580f009b
Tommi Järvinen <tommi@devlab.fi>
2016-04-07 11:34:06 +0300

    Add table headers

--- test/controllers/administration/mail_servers_controller_test.rb
+++ test/controllers/administration/mail_servers_controller_test.rb
@@ -11,5 +11,11 @@ class Administration::MailServersControllerTest < ActionController::TestCase
     assert_response :success
 
     assert_select 'h1', I18n.t('administration.mail_servers.index.header')
+
+    headers = css_select('table#mail_servers > thead > tr > th')
+
+    assert_equal headers[0].content, MailServer.human_attribute_name(:imap_server)
+    assert_equal headers[1].content, MailServer.human_attribute_name(:smtp_server)
+    assert_equal headers[2].content, MailServer.human_attribute_name(:processing_type)
   end
 end
