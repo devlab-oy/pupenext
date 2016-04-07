ec9ebdb15fb0caa576447a01056c89e4e6dbff41
Tommi Järvinen <tommi@devlab.fi>
2016-04-07 11:03:49 +0300

    Add validations

--- app/models/mail_server.rb
+++ app/models/mail_server.rb
@@ -1,3 +1,15 @@
 class MailServer < ActiveRecord::Base
-  belongs_to :company
+  belongs_to :company, required: true
+
+  validates :imap_server,     presence: true
+  validates :imap_username,   presence: true
+  validates :imap_password,   presence: true
+  validates :process_dir,     presence: true
+  validates :done_dir,        presence: true
+  validates :processing_type, presence: true, inclusion: { in: %w(huutokauppa) }
+
+  with_options if: 'smtp_server.present?' do |o|
+    o.validates :smtp_username, presence: true
+    o.validates :smtp_password, presence: true
+  end
 end
