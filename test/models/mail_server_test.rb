395e85ef4eacaba4b366120193565d597a009499
Tommi Järvinen <tommi@devlab.fi>
2016-04-07 16:02:55 +0300

    Use fixtures

--- test/models/mail_server_test.rb
+++ test/models/mail_server_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class MailServerTest < ActiveSupport::TestCase
+  fixtures %w(
+    mail_servers
+  )
+
   test 'fixtures are valid' do
     MailServer.all.each do |mail_server|
       assert mail_server.valid?, mail_server.errors.full_messages
