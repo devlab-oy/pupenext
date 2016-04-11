0a555792b26733494e47d72c9919c66a150e6ee6
Tommi Järvinen <tommi@devlab.fi>
2016-04-11 16:38:31 +0300

    Create incoming mails from all emails in configured process dir and then move the emails to configured done dir

--- app/jobs/incoming_mail_job.rb
+++ app/jobs/incoming_mail_job.rb
@@ -2,6 +2,20 @@ class IncomingMailJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    MailServer.all.each do |mail_server|
+      Mail.defaults do
+        retriever_method :imap, address:    mail_server.imap_server,
+                                port:       993,
+                                user_name:  mail_server.imap_username,
+                                password:   mail_server.imap_password,
+                                enable_ssl: true
+      end
+
+      Mail.find(mailbox: "INBOX.#{mail_server.process_dir}") do |message, connection, message_uid|
+        mail_server.incoming_mails.create(raw_source: message.raw_source)
+
+        connection.uid_move(message_uid, "INBOX.#{mail_server.done_dir}")
+      end
+    end
   end
 end
