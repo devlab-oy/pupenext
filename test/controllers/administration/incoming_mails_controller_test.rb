daa26b69695f7bf80e0b8a3c14980ae864a6adee
Tommi Järvinen <tommi@devlab.fi>
2016-04-12 13:32:54 +0300

    Test that table is rendered with correct values

--- test/controllers/administration/incoming_mails_controller_test.rb
+++ test/controllers/administration/incoming_mails_controller_test.rb
@@ -11,5 +11,16 @@ class Administration::IncomingMailsControllerTest < ActionController::TestCase
     assert_response :success
 
     assert_select 'h1', I18n.t('administration.incoming_mails.index.header')
+
+    IncomingMail.all.each_with_index do |incoming_mail, index|
+      row = css_select('#incoming_mails tbody tr')[index]
+
+      processed_at = incoming_mail.processed_at ? I18n.l(incoming_mail.processed_at) : ''
+
+      assert_equal incoming_mail.mail_server.to_s,    css_select(row, 'td')[0].content
+      assert_equal processed_at,                      css_select(row, 'td')[1].content
+      assert_equal incoming_mail.status.to_s,         css_select(row, 'td')[2].content
+      assert_equal incoming_mail.status_message.to_s, css_select(row, 'td')[3].content
+    end
   end
 end
