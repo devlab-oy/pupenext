5d4c2236d40eadf3df02f90ab3f04732c6e8e3f3
Tommi Järvinen <tommi@devlab.fi>
2016-04-12 13:57:09 +0300

    Implement sorting and searching and inherit from AdministrationController

--- app/controllers/administration/incoming_mails_controller.rb
+++ app/controllers/administration/incoming_mails_controller.rb
@@ -1,5 +1,15 @@
-class Administration::IncomingMailsController < ApplicationController
+class Administration::IncomingMailsController < AdministrationController
   def index
-    @incoming_mails = IncomingMail.all
+    @incoming_mails = IncomingMail.search_like(search_params).order(order_params)
   end
+
+  private
+
+    def sortable_columns
+      [:mail_server_id, :processed_at, :status, :status_message]
+    end
+
+    def searchable_columns
+      sortable_columns
+    end
 end
