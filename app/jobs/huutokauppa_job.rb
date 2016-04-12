61d8b1548aae9228eb84e6f8de1707a3e8339e77
Tommi Järvinen <tommi@devlab.fi>
2016-04-12 11:27:58 +0300

    Add first version of HuutokauppaJob

--- app/jobs/huutokauppa_job.rb
+++ app/jobs/huutokauppa_job.rb
@@ -1,7 +1,12 @@
 class HuutokauppaJob < ActiveJob::Base
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(id:)
+    incoming_mail = IncomingMail.find(id)
+
+    incoming_mail.update(
+      processed_at: Time.now,
+      status: :ok
+    )
   end
 end
