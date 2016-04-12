4090acb0185ff5bcdedc606ffaf07a7ddf9e206d
Tommi Järvinen <tommi@devlab.fi>
2016-04-12 12:55:07 +0300

    Add header

--- test/controllers/administration/incoming_mails_controller_test.rb
+++ test/controllers/administration/incoming_mails_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class Administration::IncomingMailsControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
+  setup do
+    login users(:bob)
   end
 
+  test 'index works' do
+    get :index
+
+    assert_response :success
+
+    assert_select 'h1', I18n.t('administration.incoming_mails.index.header')
+  end
 end
