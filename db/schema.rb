9abc2e2ad915fbae90da20e373b5eb5a337ce122
Joni Kanerva <joni@devlab.fi>
2016-04-20 13:37:33 +0300

    lisätään verkkokauppa_nakyvyys yhteyshenkilöön

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412105950) do
+ActiveRecord::Schema.define(version: 20160420103408) do
 
   create_table "abc_aputaulu", primary_key: "tunnus", force: :cascade do |t|
     t.string   "yhtio",              limit: 5,                            default: "",  null: false
@@ -3360,6 +3360,7 @@ ActiveRecord::Schema.define(version: 20160412105950) do
     t.string   "oletusyhteyshenkilo",    limit: 1,     default: "", null: false
     t.string   "aktivointikuittaus",     limit: 1,     default: "", null: false
     t.string   "verkkokauppa_salasana",  limit: 255
+    t.string   "verkkokauppa_nakyvyys",  limit: 255
     t.string   "laatija",                limit: 50,    default: "", null: false
     t.datetime "luontiaika",                                        null: false
     t.datetime "muutospvm",                                         null: false
