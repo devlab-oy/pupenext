22a667b69d5f1e400366f0157f9d2ed01115f269
Tommi Järvinen <tommi@devlab.fi>
2016-04-25 16:43:24 +0300

    Add SalesOrder::Draft#mark_as_done

--- test/models/sales_order/draft_test.rb
+++ test/models/sales_order/draft_test.rb
@@ -1,13 +1,34 @@
+require 'minitest/mock'
 require 'test_helper'
 
 class SalesOrder::DraftTest < ActiveSupport::TestCase
   fixtures %w(
     sales_order/drafts
+    users
   )
+
+  setup do
+    Current.user = users(:bob)
+  end
 
   test 'fixtures are valid' do
     SalesOrder::Draft.all.each do |draft|
       assert draft.valid?
+    end
+  end
+
+  test '#mark_as_done' do
+    SalesOrder::Draft.all.each do |draft|
+      mark_as_done = proc do
+        draft.tila = 'L'
+        draft.save(validate: false)
+      end
+
+      LegacyMethods.stub(:pupesoft_function, mark_as_done) do
+        assert_difference 'SalesOrder::Draft.count', -1 do
+          draft.mark_as_done
+        end
+      end
     end
   end
 end
