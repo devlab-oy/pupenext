fe2d14d4868513092f40679bacdd201fb2cf9547
Tommi Järvinen <tommi@devlab.fi>
2016-04-27 13:35:11 +0300

    Add sales_orders association to DeliveryMethod

--- test/models/delivery_method_test.rb
+++ test/models/delivery_method_test.rb
@@ -4,19 +4,23 @@ class DeliveryMethodTest < ActiveSupport::TestCase
   fixtures %w(
     customers
     delivery_methods
+    sales_order/orders
   )
 
   setup do
     @kaukokiito = delivery_methods(:kaukokiito)
     @kiitolinja = delivery_methods(:kiitolinja)
+    @nouto      = delivery_methods(:nouto)
   end
 
   test 'fixtures are valid' do
     assert @kaukokiito.valid?
     assert @kiitolinja.valid?
+    assert @nouto.valid?
   end
 
   test 'relations' do
     assert @kaukokiito.customers.count > 0
+    assert @nouto.sales_orders.count > 0
   end
 end
