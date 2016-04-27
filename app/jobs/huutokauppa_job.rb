b5a6f778e553949243fbe1dc8c1f7650568ac0a4
Tommi Järvinen <tommi@devlab.fi>
2016-04-27 12:30:49 +0300

    Extract customer and product updating to method

--- app/jobs/huutokauppa_job.rb
+++ app/jobs/huutokauppa_job.rb
@@ -2,24 +2,30 @@ class HuutokauppaJob < ActiveJob::Base
   queue_as :huutokauppa
 
   def perform(id:)
-    incoming_mail = IncomingMail.find(id)
+    @incoming_mail = IncomingMail.find(id)
 
-    Current.company = incoming_mail.company
+    Current.company = @incoming_mail.company
     Current.user    = Current.company.users.find_by!(kuka: 'admin')
 
-    huutokauppa_mail = HuutokauppaMail.new(incoming_mail.raw_source)
+    @huutokauppa_mail = HuutokauppaMail.new(@incoming_mail.raw_source)
 
-    case huutokauppa_mail.type
+    case @huutokauppa_mail.type
     when :offer_accepted, :offer_automatically_accepted
-      customer = huutokauppa_mail.find_customer || huutokauppa_mail.create_customer
+      update_order_customer_and_product_info
+    end
+  end
 
-      huutokauppa_mail.update_order_customer_info
-      huutokauppa_mail.update_order_product_info
+  private
 
-      incoming_mail.update(
+    def update_order_customer_and_product_info
+      customer = @huutokauppa_mail.find_customer || @huutokauppa_mail.create_customer
+
+      @huutokauppa_mail.update_order_customer_info
+      @huutokauppa_mail.update_order_product_info
+
+      @incoming_mail.update(
         processed_at: Time.now,
         status: :ok,
       )
     end
-  end
 end
