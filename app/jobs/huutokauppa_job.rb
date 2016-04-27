991445cb264d71f72fc314a53b9fbfd58ddd8ca3
Tommi Järvinen <tommi@devlab.fi>
2016-04-27 12:00:16 +0300

    Process accepted offer emails in HuutokauppaJob
    Process offer_accepted and offer_automatically_accepted type emails correctly in HuutokauppaJob

--- app/jobs/huutokauppa_job.rb
+++ app/jobs/huutokauppa_job.rb
@@ -7,9 +7,19 @@ class HuutokauppaJob < ActiveJob::Base
     Current.company = incoming_mail.company
     Current.user    = Current.company.users.find_by!(kuka: 'admin')
 
-    incoming_mail.update(
-      processed_at: Time.now,
-      status: :ok
-    )
+    huutokauppa_mail = HuutokauppaMail.new(incoming_mail.raw_source)
+
+    case huutokauppa_mail.type
+    when :offer_accepted, :offer_automatically_accepted
+      customer = huutokauppa_mail.find_customer || huutokauppa_mail.create_customer
+
+      huutokauppa_mail.update_order_customer_info
+      huutokauppa_mail.update_order_product_info
+
+      incoming_mail.update(
+        processed_at: Time.now,
+        status: :ok,
+      )
+    end
   end
 end
--- test/jobs/huutokauppa_job_test.rb
+++ test/jobs/huutokauppa_job_test.rb
@@ -4,6 +4,8 @@ class HuutokauppaJobTest < ActiveJob::TestCase
   fixtures %w(
     incoming_mails
     mail_servers
+    sales_order/drafts
+    sales_order/rows
   )
 
   test 'job is enqueued correctly' do
@@ -12,5 +14,63 @@ class HuutokauppaJobTest < ActiveJob::TestCase
     HuutokauppaJob.perform_later(id: incoming_mails(:three).id)
 
     assert_enqueued_jobs 1
+  end
+
+  test 'customer and product info are updated correctly to order when customer is found' do
+    incoming_mail = incoming_mails(:three)
+
+    incoming_mail.raw_source = huutokauppa_email(:offer_accepted_1)
+    incoming_mail.save!
+
+    assert_no_difference 'Customer.count' do
+      HuutokauppaJob.perform_now(id: incoming_mail.id)
+    end
+
+    sales_order = SalesOrder::Draft.find_by!(viesti: 277_075)
+
+    assert_equal 'Testi Testit Testitestit', sales_order.nimi
+    assert_equal 665, sales_order.rows.first.rivihinta
+
+    incoming_mail.raw_source = huutokauppa_email(:offer_automatically_accepted_1)
+    incoming_mail.save!
+
+    assert_no_difference 'Customer.count' do
+      HuutokauppaJob.perform_now(id: incoming_mail.id)
+    end
+
+    sales_order = SalesOrder::Draft.find_by!(viesti: 270_265)
+
+    assert_equal 'Test-testi Testite', sales_order.nimi
+    assert_equal 300, sales_order.rows.first.rivihinta
+  end
+
+  test 'customer is created and customer and product info are updated correctly to order when customer is not found' do
+    Customer.delete_all
+
+    incoming_mail = incoming_mails(:three)
+
+    incoming_mail.raw_source = huutokauppa_email(:offer_accepted_1)
+    incoming_mail.save!
+
+    assert_difference 'Customer.count' do
+      HuutokauppaJob.perform_now(id: incoming_mail.id)
+    end
+
+    sales_order = SalesOrder::Draft.find_by!(viesti: 277_075)
+
+    assert_equal 'Testi Testit Testitestit', sales_order.nimi
+    assert_equal 665, sales_order.rows.first.rivihinta
+
+    incoming_mail.raw_source = huutokauppa_email(:offer_automatically_accepted_1)
+    incoming_mail.save!
+
+    assert_difference 'Customer.count' do
+      HuutokauppaJob.perform_now(id: incoming_mail.id)
+    end
+
+    sales_order = SalesOrder::Draft.find_by!(viesti: 270_265)
+
+    assert_equal 'Test-testi Testite', sales_order.nimi
+    assert_equal 300, sales_order.rows.first.rivihinta
   end
 end
