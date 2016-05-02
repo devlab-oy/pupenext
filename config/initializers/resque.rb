bde981e54eb5fc28f9bd27836282dd65d2bc9cad
Joni Kanerva <joni@devlab.fi>
2016-05-02 13:34:45 +0300

    redis namespace on rails root dirikan nimi

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,4 @@
 Resque.redis = Redis.new(host: 'localhost', port: 6379)
+Resque.redis.namespace = Rails.application.root.basename.to_s
 Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
 # Resque.logger.formatter = Resque::VerboseFormatter.new
