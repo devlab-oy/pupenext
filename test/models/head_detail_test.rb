16380b336d6a4f4bbdbd8345f9167cc1f3b95bda
Tommi Järvinen <tommi@devlab.fi>
2016-05-02 11:17:33 +0300

    Add head association to HeadDetail

--- test/models/head_detail_test.rb
+++ test/models/head_detail_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class HeadDetailTest < ActiveSupport::TestCase
   fixtures %w(
     head_details
+    heads
   )
 
   test 'fixtures are valid' do
@@ -11,5 +12,10 @@ class HeadDetailTest < ActiveSupport::TestCase
     HeadDetail.all.each do |head_detail|
       assert head_detail.valid? head_detail.errors.full_messages
     end
+  end
+
+  test 'associations' do
+    assert_equal heads(:si_one), head_details(:one).head
+    assert_equal heads(:pi_H),   head_details(:two).head
   end
 end
