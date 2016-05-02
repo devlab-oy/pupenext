5618de51b47f0ffe58006f243813da5eedc856f0
Tommi Järvinen <tommi@devlab.fi>
2016-05-02 11:13:26 +0300

    Test that fixtures are valid

--- test/models/head_detail_test.rb
+++ test/models/head_detail_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class HeadDetailTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures %w(
+    head_details
+  )
+
+  test 'fixtures are valid' do
+    refute_empty HeadDetail.all
+
+    HeadDetail.all.each do |head_detail|
+      assert head_detail.valid? head_detail.errors.full_messages
+    end
+  end
 end
