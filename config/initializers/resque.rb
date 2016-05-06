475615877018060fbc3aff01047332dc1ef4d0b1
Joni Kanerva <joni@devlab.fi>
2016-05-06 12:03:50 +0300

    add resque logger

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,5 @@
 Resque.redis = Redis.new(host: 'localhost', port: 6379)
 Resque.redis.namespace = Rails.application.root.basename.to_s
 Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
+Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
 # Resque.logger.formatter = Resque::VerboseFormatter.new
