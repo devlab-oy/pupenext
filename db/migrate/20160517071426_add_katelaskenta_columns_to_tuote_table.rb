c86b147d760a73d78baa750d974eb851bba42542
Johan Tötterman <juppe@devlab.fi>
2016-06-10 16:28:04 +0300

    Inttikenttii

--- db/migrate/20160517071426_add_katelaskenta_columns_to_tuote_table.rb
+++ db/migrate/20160517071426_add_katelaskenta_columns_to_tuote_table.rb
@@ -1,7 +1,7 @@
 class AddKatelaskentaColumnsToTuoteTable < ActiveRecord::Migration
   def change
-    add_column :tuote, :myyntikate, :decimal, precision: 12, scale: 2, default: 0.0, null: false, after: :vihapvm
-    add_column :tuote, :myymalakate, :decimal, precision: 12, scale: 2, default: 0.0, null: false, after: :myyntikate
-    add_column :tuote, :nettokate, :decimal, precision: 12, scale: 2, default: 0.0, null: false, after: :myymalakate
+    add_column :tuote, :myyntikate, :integer, default: 0, null: false, after: :vihapvm
+    add_column :tuote, :myymalakate, :integer, default: 0, null: false, after: :myyntikate
+    add_column :tuote, :nettokate, :integer, default: 0, null: false, after: :myymalakate
   end
 end
