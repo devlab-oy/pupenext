2751f6a956d840cfffd96b8888d52af073051db2
Mika M <giefsufu@gmail.com>
2016-06-13 15:56:35 +0300

    test for campaign model

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,5 +3,8 @@ class Campaign < BaseModel
 
   belongs_to :company
 
+  validates :name, length: { maximum: 60 }
+  validates :description, length: { maximum: 255 }
+
   scope :active, -> { where(active: true) }
 end
