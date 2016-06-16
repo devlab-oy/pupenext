f39070fff38b0a99ea914421dea3a1f4df1a9f04
Tommi Järvinen <tommi@devlab.fi>
2016-06-16 11:08:56 +0300

    Extract company copy logic to CompanyCopier class

---
+++ app/classes/company_copier.rb
@@ -0,0 +1,103 @@
+class CompanyCopier
+  def self.copy(company:, yhtio:, nimi:)
+    raise 'Current company must be set' unless Current.company
+    raise 'Current user must be set'    unless Current.user
+
+    copied_company     = company.dup
+    copied_parameter   = company.parameter.dup
+    copied_currency    = company.currencies.first.dup
+    copied_menus       = company.menus.map(&:dup)
+    copied_profiles    = company.profiles.map(&:dup)
+    copied_user        = company.users.find_by!(kuka: 'admin').dup
+    copied_permissions = company.users.find_by!(kuka: 'admin').permissions.map(&:dup)
+    copied_sum_levels  = company.sum_levels.map(&:dup)
+    copied_accounts    = company.accounts.map(&:dup)
+    copied_keywords    = company.keywords.map(&:dup)
+
+    copied_company.update(
+      yhtio: yhtio,
+      konserni: '',
+      nimi: nimi,
+      laatija: Current.user.kuka,
+      luontiaika: Time.now,
+      muuttaja: Current.user.kuka,
+      muutospvm: Time.now,
+    )
+
+    Current.company = copied_company
+
+    copied_parameter.update(
+      company: copied_company,
+      finvoice_senderpartyid: '',
+      finvoice_senderintermediator: '',
+      verkkotunnus_vas: '',
+      verkkotunnus_lah: '',
+      verkkosala_vas: '',
+      verkkosala_lah: '',
+      lasku_tulostin: 0,
+      logo: '',
+      lasku_logo: '',
+      lasku_logo_positio: '',
+      lasku_logo_koko: 0,
+      laatija: Current.user.kuka,
+      luontiaika: Time.now,
+      muutospvm: Time.now,
+      muuttaja: Current.user.kuka,
+    )
+
+    copied_currency.update(
+      company: copied_company,
+    )
+
+    copied_menus.each do |menu|
+      menu.update(
+        company: copied_company,
+        laatija: Current.user.kuka,
+        luontiaika: Time.now,
+        muutospvm: Time.now,
+        muuttaja: Current.user.kuka,
+      )
+    end
+
+    copied_profiles.each do |profile|
+      profile.update(
+        company: copied_company,
+        laatija: Current.user.kuka,
+        luontiaika: Time.now,
+        muutospvm: Time.now,
+        muuttaja: Current.user.kuka,
+      )
+    end
+
+    copied_user.update(
+      company: copied_company,
+    )
+
+    copied_permissions.each do |permission|
+      permission.update(
+        company: copied_company,
+        user: copied_user,
+      )
+    end
+
+    copied_sum_levels.each do |sum_level|
+      sum_level.update(
+        company: copied_company,
+      )
+    end
+
+    copied_accounts.each do |account|
+      account.update(
+        company: copied_company,
+      )
+    end
+
+    copied_keywords.each do |keyword|
+      keyword.update(
+        company: copied_company,
+      )
+    end
+
+    copied_company
+  end
+end
---
+++ test/classes/company_copier_test.rb
@@ -0,0 +1,52 @@
+require 'test_helper'
+
+class CompanyCopierTest < ActiveSupport::TestCase
+  fixtures :all
+
+  test '.copy' do
+    Current.user = users(:bob)
+
+    copied_company = nil
+
+    assert_difference 'Company.count' do
+      assert_difference 'Parameter.unscoped.count' do
+        assert_difference 'Currency.unscoped.count' do
+          assert_difference 'Permission.unscoped.count', 40 do
+            assert_difference 'User.unscoped.count' do
+              assert_difference 'SumLevel.unscoped.count', 10 do
+                assert_difference 'Account.unscoped.count', 52 do
+                  assert_difference 'Keyword.unscoped.count', 29 do
+                    copied_company = CompanyCopier.copy(company: companies(:acme), yhtio: 95, nimi: 'Kala Oy')
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+
+    assert copied_company.persisted?
+
+    assert_equal 'FI', copied_company.maa
+
+    assert_empty copied_company.konserni
+
+    assert_equal '95', copied_company.yhtio
+    assert_equal 'Kala Oy', copied_company.nimi
+    assert_equal users(:bob).kuka, copied_company.laatija
+    assert_equal users(:bob).kuka, copied_company.muuttaja
+
+    Current.company = copied_company
+
+    refute_nil copied_company.parameter
+    refute_empty copied_company.currencies
+    refute_empty copied_company.menus
+    refute_empty copied_company.profiles
+    refute_empty copied_company.users
+    refute_empty copied_company.users.first.permissions
+    refute_empty copied_company.sum_levels
+    refute_empty copied_company.accounts
+    refute_empty copied_company.keywords
+  end
+end
