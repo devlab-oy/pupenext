fd0eba06200e9a0c93364e71f81939badb51a067
Tommi Järvinen <tommi@devlab.fi>
2016-06-17 15:00:11 +0300

    Add CompaniesController#copy and route

--- app/controllers/administration/companies_controller.rb
+++ app/controllers/administration/companies_controller.rb
@@ -1,2 +1,21 @@
 class Administration::CompaniesController < ApplicationController
+  def copy
+    company = Company.unscoped.find(params[:id])
+
+    copier = CompanyCopier.new(
+      yhtio: company_params[:yhtio],
+      nimi: company_params[:nimi],
+      company: company,
+    )
+
+    copied_company = copier.copy
+
+    render json: { company: { id: copied_company.id } }
+  end
+
+  private
+
+    def company_params
+      params.require(:company).permit(:yhtio, :nimi)
+    end
 end
--- test/controllers/administration/companies_controller_test.rb
+++ test/controllers/administration/companies_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class Administration::CompaniesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :all
+
+  setup do
+    login users(:bob)
+  end
+
+  test 'POST /companies/:id/copy' do
+    assert_difference 'Company.unscoped.count' do
+      post :copy, id: companies(:acme).id, company: { yhtio: 'testi', nimi: 'Testi Oy' }
+
+      assert_response :success
+    end
+
+    assert_equal Company.unscoped.last, Company.unscoped.find(json_response['company']['id'])
+  end
 end
