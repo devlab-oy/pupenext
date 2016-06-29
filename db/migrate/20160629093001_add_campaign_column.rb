0c59a6b3c1bf34cdaf2f19013cc0591cbc64fd0b
Mika M <giefsufu@gmail.com>
2016-06-29 12:29:14 +0300

    add campaign relation

---
+++ db/migrate/20160629093001_add_campaign_column.rb
@@ -0,0 +1,9 @@
+class AddCampaignColumn < ActiveRecord::Migration
+  def change
+    add_column :hinnasto,       :kampanja, :integer, limit: 4, default: 0, null: false, after: :yhtion_toimipaikka_id
+    add_column :asiakashinta,   :kampanja, :integer, limit: 4, default: 0, null: false, after: :piiri
+    add_column :asiakasalennus, :kampanja, :integer, limit: 4, default: 0, null: false, after: :piiri
+    add_column :lasku,          :kampanja, :integer, limit: 4, default: 0, null: false, after: :piiri
+    add_column :tilausrivi,     :kampanja, :integer, limit: 4, default: 0, null: false, after: :suuntalava
+  end
+end
