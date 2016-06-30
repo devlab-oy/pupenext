d121431d371cc25b370336add49e4806a7277122
Tommi Järvinen <tommi@devlab.fi>
2016-06-30 16:09:33 +0300

    No session in API

--- app/controllers/administration/companies_controller.rb
+++ app/controllers/administration/companies_controller.rb
@@ -1,4 +1,6 @@
 class Administration::CompaniesController < ApplicationController
+  protect_from_forgery with: :null_session
+
   skip_before_action :authorize,     :set_current_info, :set_locale, :access_control
   before_action      :api_authorize, :set_current_info, :set_locale, :access_control
 
