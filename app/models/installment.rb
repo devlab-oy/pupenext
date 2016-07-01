7614f4a465f8499cbfa3c755f00c3bc09719e376
Joni Kanerva <joni@devlab.fi>
2016-07-01 16:12:27 +0300

    installment has also drafts

--- app/models/installment.rb
+++ app/models/installment.rb
@@ -1,5 +1,6 @@
 class Installment < BaseModel
   belongs_to :parent_order, foreign_key: :otunnus, class_name: 'SalesOrder::Order'
+  belongs_to :parent_draft, foreign_key: :otunnus, class_name: 'SalesOrder::Draft'
   has_one :sales_order, primary_key: :uusiotunnus, foreign_key: :tunnus, class_name: 'SalesOrder::Order'
 
   self.table_name = :maksupositio
