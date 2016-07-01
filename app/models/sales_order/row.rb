6d6e3ea7a5fcf45cf09ddaea1bc89b18e58d5a78
Joni Kanerva <joni@devlab.fi>
2016-07-01 15:36:39 +0300

    fix query

--- app/models/sales_order/row.rb
+++ app/models/sales_order/row.rb
@@ -6,6 +6,10 @@ class SalesOrder::Row < Row
 
   scope :open, -> { where("(tilausrivi.varattu + tilausrivi.jt) > 0 AND tilausrivi.laskutettuaika = '0000-00-00'") }
 
+  def reserved
+    varattu + jt
+  end
+
   # Rails requires sti_name method to return type column (tyyppi) value
   def self.sti_name
     "L"
