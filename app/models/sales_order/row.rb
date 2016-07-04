7951db92d153b94b087be3101dda3733e5ea89a1
Joni Kanerva <joni@devlab.fi>
2016-07-04 08:43:54 +0300

    move to base model

--- app/models/sales_order/row.rb
+++ app/models/sales_order/row.rb
@@ -1,7 +1,4 @@
 class SalesOrder::Row < Row
-  belongs_to :order, foreign_key: :otunnus, class_name: 'SalesOrder::Base'
-  has_one :product, foreign_key: :tuoteno, primary_key: :tuoteno
-
   validates :tyyppi, inclusion: { in: ['L'] }
 
   scope :open, -> { where("(tilausrivi.varattu + tilausrivi.jt) > 0 AND tilausrivi.laskutettuaika = '0000-00-00'") }
