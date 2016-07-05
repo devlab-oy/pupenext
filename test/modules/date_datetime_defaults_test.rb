636df1a364b67fc90516baeaa3367b25916b4e41
Tommi Järvinen <tommi@devlab.fi>
2016-07-05 12:51:48 +0300

    Don't set columns which allow null to zero

--- test/modules/date_datetime_defaults_test.rb
+++ test/modules/date_datetime_defaults_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class DateDatetimeDefaultsTest < ActiveSupport::TestCase
   fixtures %w(
     heads
+    supplier_product_informations
+    terms_of_payments
   )
 
   test 'date fields are set to zero by default correctly' do
@@ -17,5 +19,18 @@ class DateDatetimeDefaultsTest < ActiveSupport::TestCase
 
     assert_equal '0000-00-00 00:00:00',                  head.h1time
     assert_equal Time.zone.local(2016, 1, 2, 12, 0, 12), heads(:si_one).h1time
+  end
+
+  test 'date fields with default values are not set to zero' do
+    terms_of_payment = TermsOfPayment.create!(teksti: 'Testimaksuehto')
+
+    assert_nil terms_of_payment.kassa_abspvm
+    assert_nil terms_of_payment.abs_pvm
+  end
+
+  test 'datetime fields with default values are not set to zero' do
+    supplier_product_information = SupplierProductInformation.create!(product_id: '123')
+
+    assert_nil supplier_product_information.p_added_date
   end
 end
