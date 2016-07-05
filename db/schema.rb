4216d80da3419eb4973f3137733ed113d4e29d08
Joni Kanerva <joni@devlab.fi>
2016-07-05 11:02:44 +0300

    add permissions to campaigns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705074028) do
+ActiveRecord::Schema.define(version: 20160705075908) do
 
   create_table "abc_aputaulu", primary_key: "tunnus", force: :cascade do |t|
     t.string   "yhtio",              limit: 5,                            default: "",  null: false
