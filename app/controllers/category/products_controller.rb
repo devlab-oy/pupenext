b56464165414b1c61b3b9048e0a18bf7a4faaa3e
Tommi Järvinen <tommi@devlab.fi>
2016-07-06 12:11:16 +0300

    Category::ProductsController#tree

--- app/controllers/category/products_controller.rb
+++ app/controllers/category/products_controller.rb
@@ -2,4 +2,8 @@ class Category::ProductsController < CategoriesController
   def index
     render json: Category::Product.roots
   end
+
+  def tree
+    render json: Category::Product.tree
+  end
 end
