5029f8142345350cfbcb466a1cdfc5091937ea83
Tommi Järvinen <tommi@devlab.fi>
2016-07-06 10:47:41 +0300

    Category::ProductsController#index

--- app/controllers/category/products_controller.rb
+++ app/controllers/category/products_controller.rb
@@ -1,2 +1,5 @@
 class Category::ProductsController < CategoriesController
+  def index
+    render json: Category::Product.roots
+  end
 end
--- test/controllers/category/products_controller_test.rb
+++ test/controllers/category/products_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class Category::ProductsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures %w(
+    category/products
+  )
+
+  test '#index' do
+    get :index, access_token: users(:bob).api_key
+
+    assert_response :success
+
+    json_response.each do |category|
+      category.assert_valid_keys(:nimi, :koodi, :tunnus)
+    end
+  end
 end
