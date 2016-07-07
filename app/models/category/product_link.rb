ca0b7ab062c5eb9f7e49d7261943f595591ca18e
Tommi Järvinen <tommi@devlab.fi>
2016-07-07 13:56:42 +0300

    Add default values

--- app/models/category/product_link.rb
+++ app/models/category/product_link.rb
@@ -1,7 +1,15 @@
 class Category::ProductLink < Category::Link
   belongs_to :product, class_name: '::Product', foreign_key: :liitos, primary_key: :tuoteno
 
+  before_save :defaults
+
   def self.sti_name
     'tuote'
   end
+
+  private
+
+    def defaults
+      self.kutsuja ||= ''
+    end
 end
