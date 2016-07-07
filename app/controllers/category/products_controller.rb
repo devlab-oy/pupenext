afe56e5d7253496efbf266b508d0898136057b82
Tommi Järvinen <tommi@devlab.fi>
2016-07-07 09:10:58 +0300

    Add Category::ProductsController#children

--- app/controllers/category/products_controller.rb
+++ app/controllers/category/products_controller.rb
@@ -1,4 +1,6 @@
 class Category::ProductsController < CategoriesController
+  before_action :find_product_category, only: :children
+
   def index
     render json: Category::Product.roots
   end
@@ -6,4 +8,14 @@ class Category::ProductsController < CategoriesController
   def tree
     render json: Category::Product.tree
   end
+
+  def children
+    render json: @product_category.children
+  end
+
+  private
+
+    def find_product_category
+      @product_category = Category::Product.find(params[:id])
+    end
 end
--- test/controllers/category/products_controller_test.rb
+++ test/controllers/category/products_controller_test.rb
@@ -24,4 +24,14 @@ class Category::ProductsControllerTest < ActionController::TestCase
       category.assert_valid_keys(:nimi, :koodi, :tunnus, :children)
     end
   end
+
+  test '#children' do
+    get :children, id: category_products(:product_category_shirts).id, access_token: users(:bob).api_key
+
+    assert_response :success
+
+    json_response.each do |category|
+      category.assert_valid_keys(:nimi, :koodi, :tunnus)
+    end
+  end
 end
