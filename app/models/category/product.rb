b7c1ca112b9036e6fc9d4cd78de8b4cce39a308f
Tommi Järvinen <tommi@devlab.fi>
2016-07-07 13:22:42 +0300

    Add links and products associations
    Add links and products associations to Category::Product

--- app/models/category/product.rb
+++ app/models/category/product.rb
@@ -1,4 +1,7 @@
 class Category::Product < Category
+  has_many :links, foreign_key: :puun_tunnus, class_name: 'ProductLink'
+  has_many :products, through: :links
+
   # Rails requires sti_name method to return type column (laji) value
   def self.sti_name
     'tuote'
