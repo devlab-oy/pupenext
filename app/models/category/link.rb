0b9ea3c2458ed0f911d08aa6c7728c9cb18790c5
Tommi Järvinen <tommi@devlab.fi>
2016-07-07 13:12:16 +0300

    Add Category::Link & Category::ProductLink

--- app/models/category/link.rb
+++ app/models/category/link.rb
@@ -1,2 +1,17 @@
 class Category::Link < ActiveRecord::Base
+  include PupenextSingleTableInheritance
+
+  self.table_name         = :puun_alkio
+  self.primary_key        = :tunnus
+  self.inheritance_column = :laji
+
+  belongs_to :category, foreign_key: :puun_tunnus
+
+  def self.default_child_instance
+    child_class :tuote
+  end
+
+  def self.child_class_names
+    { tuote: Category::ProductLink }.stringify_keys
+  end
 end
--- test/models/category/link_test.rb
+++ test/models/category/link_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class Category::LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures %w(
+    category/links
+    category/products
+    products
+  )
+
+  test 'fixtures are valid' do
+    refute_empty Category::Link.all
+
+    Category::Link.all.each do |link|
+      assert link.valid?, link.errors.full_messages
+    end
+  end
+
+  test 'associations' do
+    assert_equal category_products(:product_category_shirts), category_links(:product_category_shirts_hammer).category
+    assert_equal category_products(:product_category_pants),  category_links(:product_category_pants_helmet).category
+
+    assert_equal products(:hammer), category_links(:product_category_shirts_hammer).product
+    assert_equal products(:helmet), category_links(:product_category_pants_helmet).product
+  end
 end
