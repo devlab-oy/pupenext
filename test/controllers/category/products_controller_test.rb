c7a05df28233cadc4eff0faba5a5d92f08bc7024
Mika M <giefsufu@gmail.com>
2016-07-08 16:24:58 +0300

    add test for products including descendants

--- test/controllers/category/products_controller_test.rb
+++ test/controllers/category/products_controller_test.rb
@@ -49,6 +49,22 @@ class Category::ProductsControllerTest < ActionController::TestCase
     get :products, id: category_products(:product_category_shirts).id, access_token: users(:bob).api_key
 
     assert_response :success
+    assert_equal category_products(:product_category_shirts).products.count, json_response.count
+    json_response.each do |product|
+      product.assert_valid_keys(:tunnus)
+    end
+  end
+
+  test '#products?include_descendants=true' do
+    get :products, id: category_products(:product_category_shirts).id, include_descendants: true, access_token: users(:bob).api_key
+
+    assert_response :success
+
+    total_shirts_count = category_products(:product_category_shirts_t_shirts_v_necks).products.count
+    total_shirts_count += category_products(:product_category_shirts_t_shirts).products.count
+    total_shirts_count += category_products(:product_category_shirts).products.count
+
+    assert_equal total_shirts_count, json_response.count
 
     json_response.each do |product|
       product.assert_valid_keys(:tunnus)
