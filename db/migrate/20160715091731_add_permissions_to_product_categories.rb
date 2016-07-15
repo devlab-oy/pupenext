a3f9ea2cb3778da28b31ca9d058151bdde1e8a5f
Tommi Järvinen <tommi@devlab.fi>
2016-07-15 12:20:21 +0300

    Add permission to product categories
    Add permission to product categories instead of categories

--- db/migrate/20160715091731_add_permissions_to_product_categories.rb
+++ db/migrate/20160715091731_add_permissions_to_product_categories.rb
@@ -1,19 +1,19 @@
 require File.expand_path('test/permission_helper')
 include PermissionHelper
 
-class AddPermissionsToCategories < ActiveRecord::Migration
+class AddPermissionsToProductCategories < ActiveRecord::Migration
   def up
     PermissionHelper.add_item(
       program: 'Tuotehallinta',
       name: 'Dynaaminen puu',
-      uri: 'pupenext/categories',
+      uri: 'pupenext/product_categories',
       hidden: 'H',
     )
   end
 
   def down
     PermissionHelper.remove_all(
-      uri: 'pupenext/categories',
+      uri: 'pupenext/product_categories',
     )
   end
 end
