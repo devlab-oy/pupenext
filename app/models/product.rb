69067f4830fd965ac59218794d50340690cd5550
Joni Kanerva <joni@devlab.fi>
2016-07-19 15:56:03 +0300

    date and datetimes are handled in base_model

--- app/models/product.rb
+++ app/models/product.rb
@@ -43,7 +43,6 @@ class Product < BaseModel
   self.primary_key = :tunnus
 
   before_save :defaults
-  before_create :set_date_fields
 
   enum tuotetyyppi: {
     expence: 'B',
@@ -125,15 +124,6 @@ class Product < BaseModel
   end
 
   private
-
-    def set_date_fields
-      # Date fields can be set to zero
-      self.vihapvm           ||= 0
-      self.epakurantti25pvm  ||= 0
-      self.epakurantti50pvm  ||= 0
-      self.epakurantti75pvm  ||= 0
-      self.epakurantti100pvm ||= 0
-    end
 
     def defaults
       self.vienti ||= ''
