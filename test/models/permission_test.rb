5b6fbe318a241ca6576e28983c22ab86738baa4e
Joni Kanerva <joni@devlab.fi>
2016-08-04 10:46:12 +0300

    sovellus is required

--- test/models/permission_test.rb
+++ test/models/permission_test.rb
@@ -8,9 +8,12 @@ class PermissionTest < ActiveSupport::TestCase
     @update = permissions(:bob_customers_update)
   end
 
-  test "fixtures is valid" do
-    assert @read.valid?
-    assert @update.valid?
+  test 'fixtures is valid' do
+    refute_empty Permission.all
+
+    Permission.all.each do |permission|
+      assert permission.valid?, "Permission #{permission.nimi}: #{permission.errors.full_messages}"
+    end
   end
 
   test "permission has a user" do
