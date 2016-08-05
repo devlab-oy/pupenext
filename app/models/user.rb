e92730ba711ad6e97b39db3ecfb82e1904258884
Joni Kanerva <joni@devlab.fi>
2016-08-05 12:50:17 +0300

    add update permissions method

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,66 @@ class User < BaseModel
   def classic_ui?
     kayttoliittyma == 'C' || (kayttoliittyma.blank? && company.classic_ui?)
   end
+
+  def update_permissions
+    # mark all current user permissions with a uid
+    marked_for_delete = SecureRandom.uuid
+    permissions.update_all muuttaja: marked_for_delete
+
+    # fetch permissions this user should have
+    user_permissions.each do |p|
+      # check if user has this permission
+      permission = permissions.find_or_initialize_by find_by_parameters(p)
+
+      # update permission with valid info, don't raise exception, just fail if incorret
+      permission.update update_with_parameters(p)
+    end
+
+    # remove permission user no longer has. update above changes muuttaja to current user
+    permissions.where(muuttaja: marked_for_delete).delete_all
+  end
+
+  def profiles
+    profiilit.to_s.split ','
+  end
+
+  private
+
+    def user_permissions
+      # fetch all user profiles and users locked permissions
+      # we need to order profiles by paivitys, so we'll get update permissions later in the array
+      params = company.user_profiles.where(profiili: profiles).order(:paivitys).map(&:attributes)
+
+      # add locked parameters to the end of array, these override all profile attributes
+      params << permissions.locked_permissions.map(&:attributes)
+
+      params.flatten.uniq
+    end
+
+    def find_by_parameters(attributes)
+      # these are the unique attributes, we should fetch the permission by
+      {
+        alanimi:  attributes['alanimi'],
+        nimi:     attributes['nimi'],
+        nimitys:  attributes['nimitys'],
+        sovellus: attributes['sovellus'],
+      }
+    end
+
+    def update_with_parameters(attributes)
+      # these are the attributes we need to update for the user permission
+      {
+        hidden:        attributes['hidden'],
+        jarjestys2:    attributes['jarjestys2'],
+        jarjestys:     attributes['jarjestys'],
+        kuka:          kuka,
+        lukittu:       attributes['lukittu'],
+        muuttaja:      Current.user || kuka,
+        nimi:          attributes['nimi'],
+        nimitys:       attributes['nimitys'],
+        paivitys:      attributes['paivitys'],
+        sovellus:      attributes['sovellus'],
+        usermanualurl: attributes['usermanualurl'],
+      }
+    end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,6 +4,8 @@ class UserTest < ActiveSupport::TestCase
   fixtures %w(
     download/downloads
     download/files
+    menus
+    user_profiles
     users
   )
 
@@ -12,21 +14,21 @@ class UserTest < ActiveSupport::TestCase
     @bob = users(:bob)
   end
 
-  test "user model" do
+  test 'user model' do
     assert User.new
   end
 
-  test "fixture is valid" do
+  test 'fixture is valid' do
     assert @joe.valid?
   end
 
-  test "relations" do
+  test 'relations' do
     assert_not_nil @joe.company
     assert @joe.downloads.count > 0
     assert @joe.files.count > 0
   end
 
-  test "user has update permissions" do
+  test 'user has update permissions' do
     assert_equal 0, @joe.permissions.update_permissions.count
   end
 
@@ -38,13 +40,13 @@ class UserTest < ActiveSupport::TestCase
     assert @joe.permissions.update_access '/companies'
   end
 
-  test "can read" do
+  test 'can read' do
     assert @joe.can_read? '/customers'
     assert @bob.can_read? '/customers'
     refute users(:max).can_read? '/test'
   end
 
-  test "can update" do
+  test 'can update' do
     refute @joe.can_update? '/currencies'
     refute @joe.can_update? '/companies'
 
@@ -52,7 +54,7 @@ class UserTest < ActiveSupport::TestCase
     assert @bob.can_update? '/companies'
   end
 
-  test "classic permissions" do
+  test 'classic permissions' do
     refute @joe.can_read?   '/customers', classic: true
     refute @bob.can_read?   '/customers', classic: true
     refute @joe.can_update? '/customers', classic: true
@@ -69,29 +71,64 @@ class UserTest < ActiveSupport::TestCase
     assert @bob.can_update? 'tulosta_tuotetarrat.php', classic: true
   end
 
-  test "user has correct css" do
+  test 'user has correct css' do
     @joe.company.parameter.kayttoliittyma = 'U'
     @joe.kayttoliittyma = ''
-    refute @joe.classic_ui?, "company set new, joe set nothing, should be new"
+    refute @joe.classic_ui?, 'company set new, joe set nothing, should be new'
     @joe.kayttoliittyma = 'U'
-    refute @joe.classic_ui?, "company set new, joe set new, should be new"
+    refute @joe.classic_ui?, 'company set new, joe set new, should be new'
     @joe.kayttoliittyma = 'C'
-    assert @joe.classic_ui?, "company set new, joe set classic, should be classic"
+    assert @joe.classic_ui?, 'company set new, joe set classic, should be classic'
 
     @joe.company.parameter.kayttoliittyma = 'C'
     @joe.kayttoliittyma = ''
-    assert @joe.classic_ui?, "company set classic, joe set nothing, show be classic"
+    assert @joe.classic_ui?, 'company set classic, joe set nothing, show be classic'
     @joe.kayttoliittyma = 'U'
-    refute @joe.classic_ui?, "company set classic, joe set new, show be new"
+    refute @joe.classic_ui?, 'company set classic, joe set new, show be new'
     @joe.kayttoliittyma = 'C'
-    assert @joe.classic_ui?, "company set classic, joe set classic, show be classic"
+    assert @joe.classic_ui?, 'company set classic, joe set classic, show be classic'
 
     @joe.company.parameter.kayttoliittyma = ''
     @joe.kayttoliittyma = ''
-    assert @joe.classic_ui?, "company set nothing, joe set nothing, should be classic"
+    assert @joe.classic_ui?, 'company set nothing, joe set nothing, should be classic'
     @joe.kayttoliittyma = 'U'
-    refute @joe.classic_ui?, "company set nothing, joe set new, should be new"
+    refute @joe.classic_ui?, 'company set nothing, joe set new, should be new'
     @joe.kayttoliittyma = 'C'
-    assert @joe.classic_ui?, "company set nothing, joe set classic, should be classic"
+    assert @joe.classic_ui?, 'company set nothing, joe set classic, should be classic'
+  end
+
+  test 'update permissions' do
+    # all joes permissions are lukittu, and he does not belong to a profile
+    refute_empty @joe.permissions
+    @joe.update_attributes! profiilit: ''
+    @joe.permissions.update_all lukittu: 1
+
+    assert_no_difference '@joe.permissions.count' do
+      @joe.update_permissions
+    end
+
+    # all permissions all lukittu, but profile has permissions we don't have
+    profile = user_profiles(:acme_profile_1).dup
+    profile.update_attributes!(
+      jarjestys: 1,
+      kuka: 'new profile',
+      nimi: 'new/app',
+      profiili: 'new profile',
+      sovellus: 'new menu',
+    )
+    @joe.update_attributes! profiilit: 'new profile'
+
+    # we should have one new permission from new profile
+    assert_difference '@joe.permissions.count' do
+      @joe.update_permissions
+    end
+
+    # we should get only the one permission from the new profile
+    Permission.delete_all
+    assert_equal 0, @joe.permissions.count
+
+    assert_difference '@joe.permissions.count' do
+      @joe.update_permissions
+    end
   end
 end
