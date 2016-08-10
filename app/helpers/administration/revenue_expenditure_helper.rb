8066caaa16bf90e285acba8485c1d4b68680c8e9
Joni Kanerva <joni@devlab.fi>
2016-08-10 10:51:37 +0300

    refactor

--- app/helpers/administration/revenue_expenditure_helper.rb
+++ app/helpers/administration/revenue_expenditure_helper.rb
@@ -1,24 +1,22 @@
 module Administration::RevenueExpenditureHelper
   def week_options(week_year)
+    date_from = parse_date(week_year).years_ago(1)
+    date_to   = parse_date(week_year).years_since(1)
 
-    if week_year.empty?
-      week_year = Date.today.strftime "%Y%V"
-    end
+    date_from.upto(date_to).map do |d|
+      value = d.strftime '%Y%V'
+      text  = d.strftime '%W / %Y'
 
-    date_from, date_to = date_range week_year
-
-    options = date_from.upto(date_to).map do |d|
-      value = d.strftime "%Y%V"
-      text = d.strftime "%W / %Y"
-      [ text, value ]
-    end
-
-    options.uniq!
+      [text, value]
+    end.uniq
   end
 
-  def date_range(week_year)
-    week = week_year[4..5].to_i
-    year = week_year[0..3].to_i
-    return Date.commercial(year, week).years_ago(1), Date.commercial(year, week).years_since(1)
-  end
+  private
+
+    def parse_date(week_year)
+      week = week_year[4..5].to_i
+      year = week_year[0..3].to_i
+
+      Date.commercial year, week
+    end
 end
