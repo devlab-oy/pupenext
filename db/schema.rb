b1778956a051c1ceb27d1091b5b0a87a514a2b04
Tommi Järvinen <tommi@devlab.fi>
2016-09-02 10:12:16 +0300

    Add permissions to pankkiyhteys send

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830135154) do
+ActiveRecord::Schema.define(version: 20160902070241) do
 
   create_table "abc_aputaulu", primary_key: "tunnus", force: :cascade do |t|
     t.string   "yhtio",              limit: 5,                            default: "",  null: false
