8126b482263225203c090b08c66a41d78dc534d2
Tommi Järvinen <tommi@devlab.fi>
2016-09-07 15:58:47 +0300

    Add caching

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -9,7 +9,9 @@ class StocksController < ApplicationController
       return render json: { error: 'warehouse_ids parameter is required' }, status: :bad_request
     end
 
-    stock = Stock.new(@product, warehouse_ids: params[:warehouse_ids]).stock_available_per_warehouse
+    stock = Rails.cache.fetch("#{@product.id}/#{@product.shelf_locations.maximum(:muutospvm)}") do
+      Stock.new(@product, warehouse_ids: params[:warehouse_ids]).stock_available_per_warehouse.to_json
+    end
 
     render json: stock
   end
