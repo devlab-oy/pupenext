b64c4658960e52d33b9a0dd33c045a1d19c18254
Tommi Järvinen <tommi@devlab.fi>
2016-09-07 10:34:26 +0300

    Refactor

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,7 +2,7 @@ class StocksController < ApplicationController
   protect_from_forgery with: :null_session
 
   skip_before_action :authorize,     :set_current_info, :set_locale, :access_control
-  before_action      :api_authorize, :set_current_info, :set_locale, :access_control
+  before_action      :api_authorize, :set_current_info, :set_locale, :access_control, :find_product
 
   def stock_available
     unless params[:warehouse_ids] && params[:warehouse_ids].respond_to?(:each_with_object)
@@ -10,9 +10,15 @@ class StocksController < ApplicationController
     end
 
     stock_available = params[:warehouse_ids].each_with_object({}) do |warehouse_id, stocks|
-      stocks[warehouse_id] = Stock.new(Product.find(params[:product_id]), warehouse_ids: [warehouse_id]).stock_available
+      stocks[warehouse_id] = Stock.new(@product, warehouse_ids: [warehouse_id]).stock_available
     end
 
     render json: stock_available
   end
+
+  private
+
+    def find_product
+      @product ||= Product.find(params[:product_id])
+    end
 end
