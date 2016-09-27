6f4a77663e47de83343857071bfff2af0bcbaec0
Joni Kanerva <joni@devlab.fi>
2016-09-27 15:36:02 +0300

    poistetaan myös avainsanat kannasta

--- db/migrate/20160912144357_update_product_statuses.rb
+++ db/migrate/20160912144357_update_product_statuses.rb
@@ -4,6 +4,7 @@ class UpdateProductStatuses < ActiveRecord::Migration
       Current.company = company
 
       company.products.where.not(status: %w(A E P T)).update_all(status: 'A')
+      company.keywords.where(laji: :s).delete_all
     end
   end
 end
