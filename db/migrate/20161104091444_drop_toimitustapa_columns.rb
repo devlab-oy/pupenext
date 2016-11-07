1516eaf6145073d65d857e2ec1f84a7c62e7c112
Johan Tötterman <juppe@devlab.fi>
2016-11-07 11:28:21 +0200

    Update 20161104091444_drop_toimitustapa_columns.rb

--- db/migrate/20161104091444_drop_toimitustapa_columns.rb
+++ db/migrate/20161104091444_drop_toimitustapa_columns.rb
@@ -1,9 +1,5 @@
 class DropToimitustapaColumns < ActiveRecord::Migration
   def up
     remove_columns :toimitustapa, :kuljyksikko
-
-    Company.find_each do |company|
-      Current.company = company.yhtio
-    end
   end
 end
