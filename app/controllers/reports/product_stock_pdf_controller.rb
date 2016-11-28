e9d4be13a073a2cdd44d8fbbf69538585eb8a39f
Joni Kanerva <joni@devlab.fi>
2016-11-28 10:51:51 +0200

    cleanup logic

--- app/controllers/reports/product_stock_pdf_controller.rb
+++ app/controllers/reports/product_stock_pdf_controller.rb
@@ -6,8 +6,7 @@ class Reports::ProductStockPdfController < ApplicationController
     product = Product.find_by tuoteno: product_params[:sku]
 
     if product
-      qty = product_params[:qty].blank? ? 1 : product_params[:qty]
-      redirect_to product_stock_pdf_path(qty, product.id, format: :pdf)
+      redirect_to product_stock_pdf_path(product_qty, product.id, format: :pdf)
     else
       notice = t('.product_not_found', sku: product_params[:sku])
       redirect_to product_stock_pdf_index_path(product_params), notice: notice
@@ -16,7 +15,7 @@ class Reports::ProductStockPdfController < ApplicationController
 
   def show
     @product = Product.find product_params[:id]
-    @qty = params[:qty].to_s
+    @qty = product_qty
 
     render :product
   end
@@ -25,5 +24,9 @@ class Reports::ProductStockPdfController < ApplicationController
 
     def product_params
       params.permit(:sku, :id, :qty)
+    end
+
+    def product_qty
+      product_params[:qty].blank? ? '1' : product_params[:qty].to_s
     end
 end
