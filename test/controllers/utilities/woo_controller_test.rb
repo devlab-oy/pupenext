28c0e5e8cd01fd44582d791d23bcba4758423d88
Joni Kanerva <joni@devlab.fi>
2016-12-09 17:05:41 +0200

    refactor

--- test/controllers/utilities/woo_controller_test.rb
+++ test/controllers/utilities/woo_controller_test.rb
@@ -1,46 +1,39 @@
-require "test_helper"
-require "minitest/mock"
-require "json_helper"
+require 'test_helper'
+require 'minitest/mock'
+require 'json_helper'
 
 class Utilities::WooControllerTest < ActionController::TestCase
   include JsonHelper
 
-  setup do
-    @user = users(:bob)
-    login @user
-  end
-
   test 'complete order' do
     params = {
-      access_token: @user.api_key,
+      access_token: users(:bob).api_key,
       store_url: 'dummy_url',
       consumer_key: 'test_key',
       consumer_secret: 'consumer_secret',
-      company_id: companies(:acme).id,
       order: {
         order_number: 123,
-        tracking_code: 'ff123456'
-      }
+        tracking_code: 'ff123456',
+      },
     }
 
     # failing complete order
     woo_orders = MiniTest::Mock.new
-    woo_orders.expect :complete_order, nil, ["123", "ff123456"]
+    woo_orders.expect :complete_order, nil, %w(123 ff123456)
 
-    error_response = {:error_messages=>"virhe tilauksen päivityksessä"}
+    error_response = { error_messages: 'virhe tilauksen päivityksessä' }
     Woo::Orders.stub :new, woo_orders do
       get :complete_order, params
       assert_equal error_response, json
     end
 
     # succesfull complete order
-    woo_orders.expect :complete_order, true, ["123", "ff123456"]
+    woo_orders.expect :complete_order, true, %w(123 ff123456)
 
-    success_response = {:message=>"tilaus päivitetty onnistuneesti"}
+    success_response = { message: 'tilaus päivitetty onnistuneesti' }
     Woo::Orders.stub :new, woo_orders do
       get :complete_order, params
       assert_equal success_response, json
     end
   end
-
 end
