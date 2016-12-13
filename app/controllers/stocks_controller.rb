ae9d81a03f5ef8f7e270a776e0b9aae97a6b202d
Joni Kanerva <joni@devlab.fi>
2016-12-13 09:54:02 +0200

    codestyle

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,18 +5,23 @@ class StocksController < ApplicationController
   before_action      :api_authorize, :set_current_info, :set_locale, :access_control, :find_product
 
   def stock_available_per_warehouse
-    unless params[:warehouse_ids] && params[:warehouse_ids].respond_to?(:each_with_object)
-      return render json: { error: 'warehouse_ids parameter is required' }, status: :bad_request
-    end
+    stock = Stock.new(@product, warehouse_ids: stock_parameters[:warehouse_ids])
 
-    stock = Stock.new(@product, warehouse_ids: params[:warehouse_ids]).stock_available_per_warehouse.to_json
-
-    render json: stock
+    render json: stock.stock_available_per_warehouse.to_json
+  rescue ActionController::ParameterMissing => e
+    render json: { message: e.to_s }, status: :bad_request
+  rescue
+    render json: { message: 'internal server error' }, status: :internal_server_error
   end
 
   private
 
     def find_product
       @product ||= Product.find(params[:product_id])
+    end
+
+    def stock_parameters
+      params.require(:warehouse_ids)
+      params.permit(warehouse_ids: [])
     end
 end
--- test/controllers/stocks_controller_test.rb
+++ test/controllers/stocks_controller_test.rb
@@ -15,26 +15,39 @@ class StocksControllerTest < ActionController::TestCase
 
   setup do
     @product = products(:hammer)
+    @api_key = users(:bob).api_key
   end
 
   test '#stock_available_per_warehouse' do
-    get :stock_available_per_warehouse, product_id: @product.id,
-                                        warehouse_ids: [warehouses(:veikkola).id, warehouses(:kontula).id],
-                                        access_token: users(:bob).api_key
+    params = {
+      product_id: @product.id,
+      warehouse_ids: [warehouses(:veikkola).id, warehouses(:kontula).id],
+      access_token: @api_key,
+    }
 
-    assert_response :success
+    get :stock_available_per_warehouse, params
+    assert_response :success, json_response
     assert_equal({ '198948233': '10.0', '250043353': '2.0' }, json_response)
   end
 
   test '#stock_available_per_warehouse without warehouse_ids' do
-    get :stock_available_per_warehouse, product_id: @product.id, access_token: users(:bob).api_key
+    params = {
+      product_id: @product.id,
+      access_token: @api_key,
+    }
 
-    assert_response :bad_request
+    get :stock_available_per_warehouse, params
+    assert_response :bad_request, json_response
   end
 
   test '#stock_available_per_warehouse with empty warehouse_ids' do
-    get :stock_available_per_warehouse, product_id: @product.id, warehouse_ids: [], access_token: users(:bob).api_key
+    params = {
+      product_id: @product.id,
+      warehouse_ids: [],
+      access_token: @api_key,
+    }
 
-    assert_response :success
+    get :stock_available_per_warehouse, params
+    assert_response :bad_request, json_response
   end
 end
