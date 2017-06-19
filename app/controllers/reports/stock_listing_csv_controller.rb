f1a82042913cd77bee781f73c2b072c13d1de2bc
Tommi Järvinen <tommi.jarvinen@sprintit.fi>
2017-06-19 15:52:45 +0300

    Revert "Add configurable stock listing report to stock listing controller"
    This reverts commit d3ef6ff697d34fcb6aed6c21eacc42b40afa03ef.

--- app/controllers/reports/stock_listing_csv_controller.rb
+++ app/controllers/reports/stock_listing_csv_controller.rb
@@ -19,14 +19,6 @@ class Reports::StockListingCsvController < ApplicationController
       report_name: t('reports.stock_listing_csv.index.header_ean')
     )
 
-    ReportJob.perform_later(
-      user_id: current_user.id,
-      company_id: current_company.id,
-      report_class: 'StockListingConfigurableCsv',
-      report_params: { company_id: current_company.id, column_separator: ';' },
-      report_name: t('reports.stock_listing_csv.index.header_configurable'),
-    )
-
     flash[:notice] = t('reports.stock_listing_csv.index.running')
     redirect_to stock_listing_csv_path
   end
--- test/controllers/reports/stock_listing_csv_controller_test.rb
+++ test/controllers/reports/stock_listing_csv_controller_test.rb
@@ -32,24 +32,14 @@ class Reports::StockListingCsvControllerTest < ActionController::TestCase
       report_name: "Varastosaldot EAN CSV"
     }]
 
-    configurable_job_args = [{
-      user_id: @bob.id,
-      company_id: @bob.company.id,
-      report_class: 'StockListingConfigurableCsv',
-      report_params: { company_id: @bob.company.id, column_separator: ';' },
-      report_name: 'Varastosaldot FIN CSV',
-    }]
-
     assert_enqueued_with(job: ReportJob, args: job_args) do
       assert_enqueued_with(job: ReportJob, args: ean_job_args) do
-        assert_enqueued_with(job: ReportJob, args: configurable_job_args) do
-          post :run
-          assert_redirected_to stock_listing_csv_path
-          assert_not_equal '', flash.notice
-        end
+        post :run
+        assert_redirected_to stock_listing_csv_path
+        assert_not_equal "", flash.notice
       end
     end
 
-    assert_enqueued_jobs 3
+    assert_enqueued_jobs 2
   end
 end
