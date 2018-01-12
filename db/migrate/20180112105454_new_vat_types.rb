94a1580f8c9c4ad8b61fe9ac9897e01154d4b21d
Tony Selkamo <tony@devlab.fi>
2018-01-12 14:53:16 +0200

    New VAT type

---
+++ db/migrate/20180112105454_new_vat_types.rb
@@ -0,0 +1,16 @@
+class NewVatTypes < ActiveRecord::Migration
+  def up
+    Company.find_each do |company|
+      sql = ""
+      execute "INSERT INTO `taso` (`yhtio`, `tyyppi`, `taso`, `nimi`) VALUES ('#{company.yhtio}', 'A', 'fi304', 'Vero tavaroiden maahantuonnista EU:n ulkop.');"
+      execute "INSERT INTO `taso` (`yhtio`, `tyyppi`, `taso`, `nimi`) VALUES ('#{company.yhtio}', 'A', 'fi310', 'Tavaroiden maahantuonnit EU:n ulkop.');"
+    end
+  end
+
+  def down
+    Company.find_each do |company|
+      execute "DELETE FROM taso WHERE yhtio = '#{company.yhtio}' AND tyyppi = 'A' AND taso = 'fi304';"
+      execute "DELETE FROM taso WHERE yhtio = '#{company.yhtio}' AND tyyppi = 'A' AND taso = 'fi310';"
+    end
+  end
+end
