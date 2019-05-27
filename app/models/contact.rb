e99c9b304c25b1e56b8aa0d59d93ca9e1e67a4c5
Olli Björkqvist <olli.bjorkqvist@sprintit.fi>
2019-05-27 18:16:21 +0300

    added contact model

---
+++ app/models/contact.rb
@@ -0,0 +1,8 @@
+class Head < BaseModel
+  include PupenextSingleTableInheritance
+
+  self.table_name = :yhteyshenkilo
+  self.primary_key = :tunnus
+  
+  belongs_to :customer, foreign_key: :liitostunnus
+end
