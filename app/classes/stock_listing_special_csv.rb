891fb558d1955dfd5e4e2dcc4911d6c80256b39a
Olli Björkqvist <olli.bjorkqvist@sprintit.fi>
2019-08-02 13:12:51 +0300

    uusi rapsa

---
+++ app/classes/stock_listing_special_csv.rb
@@ -0,0 +1,46 @@
+require 'csv'
+
+class StockListingSpecialCsv < StockListingCsv
+  # This is the Special version of StockListingCsv
+  # Difference is the output of the data method
+  # and the output filename should not be unique
+
+  def to_file
+    filename = "#{Dir.tmpdir}/VarastotilannePuutteilla #{Date.today.strftime('%d.%m.%Y')}.csv"
+
+    CSV.open(filename, "wb", @options) do |csv|
+      data.map { |row| csv << row }
+    end
+
+    filename
+  end
+
+  private
+
+    def data
+      @data ||= company.products.inventory_management.active.where.not(eankoodi: '').find_each.map do |product|
+        row = ProductRow.new product
+        myynti6kk = ActiveRecord::Base.connection.execute("select round(sum(rivihinta)) from tilausrivi where tuoteno = #{row.product.tuoteno} and tyyppi ='L' and laskutettuaika >= (DATE_SUB(CURDATE(), INTERVAL 6 MONTH));")
+        myynti12kk = ActiveRecord::Base.connection.execute("select round(sum(rivihinta)) from tilausrivi where tuoteno = #{row.product.tuoteno} and tyyppi ='L' and laskutettuaika >= (DATE_SUB(CURDATE(), INTERVAL 12 MONTH));")
+
+        [
+	     row.product.tuotemerkki,
+         row.product.nimitys,
+	     row.product.tuoteno,
+         row.stock,
+	     row.product.tuoteno,
+         row.product.stock_reserved,
+         row.product.tuoteno,
+         row.product.coming_in_next_order,
+         row.product.tuoteno,
+         #var_kiertonop
+         row.product.tuoteno,
+         myynti6kk,
+         row.product.tuoteno,
+         myynti12kk,
+         row.product.tuoteno,
+         row.product.shortage_days
+        ]
+      end
+    end
+end
--- app/controllers/reports/stock_listing_csv_controller.rb
+++ app/controllers/reports/stock_listing_csv_controller.rb
@@ -19,6 +19,14 @@ class Reports::StockListingCsvController < ApplicationController
       report_name: t('reports.stock_listing_csv.index.header_ean')
     )
 
+    ReportJob.perform_later(
+      user_id: current_user.id,
+      company_id: current_company.id,
+      report_class: 'StockListingSpecialCsv',
+      report_params: { company_id: current_company.id, column_separator: ';' },
+      report_name: t('reports.stock_listing_csv.index.header_ean')
+    )
+
     flash[:notice] = t('reports.stock_listing_csv.index.running')
     redirect_to stock_listing_csv_path
   end
